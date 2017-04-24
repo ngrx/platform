c5adbc88fee54609b189f92f91d50be1acc9e1a4
Brandon <robertsbt@gmail.com>
2017-04-23 22:21:26 -0500

    build: Add nightly deployment to build repos

---
+++ build/deploy-build.ts
@@ -0,0 +1,16 @@
+import * as tasks from './tasks';
+import { createBuilder } from './util';
+import { packages } from './config';
+
+
+const deploy = createBuilder([
+  [ 'Deploy builds', tasks.publishToRepo ]
+]);
+
+deploy({
+  scope: '@ngrx',
+  packages
+}).catch(err => {
+  console.error(err);
+  process.exit(1);
+});
--- build/index.ts
+++ build/index.ts
@@ -1,14 +1,10 @@
 import build from './builder';
+import { packages } from './config';
 
 
 build({
   scope: '@ngrx',
-  packages: [
-    'store',
-    'effects',
-    'router-store',
-    'store-devtools',
-  ]
+  packages
 }).catch(err => {
   console.error(err);
   process.exit(1);
