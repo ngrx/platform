95d288b3e350d2e47390a769f9666bfe03a6b1a4
Brandon <robertsbt@gmail.com>
2017-04-27 20:19:37 -0500

    chore(build): Use custom format for commit message (#21)

--- build/tasks.ts
+++ build/tasks.ts
@@ -208,7 +208,6 @@ export async function publishToRepo(config: Config) {
     const REPO_DIR = `./tmp/${pkg}`;
     const SHA = await util.git([`rev-parse HEAD`]);
     const SHORT_SHA = await util.git([`rev-parse --short HEAD`]);
-    const COMMIT_MSG = await util.git([`log --oneline -1`]);
     const COMMITTER_USER_NAME = await util.git([`--no-pager show -s --format='%cN' HEAD`]);
     const COMMITTER_USER_EMAIL = await util.git([`--no-pager show -s --format='%cE' HEAD`]);
 
@@ -222,13 +221,14 @@ export async function publishToRepo(config: Config) {
     await util.git(['checkout -b master']);
     await process.chdir('../../');
     await util.cmd('rm -rf', [`${REPO_DIR}/*`]);
+    await util.git([`log --format="%h %s" -n 1 > ${REPO_DIR}/commit_message`]);
     await util.cmd('cp', [`-R ${SOURCE_DIR}/* ${REPO_DIR}/`]);
     await process.chdir(`${REPO_DIR}`);
     await util.git([`config user.name "${COMMITTER_USER_NAME}"`]);
     await util.git([`config user.email "${COMMITTER_USER_EMAIL}"`]);
     await util.git(['add --all']);
-    await util.cmd('echo', [`"${COMMIT_MSG.replace(/\r?\n|\r/g, '')}" > commit_message`]);
     await util.git([`commit -F commit_message`]);
+    await util.cmd('rm', ['commit_message']);
     await util.git(['push origin master --force']);
     await process.chdir('../../');
   }
