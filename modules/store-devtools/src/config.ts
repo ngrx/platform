e21d688aff4fea52d7659c73d7bde48c97efe09b
Blake LaFleur <blake.k.lafleur@gmail.com>
2017-05-24 22:13:06 -0500

    fix(StoreDevtools): Type InjectionToken for AOT compilation

--- modules/store-devtools/src/config.ts
+++ modules/store-devtools/src/config.ts
@@ -8,10 +8,9 @@ export interface StoreDevtoolsConfig {
   shouldInstrument: Type<boolean> | InjectionToken<boolean>;
 }
 
-export const STORE_DEVTOOLS_CONFIG = new InjectionToken('@ngrx/devtools Options');
-export const INITIAL_OPTIONS = new InjectionToken('@ngrx/devtools Initial Config');
+export const STORE_DEVTOOLS_CONFIG = new InjectionToken<StoreDevtoolsConfig>('@ngrx/devtools Options');
+export const INITIAL_OPTIONS = new InjectionToken<StoreDevtoolsConfig>('@ngrx/devtools Initial Config');
 export const SHOULD_INSTRUMENT = new InjectionToken<boolean>('@ngrx/devtools Should Instrument');
-
 
 export type StoreDevtoolsOptions
   = Partial<StoreDevtoolsConfig>
