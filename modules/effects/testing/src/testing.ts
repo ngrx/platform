0913919b919ec44d8273277220e2f39853a42f9b
Mike Ryan <mike.ryan52@gmail.com>
2017-07-11 19:52:10 -0500

    feat(Effects): Introduce new Effects testing module

---
+++ modules/effects/testing/src/testing.ts
@@ -0,0 +1,20 @@
+import { Provider } from '@angular/core';
+import { Actions } from '@ngrx/effects';
+import { Observable } from 'rxjs/Observable';
+import { defer } from 'rxjs/observable/defer';
+
+
+export function provideMockActions(source: Observable<any>): Provider;
+export function provideMockActions(factory: () => Observable<any>): Provider;
+export function provideMockActions(factoryOrSource: (() => Observable<any>) | Observable<any>): Provider {
+  return {
+    provide: Actions,
+    useFactory: (): Observable<any> => {
+      if (typeof factoryOrSource === 'function') {
+        return defer(factoryOrSource);
+      }
+
+      return factoryOrSource;
+    },
+  };
+}
