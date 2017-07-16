24565e2308efd4670bab87bf5af0c5758cd2d285
Brian Frichette <brian.frichette@gmail.com>
2017-07-15 21:20:42 -0700

    test(platform): Add spec watcher and unit tests
    - Add watch:tests for simpler dev / test
    - Store utils spec

--- modules/store/src/utils.ts
+++ modules/store/src/utils.ts
@@ -45,9 +45,15 @@ export function omit<T extends { [key: string]: any }>(
   object: T,
   keyToRemove: keyof T
 ): Partial<T> {
-  return Object.keys(object)
-    .filter(key => key !== keyToRemove)
-    .reduce((result, key) => Object.assign(result, { [key]: object[key] }), {});
+  const ret: Partial<T> = {};
+
+  for (const key of Object.keys(object)) {
+    if (key !== keyToRemove) {
+      ret[key] = object[key];
+    }
+  }
+
+  return ret;
 }
 
 export function compose<A>(): (i: A) => A;
