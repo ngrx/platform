98bf99178c6cb6f019793d2c533a698017e7ee07
Rob Wormald <robwormald@gmail.com>
2017-07-18 15:44:01 -0700

    fix(effects): allow downleveled annotations

--- modules/effects/spec/effects_metadata.spec.ts
+++ modules/effects/spec/effects_metadata.spec.ts
@@ -23,6 +23,24 @@ describe('Effect Metadata', () => {
       ]);
     });
 
+    it('should get the effects metadata for a downleveled class instance', () => {
+      class Fixture {
+        static get propDecorators() {
+          return {
+            a: [{ type: Effect, args: [{ dispatch: false }] }],
+            b: [{ type: Effect, args: [] }],
+          };
+        }
+      }
+
+      const mock = new Fixture();
+
+      expect(getSourceMetadata(mock)).toEqual([
+        { propertyName: 'a', dispatch: false },
+        { propertyName: 'b', dispatch: true },
+      ]);
+    });
+
     it('should return an empty array if the class has not been decorated', () => {
       class Fixture {
         a: any;
