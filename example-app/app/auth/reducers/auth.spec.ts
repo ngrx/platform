60100fa742e48c8bde19de21ab3749ae2ba8a9f2
bcabanes <contact@benjamincabanes.com>
2017-07-19 12:10:29 -0400

    test(example): add auth and login-page tests

---
+++ example-app/app/auth/reducers/auth.spec.ts
@@ -0,0 +1,49 @@
+import { reducer } from './auth';
+import * as fromAuth from './auth';
+import { Login, LoginSuccess, Logout } from '../actions/auth';
+import { Authenticate, User } from '../models/user';
+
+describe('AuthReducer', () => {
+  describe('undefined action', () => {
+    it('should return the default state', () => {
+      const action = {} as any;
+
+      const result = reducer(undefined, action);
+      expect(result).toEqual(fromAuth.initialState);
+    });
+  });
+
+  describe('LOGIN_SUCCESS', () => {
+    it('should login a user', () => {
+      const user = { name: 'test' } as User;
+      const createAction = new LoginSuccess({ user });
+      const expectedResult = {
+        loggedIn: true,
+        user: { name: 'test' },
+      };
+      const result = reducer(fromAuth.initialState, createAction);
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should NOT authenticate a user', () => {
+      const user = { username: 'someUserName' } as Authenticate;
+      const createAction = new Login(user);
+      const expectedResult = fromAuth.initialState;
+      const result = reducer(fromAuth.initialState, createAction);
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe('LOGOUT', () => {
+    it('should logout a user', () => {
+      const initialState = {
+        loggedIn: true,
+        user: { name: 'test' },
+      } as fromAuth.State;
+      const createAction = new Logout();
+      const expectedResult = fromAuth.initialState;
+      const result = reducer(initialState, createAction);
+      expect(result).toEqual(expectedResult);
+    });
+  });
+});
