8d7133b641bb4ec4809314a5c9c010056ac2277a
Daniel Karp <danielkarp@gmail.com>
2017-07-22 11:33:59 -0400

    Typed version of toPayload

--- modules/effects/src/util.ts
+++ modules/effects/src/util.ts
@@ -1,5 +1,9 @@
-import { Action } from '@ngrx/store';
+import { Action, ActionWithPayload } from '@ngrx/store';
 
 export function toPayload(action: Action): any {
   return (action as any).payload;
+}
+
+export function toPayloadTyped<T>(action: ActionWithPayload<T>): T {
+  return action.payload;
 }
