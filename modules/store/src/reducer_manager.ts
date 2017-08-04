4aec80c7f750cc325e9f898f1152618d78971b68
Brandon <robertsbt@gmail.com>
2017-08-04 09:15:18 -0500

    fix(Store): Set initial state for feature modules (#235)
    Closes #206, #233

--- modules/store/src/reducer_manager.ts
+++ modules/store/src/reducer_manager.ts
@@ -40,7 +40,7 @@ export class ReducerManager extends BehaviorSubject<ActionReducer<any, any>>
   }: StoreFeature<any, any>) {
     const reducer =
       typeof reducers === 'function'
-        ? reducers
+        ? (state = initialState, action: any) => reducers(state, action)
         : createReducerFactory(reducerFactory, metaReducers)(
             reducers,
             initialState
