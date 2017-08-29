d7cd73ddfd2a384a584ec38bde340579c08bdc39
tim <timdeschryver@outlook.com>
2017-08-29 18:24:43 +0200

    feat(Effects): Add root effects init action

--- modules/effects/src/effects_root_module.ts
+++ modules/effects/src/effects_root_module.ts
@@ -1,18 +1,22 @@
 import { NgModule, Inject, Optional } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store } from '@ngrx/store';
 import { EffectsRunner } from './effects_runner';
 import { EffectSources } from './effect_sources';
 import { ROOT_EFFECTS } from './tokens';
+
+export const ROOT_EFFECTS_INIT = '@ngrx/store/root effects init' as '@ngrx/store/root effects init';
 
 @NgModule({})
 export class EffectsRootModule {
   constructor(
     private sources: EffectSources,
     runner: EffectsRunner,
+    store: Store<any>,
     @Inject(ROOT_EFFECTS) rootEffects: any[],
     @Optional() storeModule: StoreModule
   ) {
     runner.start();
+    store.dispatch({ type: ROOT_EFFECTS_INIT });
 
     rootEffects.forEach(effectSourceInstance =>
       sources.addEffects(effectSourceInstance)
