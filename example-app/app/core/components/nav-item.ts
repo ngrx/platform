8e04fd178096f5dc41394a8f3ba75a97a23b1845
Brandon Roberts <robertsbt@gmail.com>
2017-09-27 19:48:10 -0500

    chore(Example): Add login info to example app. Fix sidenav issue with IE

--- example-app/app/core/components/nav-item.ts
+++ example-app/app/core/components/nav-item.ts
@@ -3,7 +3,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'bc-nav-item',
   template: `
-    <a md-list-item [routerLink]="routerLink" (click)="activate.emit()">
+    <a md-list-item [routerLink]="routerLink" (click)="navigate.emit()">
       <md-icon md-list-icon>{{ icon }}</md-icon>
       <span md-line><ng-content></ng-content></span>
       <span md-line class="secondary">{{ hint }}</span>
@@ -21,5 +21,5 @@ export class NavItemComponent {
   @Input() icon = '';
   @Input() hint = '';
   @Input() routerLink: string | any[] = '/';
-  @Output() activate = new EventEmitter();
+  @Output() navigate = new EventEmitter();
 }
