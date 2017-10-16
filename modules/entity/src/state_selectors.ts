0adf4ee0e4b47fe48bb90c9201d2b1e56f173032
Mike Ryan <mike.ryan52@gmail.com>
2017-10-16 11:58:41 -0500

    feat(Entity): Enable creating entity selectors without composing a state selector

--- modules/entity/src/state_selectors.ts
+++ modules/entity/src/state_selectors.ts
@@ -2,27 +2,40 @@ import { createSelector } from '@ngrx/store';
 import { EntityState, EntitySelectors, Dictionary } from './models';
 
 export function createSelectorsFactory<T>() {
-  return {
-    getSelectors<V>(
-      selectState: (state: V) => EntityState<T>
-    ): EntitySelectors<T, V> {
-      const selectIds = (state: any) => state.ids;
-      const selectEntities = (state: EntityState<T>) => state.entities;
-      const selectAll = createSelector(
+  function getSelectors(): EntitySelectors<T, EntityState<T>>;
+  function getSelectors<V>(
+    selectState: (state: V) => EntityState<T>
+  ): EntitySelectors<T, V>;
+  function getSelectors(
+    selectState?: (state: any) => EntityState<T>
+  ): EntitySelectors<T, any> {
+    const selectIds = (state: any) => state.ids;
+    const selectEntities = (state: EntityState<T>) => state.entities;
+    const selectAll = createSelector(
+      selectIds,
+      selectEntities,
+      (ids: T[], entities: Dictionary<T>): any =>
+        ids.map((id: any) => (entities as any)[id])
+    );
+
+    const selectTotal = createSelector(selectIds, ids => ids.length);
+
+    if (!selectState) {
+      return {
         selectIds,
         selectEntities,
-        (ids: T[], entities: Dictionary<T>): any =>
-          ids.map((id: any) => (entities as any)[id])
-      );
-
-      const selectTotal = createSelector(selectIds, ids => ids.length);
-
-      return {
-        selectIds: createSelector(selectState, selectIds),
-        selectEntities: createSelector(selectState, selectEntities),
-        selectAll: createSelector(selectState, selectAll),
-        selectTotal: createSelector(selectState, selectTotal),
+        selectAll,
+        selectTotal,
       };
-    },
-  };
+    }
+
+    return {
+      selectIds: createSelector(selectState, selectIds),
+      selectEntities: createSelector(selectState, selectEntities),
+      selectAll: createSelector(selectState, selectAll),
+      selectTotal: createSelector(selectState, selectTotal),
+    };
+  }
+
+  return { getSelectors };
 }
