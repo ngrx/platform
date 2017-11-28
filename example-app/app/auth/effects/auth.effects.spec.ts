299b94189b4c6ec1343b71ba88bd67ff43080571
bcabanes <contact@benjamincabanes.com>
2017-11-27 22:36:04 -0500

    chore(Example): Add authEffects router tests

--- example-app/app/auth/effects/auth.effects.spec.ts
+++ example-app/app/auth/effects/auth.effects.spec.ts
@@ -28,6 +28,7 @@ describe('AuthEffects', () => {
   let effects: AuthEffects;
   let authService: any;
   let actions$: TestActions;
+  let routerService: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -51,6 +52,9 @@ describe('AuthEffects', () => {
     effects = TestBed.get(AuthEffects);
     authService = TestBed.get(AuthService);
     actions$ = TestBed.get(Actions);
+    routerService = TestBed.get(Router);
+
+    spyOn(routerService, 'navigate').and.callThrough();
   });
 
   describe('login$', () => {
@@ -80,6 +84,22 @@ describe('AuthEffects', () => {
       authService.login = jest.fn(() => response);
 
       expect(effects.login$).toBeObservable(expected);
+    });
+  });
+
+  describe('loginSuccess$', () => {
+    it('should dispatch a RouterNavigation action', () => {
+      effects.loginSuccess$.subscribe(() => {
+        expect(routerService.navigate).toHaveBeenCalledWith('/');
+      });
+    });
+  });
+
+  describe('loginRedirect$', () => {
+    it('should dispatch a RouterNavigation action', () => {
+      effects.loginRedirect$.subscribe(() => {
+        expect(routerService.navigate).toHaveBeenCalledWith('/login');
+      });
     });
   });
 });
