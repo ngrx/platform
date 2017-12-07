09aae11903cb5f4d1b4651854771da9ef286d2dd
Jason Bedard <jason+github@jbedard.ca>
2017-12-07 00:39:32 -0800

    feat(ErrorReporter): expose ErrorReporter as a ErrorHandler for ngrx
    Fixes #626

--- modules/effects/src/error_reporter.ts
+++ modules/effects/src/error_reporter.ts
@@ -1,15 +1,37 @@
-import { Injectable, InjectionToken, Inject } from '@angular/core';
+import {
+  ErrorHandler,
+  Injectable,
+  InjectionToken,
+  Inject,
+} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Notification } from 'rxjs/Notification';
+import { Action } from '@ngrx/store';
 import { CONSOLE } from './tokens';
 
+export interface EffectError extends Error {
+  Source: any;
+  Effect: Observable<any> | (() => Observable<any>);
+  Error: any;
+  Notification: Notification<Action | null | undefined>;
+}
+
+export interface InvalidActionError extends Error {
+  Source: any;
+  Effect: Observable<any> | (() => Observable<any>);
+  Dispatched: Action | null | undefined;
+  Notification: Notification<Action | null | undefined>;
+}
+
 @Injectable()
-export class ErrorReporter {
+export class ErrorReporter implements ErrorHandler {
   constructor(@Inject(CONSOLE) private console: any) {}
 
-  report(reason: string, details: any): void {
-    this.console.group(reason);
+  handleError(error: any): void {
+    this.console.group(error.message);
 
-    for (let key in details) {
-      this.console.error(`${key}:`, details[key]);
+    for (let key in error) {
+      this.console.error(`${key}:`, error[key]);
     }
 
     this.console.groupEnd();
