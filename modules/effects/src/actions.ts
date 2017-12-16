dcbb050c0395ac8bbd6460ff2af5b24ea655946c
Brandon Roberts <robertsbt@gmail.com>
2017-12-16 15:18:08 -0600

    feat(Effects): Add lettable ofType operator
    Introduces lettable operator for Actions.ofType.
    
    BREAKING CHANGE:
    
    Updates minimum version of RxJS dependency.
    
    BEFORE:
    
    Minimum peer dependency of RxJS ^5.0.0
    
    AFTER:
    
    Minimum peer dependency of RxJS ^5.5.0

--- modules/effects/src/actions.ts
+++ modules/effects/src/actions.ts
@@ -3,6 +3,7 @@ import { Action, ScannedActionsSubject } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { Operator } from 'rxjs/Operator';
 import { filter } from 'rxjs/operator/filter';
+import { OperatorFunction } from 'rxjs/interfaces';
 
 @Injectable()
 export class Actions<V = Action> extends Observable<V> {
@@ -22,8 +23,14 @@ export class Actions<V = Action> extends Observable<V> {
   }
 
   ofType<V2 extends V = V>(...allowedTypes: string[]): Actions<V2> {
-    return filter.call(this, (action: Action) =>
+    return ofType<any>(...allowedTypes)(this.source as Actions<V2>);
+  }
+}
+
+export function ofType<T extends Action>(...allowedTypes: string[]) {
+  return function ofTypeOperator(source$: Actions<T>): Actions<T> {
+    return filter.call(source$, (action: Action) =>
       allowedTypes.some(type => type === action.type)
     );
-  }
+  };
 }
