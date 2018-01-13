e109ae7ce6bcb6a98b7e87a2f13fb4c7e85017f3
Brandon Roberts <robertsbt@gmail.com>
2018-01-12 23:05:45 -0600

    feat(Schematics): Add effect to registered effects array

--- modules/schematics/src/effect/index.spec.ts
+++ modules/schematics/src/effect/index.spec.ts
@@ -1,7 +1,11 @@
 import { Tree, VirtualTree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
-import { createAppModule, getFileContent } from '../utility/test';
+import {
+  createAppModule,
+  getFileContent,
+  createAppModuleWithEffects,
+} from '../utility/test';
 import { Schema as EffectOptions } from './schema';
 
 describe('Effect Schematic', () => {
@@ -16,6 +20,8 @@ describe('Effect Schematic', () => {
     spec: true,
     module: undefined,
     flat: false,
+    feature: false,
+    root: false,
   };
 
   let appTree: Tree;
@@ -76,5 +82,70 @@ describe('Effect Schematic', () => {
       0
     );
     expect(files.indexOf('/src/app/foo/foo.effects.spec.ts')).toEqual(-1);
+  });
+
+  it('should register the root effect in the provided module', () => {
+    const options = { ...defaultOptions, root: true, module: 'app.module.ts' };
+
+    const tree = schematicRunner.runSchematic('effect', options, appTree);
+    const content = getFileContent(tree, '/src/app/app.module.ts');
+
+    expect(content).toMatch(/EffectsModule\.forRoot\(\[FooEffects\]\)/);
+  });
+
+  it('should register the feature effect in the provided module', () => {
+    const options = { ...defaultOptions, module: 'app.module.ts' };
+
+    const tree = schematicRunner.runSchematic('effect', options, appTree);
+    const content = getFileContent(tree, '/src/app/app.module.ts');
+
+    expect(content).toMatch(/EffectsModule\.forFeature\(\[FooEffects\]\)/);
+  });
+
+  it('should add an effect to the empty array of registered effects', () => {
+    const storeModule = '/src/app/store.module.ts';
+    const options = { ...defaultOptions, module: 'store.module.ts' };
+    appTree = createAppModuleWithEffects(
+      appTree,
+      storeModule,
+      'EffectsModule.forRoot([])'
+    );
+
+    const tree = schematicRunner.runSchematic('effect', options, appTree);
+    const content = getFileContent(tree, storeModule);
+
+    expect(content).toMatch(/EffectsModule\.forRoot\(\[FooEffects\]\)/);
+  });
+
+  it('should add an effect to the existing registered effects', () => {
+    const storeModule = '/src/app/store.module.ts';
+    const options = { ...defaultOptions, module: 'store.module.ts' };
+    appTree = createAppModuleWithEffects(
+      appTree,
+      storeModule,
+      'EffectsModule.forRoot([UserEffects])'
+    );
+
+    const tree = schematicRunner.runSchematic('effect', options, appTree);
+    const content = getFileContent(tree, '/src/app/store.module.ts');
+
+    expect(content).toMatch(
+      /EffectsModule\.forRoot\(\[UserEffects, FooEffects\]\)/
+    );
+  });
+
+  it('should not add an effect to registered effects defined with a variable', () => {
+    const storeModule = '/src/app/store.module.ts';
+    const options = { ...defaultOptions, module: 'store.module.ts' };
+    appTree = createAppModuleWithEffects(
+      appTree,
+      storeModule,
+      'EffectsModule.forRoot(effects)'
+    );
+
+    const tree = schematicRunner.runSchematic('effect', options, appTree);
+    const content = getFileContent(tree, '/src/app/store.module.ts');
+
+    expect(content).not.toMatch(/EffectsModule\.forRoot\(\[FooEffects\]\)/);
   });
 });
