b390ef5c575160135f9a8c0e9fcadf88ca11e97e
Brandon <robertsbt@gmail.com>
2018-01-21 20:17:25 -0800

    fix(Effects): Remove toPayload utility function (#738)
    BREAKING CHANGE: The utility function `toPayload`, deprecated in @ngrx/effects v4.0, has been removed.
    
       Before:
        
        ```ts
        import { toPayload } from '@ngrx/effects';
        
        actions$.ofType('SOME_ACTION').map(toPayload);
        ```
    
        After:
    
        ```ts
        actions$.ofType('SOME_ACTION').map((action: SomeActionWithPayload) => action.payload)
        ```

--- modules/effects/src/util.ts
+++
@@ -1,8 +0,0 @@
-import { Action } from '@ngrx/store';
-
-/**
- * @deprecated Since version 4.1. Will be deleted in version 5.0.
- */
-export function toPayload(action: Action): any {
-  return (action as any).payload;
-}
