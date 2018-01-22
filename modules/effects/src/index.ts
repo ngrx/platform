f0e0fc0511474ca2531278086088ffe0ccef54fe
Brandon Roberts <robertsbt@gmail.com>
2018-01-21 21:50:22 -0600

    fix(Effects): Remove toPayload utility function
    BREAKING CHANGE: toPayload function is no longer exported
    
    BEFORE:
    
    actions$.ofType('SOME_ACTION').map(toPayload);
    
    AFTER:
    
    actions$.ofType('SOME_ACTION').map((action: SomeActionWithPayload) => action.payload)

--- modules/effects/src/index.ts
+++ modules/effects/src/index.ts
@@ -8,6 +8,5 @@ export { Actions, ofType } from './actions';
 export { EffectsModule } from './effects_module';
 export { EffectSources } from './effect_sources';
 export { OnRunEffects } from './on_run_effects';
-export { toPayload } from './util';
 export { EffectNotification } from './effect_notification';
 export { ROOT_EFFECTS_INIT } from './effects_root_module';
