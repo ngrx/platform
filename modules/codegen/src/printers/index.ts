8d4c2e881705ad9de62016437a0bc6eaed9d9a56
Mike Ryan <mike.ryan52@gmail.com>
2018-02-16 08:35:30 -0600

    chore(platform): Remove Codegen package (#826)

--- modules/codegen/src/printers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './action-factory-declaration';
-export * from './enum-declaration';
-export * from './import-declaration';
-export * from './type-dictionary-declaration';
-export * from './type-union-declaration';
