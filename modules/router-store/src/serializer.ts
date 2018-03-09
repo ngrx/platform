25ca678597eb78c9248f132a0a4b9dbab453d07a
vsavkin <avix1000@gmail.com>
2018-03-09 11:25:42 -0500

    fix(RouterStore): change the default serializer to work around cycles in RouterStateSnapshot

--- modules/router-store/src/serializer.ts
+++ modules/router-store/src/serializer.ts
@@ -1,13 +1,47 @@
-import { InjectionToken } from '@angular/core';
-import { RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 export abstract class RouterStateSerializer<T> {
   abstract serialize(routerState: RouterStateSnapshot): T;
 }
 
+export interface SerializedRouterStateSnapshot {
+  root: ActivatedRouteSnapshot;
+  url: string;
+}
+
 export class DefaultRouterStateSerializer
-  implements RouterStateSerializer<RouterStateSnapshot> {
-  serialize(routerState: RouterStateSnapshot) {
-    return routerState;
+  implements RouterStateSerializer<SerializedRouterStateSnapshot> {
+  serialize(routerState: RouterStateSnapshot): SerializedRouterStateSnapshot {
+    return {
+      root: this.serializeRoute(routerState.root),
+      url: routerState.url,
+    };
+  }
+
+  private serializeRoute(
+    route: ActivatedRouteSnapshot
+  ): ActivatedRouteSnapshot {
+    const children = route.children.map(c => this.serializeRoute(c));
+    return {
+      params: route.params,
+      paramMap: route.paramMap,
+      data: route.data,
+      url: route.url,
+      outlet: route.outlet,
+      routeConfig: {
+        component: route.routeConfig ? route.routeConfig.component : undefined,
+      },
+      queryParams: route.queryParams,
+      queryParamMap: route.queryParamMap,
+      fragment: route.fragment,
+      component: (route.routeConfig
+        ? route.routeConfig.component
+        : undefined) as any,
+      root: undefined as any,
+      parent: undefined as any,
+      firstChild: children[0],
+      pathFromRoot: undefined as any,
+      children,
+    };
   }
 }
