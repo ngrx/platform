07c97131e6ebdf1cf9bf6a002d27a68bfc84c372
sandangel <vinhsannguyen91@gmail.com>
2018-03-28 10:37:18 +0900

    build: work around for angular/angular#22210

--- tests.js
+++ tests.js
@@ -1,13 +1,4 @@
 require('ts-node/register');
-require('core-js/es7/reflect');
-require('zone.js/dist/zone-node.js');
-require('zone.js/dist/long-stack-trace-zone.js');
-require('zone.js/dist/proxy.js');
-require('zone.js/dist/sync-test.js');
-require('zone.js/dist/async-test.js');
-require('zone.js/dist/fake-async-test.js');
-const Jasmine = require('jasmine');
-
 const tsConfig = require("./tsconfig.json");
 const tsConfigPaths = require("tsconfig-paths");
  
@@ -17,6 +8,16 @@ tsConfigPaths.register({
     baseUrl,
     paths: tsConfig.compilerOptions.paths
 });
+
+require('core-js/es7/reflect');
+require('zone.js/dist/zone-node.js');
+require('zone.js/dist/long-stack-trace-zone.js');
+require('zone.js/dist/proxy.js');
+require('zone.js/dist/sync-test.js');
+require('zone.js/dist/async-test.js');
+require('zone.js/dist/fake-async-test.js');
+
+const Jasmine = require('jasmine');
 
 const runner = new Jasmine();
 
@@ -28,7 +29,6 @@ const { getTestBed } = require('@angular/core/testing');
 const { ServerTestingModule, platformServerTesting } = require('@angular/platform-server/testing');
 
 getTestBed().initTestEnvironment(ServerTestingModule, platformServerTesting());
-
 
 runner.loadConfig({
   spec_dir: 'modules',
