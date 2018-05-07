380ef4898d87e6bd0590b362e057c331d063684e
Piotr Staniów <staniowp@gmail.com>
2018-05-07 20:32:12 +0200

    feature: Testing module for @ngrx/store

---
+++ modules/store/testing/src/testing.ts
@@ -0,0 +1,31 @@
+import {
+  StateObservable,
+  Store,
+  ReducerManager,
+  ActionsSubject,
+} from '@ngrx/store';
+import { BehaviorSubject } from 'rxjs';
+
+export class MockStore<T> extends Store<T> {
+  private stateSubject = new BehaviorSubject<T>({} as T);
+
+  constructor(
+    state$: StateObservable,
+    actionsObserver: ActionsSubject,
+    reducerManager: ReducerManager
+  ) {
+    super(state$, actionsObserver, reducerManager);
+    this.source = this.stateSubject.asObservable();
+  }
+
+  nextMock(nextState: T) {
+    this.stateSubject.next(nextState);
+  }
+}
+
+export function provideMockStore() {
+  return {
+    provide: Store,
+    useClass: MockStore,
+  };
+}
