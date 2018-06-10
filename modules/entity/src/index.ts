58ae6993b93d560830a34917556cce9750af1f18
aokrushko <alex.okrushko@gmail.com>
2018-06-09 22:29:37 -0400

    expose Dictionary as part of an API

--- modules/entity/src/index.ts
+++ modules/entity/src/index.ts
@@ -1,2 +1,2 @@
 export { createEntityAdapter } from './create_adapter';
-export { EntityState, EntityAdapter, Update } from './models';
+export { Dictionary, EntityState, EntityAdapter, Update } from './models';
