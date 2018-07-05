76639728f2fd679b6c7c5125e9beadbf97be193b
Tim Deschryver <timdeschryver@outlook.com>
2018-07-05 16:09:51 +0200

    test: add id is undefined tests

--- modules/entity/spec/utils.spec.ts
+++ modules/entity/spec/utils.spec.ts
@@ -20,11 +20,36 @@ describe('Entity utils', () => {
       expect(spy).toHaveBeenCalled();
     });
 
+    it('should warn when key is undefined in dev mode', () => {
+      const spy = spyOn(console, 'warn');
+
+      const undefinedAClockworkOrange = { ...AClockworkOrange, id: undefined };
+      const key = selectIdValue(
+        undefinedAClockworkOrange,
+        (book: any) => book.id
+      );
+
+      expect(spy).toHaveBeenCalled();
+    });
+
     it('should not warn when key does not exist in prod mode', () => {
       spyOn(ngCore, 'isDevMode').and.returnValue(false);
       const spy = spyOn(console, 'warn');
 
       const key = selectIdValue(AClockworkOrange, (book: any) => book.foo);
+
+      expect(spy).not.toHaveBeenCalled();
+    });
+
+    it('should not warn when key is undefined in dev mode', () => {
+      spyOn(ngCore, 'isDevMode').and.returnValue(false);
+      const spy = spyOn(console, 'warn');
+
+      const undefinedAClockworkOrange = { ...AClockworkOrange, id: undefined };
+      const key = selectIdValue(
+        undefinedAClockworkOrange,
+        (book: any) => book.id
+      );
 
       expect(spy).not.toHaveBeenCalled();
     });
