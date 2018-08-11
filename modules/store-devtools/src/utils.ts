f1ac91d65c0ac0906c93b1cbae79807813432102
Wykks <wykksoss@gmail.com>
2018-08-11 18:05:31 +0200

    feat(StoreDevtools): implement actionsBlacklist/Whitelist & predicate

--- modules/store-devtools/src/utils.ts
+++ modules/store-devtools/src/utils.ts
@@ -2,7 +2,12 @@ import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import * as Actions from './actions';
-import { ActionSanitizer, StateSanitizer } from './config';
+import {
+  ActionSanitizer,
+  StateSanitizer,
+  Predicate,
+  StoreDevtoolsConfig,
+} from './config';
 import {
   ComputedState,
   LiftedAction,
@@ -92,4 +97,67 @@ export function sanitizeState(
   stateIdx: number
 ) {
   return stateSanitizer(state, stateIdx);
+}
+
+/**
+ * Read the config and tell if actions should be filtered
+ */
+export function shouldFilterActions(config: StoreDevtoolsConfig) {
+  return config.predicate || config.actionsWhitelist || config.actionsBlacklist;
+}
+
+/**
+ * Return a full filtered lifted state
+ */
+export function filterLiftedState(
+  liftedState: LiftedState,
+  predicate?: Predicate,
+  whitelist?: string[],
+  blacklist?: string[]
+): LiftedState {
+  const filteredStagedActionIds: number[] = [];
+  const filteredActionsById: LiftedActions = {};
+  const filteredComputedStates: ComputedState[] = [];
+  liftedState.stagedActionIds.forEach((id, idx) => {
+    const liftedAction = liftedState.actionsById[id];
+    if (!liftedAction) return;
+    if (
+      idx &&
+      isActionFiltered(
+        liftedState.computedStates[idx],
+        liftedAction,
+        predicate,
+        whitelist,
+        blacklist
+      )
+    ) {
+      return;
+    }
+    filteredActionsById[id] = liftedAction;
+    filteredStagedActionIds.push(id);
+    filteredComputedStates.push(liftedState.computedStates[idx]);
+  });
+  return {
+    ...liftedState,
+    stagedActionIds: filteredStagedActionIds,
+    actionsById: filteredActionsById,
+    computedStates: filteredComputedStates,
+  };
+}
+
+/**
+ * Return true is the action should be ignored
+ */
+export function isActionFiltered(
+  state: any,
+  action: LiftedAction,
+  predicate?: Predicate,
+  whitelist?: string[],
+  blacklist?: string[]
+) {
+  return (
+    (predicate && !predicate(state, action.action)) ||
+    (whitelist && !action.action.type.match(whitelist.join('|'))) ||
+    (blacklist && action.action.type.match(blacklist.join('|')))
+  );
 }
