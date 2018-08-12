1f6a74becb47c2bef52a81529e5749e5973b841c
Marc Redemske <marc.redemske@gmail.com>
2018-08-12 13:18:14 +0200

    fix: dispatch `CloseSidenav` when nav closes
    * This dispatches `CloseSidenav` on closedStart event
    
    IMPORTANT: This does not work properly!
    After unfocusing the sidenav, the element cannot be closed with
    `escape` again, at least on the login screen. On other screens
    it's not working at all.

--- example-app/app/core/components/sidenav.component.ts
+++ example-app/app/core/components/sidenav.component.ts
@@ -1,9 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as fromRoot from '../../reducers';
+import * as LayoutActions from '../actions/layout.actions';
 
 @Component({
   selector: 'bc-sidenav',
   template: `
-    <mat-sidenav [opened]="open">
+    <mat-sidenav [opened]="open" (closedStart)="closeSidenav()">
       <mat-nav-list>
         <ng-content></ng-content>
       </mat-nav-list>
@@ -19,4 +23,9 @@ import { Component, Input } from '@angular/core';
 })
 export class SidenavComponent {
   @Input() open = false;
+  constructor(private store: Store<fromRoot.State>) {}
+
+  closeSidenav() {
+    this.store.dispatch(new LayoutActions.CloseSidenav());
+  }
 }
