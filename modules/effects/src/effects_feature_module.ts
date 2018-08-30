0059e019f4c86a14580bb5b59d498c64afe3a22c
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2018-08-30 20:42:18 +0200

    revert removing optional parameters

--- modules/effects/src/effects_feature_module.ts
+++ modules/effects/src/effects_feature_module.ts
@@ -1,5 +1,5 @@
-import { NgModule, Inject } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { NgModule, Inject, Optional } from '@angular/core';
+import { StoreRootModule, StoreFeatureModule, Store } from '@ngrx/store';
 import { EffectsRootModule } from './effects_root_module';
 import { FEATURE_EFFECTS } from './tokens';
 import { getSourceForInstance } from './effects_metadata';
@@ -15,7 +15,9 @@ export class EffectsFeatureModule {
   constructor(
     root: EffectsRootModule,
     store: Store<any>,
-    @Inject(FEATURE_EFFECTS) effectSourceGroups: any[][]
+    @Inject(FEATURE_EFFECTS) effectSourceGroups: any[][],
+    @Optional() storeRootModule: StoreRootModule,
+    @Optional() storeFeatureModule: StoreFeatureModule
   ) {
     effectSourceGroups.forEach(group => {
       let effectSourceNames: string[] = [];
