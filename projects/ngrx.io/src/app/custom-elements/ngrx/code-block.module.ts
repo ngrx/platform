10eda4e1b08d4123f7cdfb48cc1c3852e83d0708
Brandon Roberts <robertsbt@gmail.com>
2018-09-03 21:30:45 -0500

    Add reworked homepage and links

--- projects/ngrx.io/src/app/custom-elements/ngrx/code-block.module.ts
+++ projects/ngrx.io/src/app/custom-elements/ngrx/code-block.module.ts
@@ -2,12 +2,14 @@ import { NgModule, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeBlockComponent } from './code-block.component';
 import { WithCustomElementComponent } from '../element-registry';
+import { PrettyPrinter } from '../code/pretty-printer.service';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [CodeBlockComponent],
   exports: [CodeBlockComponent],
   entryComponents: [CodeBlockComponent],
+  providers: [PrettyPrinter],
 })
 export class CodeBlockModule implements WithCustomElementComponent {
   customElementComponent: Type<any> = CodeBlockComponent;
