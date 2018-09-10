387bcf1703ca8f3c3fe0beae3f2f37b58c7dafc3
Chris McKnight <cmckni3@gmail.com>
2018-09-10 07:16:14 -0500

    refactor(example): update logger metareducer (#1322)

--- projects/example-app/src/app/reducers/index.ts
+++ projects/example-app/src/app/reducers/index.ts
@@ -45,11 +45,15 @@ export const reducers: ActionReducerMap<State> = {
 
 // console.log all actions
 export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
-  return function(state: State, action: any): State {
-    console.log('state', state);
+  return (state: State, action: any): any => {
+    const result = reducer(state, action);
+    console.groupCollapsed(action.type);
+    console.log('prev state', state);
     console.log('action', action);
+    console.log('next state', result);
+    console.groupEnd();
 
-    return reducer(state, action);
+    return result;
   };
 }
 
