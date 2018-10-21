70031326d0aebf29a41b99a82d62bb03359ab674
timdeschryver <timdeschryver@users.noreply.github.com>
2018-10-21 13:30:11 +0200

    refactor(StoreDevtools): rename recompute action
    The recompute action is renamed to @ngrx/store-devtools/recompute
    This action will now also be exported

--- modules/store-devtools/src/index.ts
+++ modules/store-devtools/src/index.ts
@@ -1,4 +1,4 @@
 export { StoreDevtoolsModule } from './instrument';
-export { LiftedState } from './reducer';
+export { LiftedState, RECOMPUTE } from './reducer';
 export { StoreDevtools } from './devtools';
 export { StoreDevtoolsConfig, StoreDevtoolsOptions } from './config';
