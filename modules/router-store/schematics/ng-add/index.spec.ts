51790c2e5635d2449b5881c89416a7bbbb26cee5
Felix Lemke <flemke.dev@gmail.com>
2018-12-26 18:06:24 +0100

    feat(schematics): implement router store ng-add
    add ng-add schematics, add schema
    implement tests
    
    Closes #1479

---
+++ modules/router-store/schematics/ng-add/index.spec.ts
@@ -0,0 +1,79 @@
+import {
+  SchematicTestRunner,
+  UnitTestTree,
+} from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+import { Schema as RouterStoreOptions } from './schema';
+import {
+  getTestProjectPath,
+  createWorkspace,
+} from '../../../schematics-core/testing';
+
+describe('Router Store ng-add Schematic', () => {
+  const schematicRunner = new SchematicTestRunner(
+    '@ngrx/router-store',
+    path.join(__dirname, '../collection.json')
+  );
+  const defaultOptions: RouterStoreOptions = {
+    skipPackageJson: false,
+    module: 'app',
+  };
+
+  const projectPath = getTestProjectPath();
+
+  let appTree: UnitTestTree;
+
+  beforeEach(() => {
+    appTree = createWorkspace(schematicRunner, appTree);
+  });
+
+  it('should update package.json', () => {
+    const options = { ...defaultOptions };
+
+    const tree = schematicRunner.runSchematic('ng-add', options, appTree);
+    const packageJson = JSON.parse(tree.readContent('/package.json'));
+
+    expect(packageJson.dependencies['@ngrx/router-store']).toBeDefined();
+  });
+
+  it('should skip package.json update', () => {
+    const options = { ...defaultOptions, skipPackageJson: true };
+
+    const tree = schematicRunner.runSchematic('ng-add', options, appTree);
+    const packageJson = JSON.parse(tree.readContent('/package.json'));
+
+    expect(packageJson.dependencies['@ngrx/router-store']).toBeUndefined();
+  });
+
+  it('should be provided by default', () => {
+    const options = { ...defaultOptions };
+
+    const tree = schematicRunner.runSchematic('ng-add', options, appTree);
+    const content = tree.readContent(`${projectPath}/src/app/app.module.ts`);
+    expect(content).toMatch(
+      /import { StoreRouterConnectingModule } from '@ngrx\/router-store';/
+    );
+    expect(content).toMatch(/StoreRouterConnectingModule.forRoot\(\)/);
+  });
+
+  it('should import into a specified module', () => {
+    const options = { ...defaultOptions };
+
+    const tree = schematicRunner.runSchematic('ng-add', options, appTree);
+    const content = tree.readContent(`${projectPath}/src/app/app.module.ts`);
+    expect(content).toMatch(
+      /import { StoreRouterConnectingModule } from '@ngrx\/router-store';/
+    );
+  });
+
+  it('should fail if specified module does not exist', () => {
+    const options = { ...defaultOptions, module: '/src/app/app.moduleXXX.ts' };
+    let thrownError: Error | null = null;
+    try {
+      schematicRunner.runSchematic('ng-add', options, appTree);
+    } catch (err) {
+      thrownError = err;
+    }
+    expect(thrownError).toBeDefined();
+  });
+});
---
+++ modules/router-store/schematics/ng-add/index.ts
@@ -0,0 +1,115 @@
+import {
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  Tree,
+  branchAndMerge,
+  chain,
+  noop,
+} from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import * as ts from 'typescript';
+import {
+  InsertChange,
+  addImportToModule,
+  addPackageToPackageJson,
+  buildRelativePath,
+  findModuleFromOptions,
+  getProjectPath,
+  insertImport,
+  parseName,
+  platformVersion,
+  stringUtils,
+} from '@ngrx/router-store/schematics-core';
+import { Schema as RouterStoreOptions } from './schema';
+
+function addImportToNgModule(options: RouterStoreOptions): Rule {
+  return (host: Tree) => {
+    const modulePath = options.module;
+
+    if (!modulePath) {
+      return host;
+    }
+
+    if (!host.exists(modulePath)) {
+      throw new Error('Specified module does not exist');
+    }
+
+    const text = host.read(modulePath);
+    if (text === null) {
+      throw new SchematicsException(`File ${modulePath} does not exist.`);
+    }
+    const sourceText = text.toString('utf-8');
+
+    const source = ts.createSourceFile(
+      modulePath,
+      sourceText,
+      ts.ScriptTarget.Latest,
+      true
+    );
+
+    const [routerStoreNgModuleImport] = addImportToModule(
+      source,
+      modulePath,
+      `StoreRouterConnectingModule.forRoot()`,
+      `@ngrx/router-store`
+    );
+
+    const changes = [
+      insertImport(
+        source,
+        modulePath,
+        'StoreRouterConnectingModule',
+        '@ngrx/router-store'
+      ),
+      routerStoreNgModuleImport,
+    ];
+    const recorder = host.beginUpdate(modulePath);
+
+    for (const change of changes) {
+      if (change instanceof InsertChange) {
+        recorder.insertLeft(change.pos, change.toAdd);
+      }
+    }
+    host.commitUpdate(recorder);
+
+    return host;
+  };
+}
+
+function addNgRxRouterStoreToPackageJson() {
+  return (host: Tree, context: SchematicContext) => {
+    addPackageToPackageJson(
+      host,
+      'dependencies',
+      '@ngrx/router-store',
+      platformVersion
+    );
+    context.addTask(new NodePackageInstallTask());
+    return host;
+  };
+}
+
+export default function(options: RouterStoreOptions): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    options.path = getProjectPath(host, options);
+
+    if (options.module) {
+      options.module = findModuleFromOptions(host, {
+        name: '',
+        module: options.module,
+        path: options.path,
+      });
+    }
+
+    const parsedPath = parseName(options.path, '');
+    options.path = parsedPath.path;
+
+    return chain([
+      branchAndMerge(chain([addImportToNgModule(options)])),
+      options && options.skipPackageJson
+        ? noop()
+        : addNgRxRouterStoreToPackageJson(),
+    ])(host, context);
+  };
+}
