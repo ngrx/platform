e0ad3c3eee7a2a16c17036139e5d305fb86418e4
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2019-01-20 03:47:24 +0100

    fix(store): don't call the projector function if there are no selectors and props (#1515)
    Closes #1501

--- modules/store/spec/selector.spec.ts
+++ modules/store/spec/selector.spec.ts
@@ -143,6 +143,17 @@ describe('Selectors', () => {
       });
     });
 
+    it('should not short circuit to the projector fn if there are no selectors and props', () => {
+      const projectFn = jasmine.createSpy('projectionFn');
+      const state = { counter: {} };
+
+      const selector = (createSelector(projectFn) as any)(state);
+
+      // the projector still fires but without arguments,
+      // this because there are no selectors and props
+      expect(projectFn).toHaveBeenCalledWith();
+    });
+
     it('should be possible to test a projector fn independent from the selectors it is composed of', () => {
       const projectFn = jasmine.createSpy('projectionFn');
       const selector = createSelector(
