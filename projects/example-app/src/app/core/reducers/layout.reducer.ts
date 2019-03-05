4cfcc08b5d69531dc58891392a867eed0161c1f0
tja4472 <tja2353@gmail.com>
2019-03-05 17:45:36 +0000

    fix(Example): linter problems (#1597)

--- projects/example-app/src/app/core/reducers/layout.reducer.ts
+++ projects/example-app/src/app/core/reducers/layout.reducer.ts
@@ -1,6 +1,5 @@
-import {
-  LayoutActions
-} from '@example-app/core/actions';
+import { Action } from '@ngrx/store';
+import { LayoutActions } from '@example-app/core/actions';
 
 export interface State {
   showSidenav: boolean;
@@ -10,11 +9,10 @@ const initialState: State = {
   showSidenav: false,
 };
 
-export function reducer(
-  state: State = initialState,
-  action: LayoutActions.LayoutActionsUnion
-): State {
-  switch (action.type) {
+export function reducer(state: State = initialState, action: Action): State {
+  const specificAction = action as LayoutActions.LayoutActionsUnion;
+
+  switch (specificAction.type) {
     case LayoutActions.LayoutActionTypes.CloseSidenav:
       return {
         showSidenav: false,
