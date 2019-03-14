3e3548ca458bdc929f4f2f97b6be237dc153da05
ijz953 <john.crowson@capitalone.com>
2019-03-14 11:57:03 -0400

    docs: adding event list component tests

--- projects/ngrx.io/src/app/custom-elements/events/event-list.component.spec.ts
+++ projects/ngrx.io/src/app/custom-elements/events/event-list.component.spec.ts
@@ -1,73 +1,29 @@
-import { ReflectiveInjector } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EventListComponent } from './event-list.component';
-import { EventResponse } from './event.model';
+import { of } from 'rxjs';
+import { EventService } from './event.service';
 
-// Testing the component class behaviors, independent of its template
-// Let e2e tests verify how it displays.
+class TestEventService {
+  upcomingEvents$ = of();
+  pastEvents$ = of();
+}
+
 describe('EventListComponent', () => {
 
+  let fixture: ComponentFixture<EventListComponent>;
   let component: EventListComponent;
-  let injector: ReflectiveInjector;
 
   beforeEach(() => {
-    injector = ReflectiveInjector.resolveAndCreate([
-      EventListComponent
-    ]);
+    TestBed.configureTestingModule({
+      declarations: [ EventListComponent ],
+      providers: [{ provide: EventService, useClass: TestEventService }]
+    });
+
+    fixture = TestBed.createComponent(EventListComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should put each event into the correct bucket and correctly format the date range string', () => {
-    component = getComponent();
-    component.currentDate = new Date('01-02-2019');
-    const mockEvents: EventResponse[] = [
-      {
-        name: 'conf1',
-        url: '',
-        location: '',
-        startDate: '06-28-2018',
-        endDate: '07-01-2018'
-      },
-      {
-        name: 'conf2',
-        url: '',
-        location: '',
-        startDate: '12-25-2018',
-        endDate: '01-01-2019'
-      },
-      {
-        name: 'conf3',
-        url: '',
-        location: '',
-        startDate: '01-01-2019',
-        endDate: '01-03-2019'
-      },
-      {
-        name: 'conf4',
-        url: '',
-        location: '',
-        endDate: '04-01-2019'
-      },
-      {
-        name: 'conf5',
-        url: '',
-        location: '',
-        startDate: '04-02-2019',
-        endDate: '04-02-2019'
-      },
-    ];
-    component.events = mockEvents;
-    expect(component.pastEvents.length).toEqual(2);
-    expect(component.upcomingEvents.length).toEqual(3);
-    expect(component.pastEvents[0].dateRangeString).toEqual('June 28 - July 1, 2018');
-    expect(component.pastEvents[1].dateRangeString).toEqual('December 25, 2018 - January 1, 2019');
-    expect(component.upcomingEvents[0].dateRangeString).toEqual('January 1 - 3, 2019');
-    expect(component.upcomingEvents[1].dateRangeString).toEqual('April 1, 2019');
-    expect(component.upcomingEvents[2].dateRangeString).toEqual('April 2, 2019');
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
-
-
-  //// Test Helpers ////
-  function  getComponent(): EventListComponent {
-    const comp = injector.get(EventListComponent);
-    return comp;
-  }
 });
