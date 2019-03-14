518223b8f773360edd95b11a305d9b6502a1c8f5
ijz953 <john.crowson@capitalone.com>
2019-03-14 12:22:19 -0400

    docs: improving event list tests

--- projects/ngrx.io/src/app/custom-elements/events/event-list.component.spec.ts
+++ projects/ngrx.io/src/app/custom-elements/events/event-list.component.spec.ts
@@ -2,10 +2,31 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EventListComponent } from './event-list.component';
 import { of } from 'rxjs';
 import { EventService } from './event.service';
+import { Event } from './event.model';
+
+const mockUpcomingEvents: Event[] = [
+  {
+    name: 'NG-Rome',
+    url: 'https://ngrome.io/',
+    location: 'Rome, Italy',
+    endDate: new Date('10-07-2019'),
+    dateRangeString: 'October 7, 2019'
+  }
+];
+
+const mockPastEvents: Event[] = [
+  {
+    name: 'DevFestATL',
+    url: 'http://devfestatl.com',
+    location: 'Atlanta, Georgia',
+    endDate: new Date('09-22-2018'),
+    dateRangeString: 'September 22, 2018'
+  }
+];
 
 class TestEventService {
-  upcomingEvents$ = of();
-  pastEvents$ = of();
+  upcomingEvents$ = of(mockUpcomingEvents);
+  pastEvents$ = of(mockPastEvents);
 }
 
 describe('EventListComponent', () => {
@@ -25,5 +46,13 @@ describe('EventListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the upcomingEvents', () => {
+    component.upcomingEvents$.subscribe(events => expect(events).toEqual(mockUpcomingEvents));
+  });
+
+  it('should set the pastEvents', () => {
+    component.pastEvents$.subscribe(events => expect(events).toEqual(mockPastEvents));
   });
 });
