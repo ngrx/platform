cec8f45099822faea1513c8b399d0379394cca66
ijz953 <john.crowson@capitalone.com>
2019-03-17 22:39:53 -0400

    docs: Adding refactored app component tests

--- projects/ngrx.io/src/app/app.component.spec.ts
+++ projects/ngrx.io/src/app/app.component.spec.ts
@@ -5,16 +5,26 @@ import { DocumentService, DocumentContents } from './documents/document.service'
 import { ElementRef, Component, Input } from '@angular/core';
 import { of, Observable, ReplaySubject } from 'rxjs';
 import { LocationService } from './shared/location.service';
-import { NavigationService, CurrentNodes } from './navigation/navigation.service';
+import { NavigationService, CurrentNodes, VersionInfo, NavigationViews } from './navigation/navigation.service';
 import { ScrollService } from './shared/scroll.service';
 import { SearchService } from './search/search.service';
 import { TocService, TocItem } from './shared/toc.service';
 import { MatProgressBarModule, MatIconModule, MatToolbarModule, MatSidenavModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MockSearchService } from 'testing/search.service';
+import { NotificationComponent } from './layout/notification/notification.component';
 
-describe('AppComponent', () => {
+const hideToCBreakPoint = 800;
+
+fdescribe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let searchService: SearchService;
+  let deployment: Deployment;
+  let locationService: LocationService;
+  let locationServiceReplaceSpy: jasmine.Spy;
+  let scrollService: ScrollService;
+  let tocService: TocService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -48,8 +58,9 @@ describe('AppComponent', () => {
         { provide: ElementRef,
           useClass: MockElementRef
         },
-        { provide: LocationService,
-          useClass: MockLocationService
+        {
+          provide: LocationService,
+          useClass: MockLocationService,
         },
         { provide: NavigationService,
           useClass: MockNavigationService
@@ -71,25 +82,191 @@ describe('AppComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    component.notification = { showNotification: 'show' } as NotificationComponent;
+    spyOn(component, 'onResize').and.callThrough();
+    searchService = TestBed.get(SearchService);
+    deployment = TestBed.get(Deployment);
+    locationService = TestBed.get(LocationService);
+    locationServiceReplaceSpy = spyOn(locationService, 'replace');
+    scrollService = TestBed.get(ScrollService);
+    tocService = TestBed.get(TocService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('ngOnInit', () => {
+    it('should set the currentDocument from the DocumentService', () => {
+      expect(component.currentDocument).toEqual({ id: '1', contents: 'contents' });
+    });
+
+    it('should size the window', () => {
+      expect(component.onResize).toHaveBeenCalledTimes(1);
+    });
+
+    it('should initialize the search worker', () => {
+      expect(searchService.initWorker).toHaveBeenCalledWith('app/search/search-worker.js', 2000);
+    });
+
+    describe('archive redirection', () => {
+
+    it('should redirect to docs if we are in archive mode and are not hitting a docs, api, guide, or tutorial page', () => {
+      deployment.mode = 'archive';
+      locationService.currentPath = of('events');
+      component.ngOnInit();
+      expect(locationService.replace).toHaveBeenCalledWith('docs');
+    });
+
+    it('should not redirect to docs if we are hitting a docs page', () => {
+      deployment.mode = 'archive';
+      locationService.currentPath = of('docs');
+      locationServiceReplaceSpy.calls.reset();
+      component.ngOnInit();
+      expect(locationService.replace).not.toHaveBeenCalled();
+    });
+
+    it('should not redirect to docs if we are hitting a api page', () => {
+      deployment.mode = 'archive';
+      locationService.currentPath = of('api');
+      locationServiceReplaceSpy.calls.reset();
+      component.ngOnInit();
+      expect(locationService.replace).not.toHaveBeenCalled();
+    });
+
+    it('should not redirect to docs if we are hitting a guide page', () => {
+      deployment.mode = 'archive';
+      locationService.currentPath = of('guide');
+      locationServiceReplaceSpy.calls.reset();
+      component.ngOnInit();
+      expect(locationService.replace).not.toHaveBeenCalled();
+    });
+
+    it('should not redirect to docs if we are hitting a tutorial page', () => {
+      deployment.mode = 'archive';
+      locationService.currentPath = of('tutorial');
+      locationServiceReplaceSpy.calls.reset();
+      component.ngOnInit();
+      expect(locationService.replace).not.toHaveBeenCalled();
+    });
+  });
+
+    it('should auto scroll if the path changes while on the current page', () => {
+      component.currentPath = 'docs';
+      locationService.currentPath = of('docs');
+      spyOn(scrollService, 'scroll');
+      component.ngOnInit();
+      expect(scrollService.scroll).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not auto scroll if the path changes to a different page', () => {
+      component.currentPath = 'docs';
+      locationService.currentPath = of('events');
+      spyOn(scrollService, 'scroll');
+      component.ngOnInit();
+      expect(scrollService.scroll).not.toHaveBeenCalled();
+    });
+
+    describe('version picker', () => {
+      it('should contain next and stable by default', () => {
+        expect(component.docVersions).toContain({ title: 'next', url: 'https://next.ngrx.io' });
+        expect(component.docVersions).toContain({ title: 'stable (v6.3)', url: 'https://ngrx.io' });
+      });
+
+      it('should add the current version if in archive mode', () => {
+        deployment.mode = 'archive';
+        component.ngOnInit();
+        expect(component.docVersions).toContain({ title: 'v6 (v6.3)'});
+      });
+
+      it('should find the current version by deployment mode and append the raw version info to the title', () => {
+        expect(component.currentDocVersion.title).toBe('stable (v6.3)');
+      });
+
+      it('should find the current version by the current maajor version and append the raw version info to the title', () => {
+        deployment.mode = 'archive';
+        component.ngOnInit();
+        expect(component.currentDocVersion.title).toBe('v6 (v6.3)');
+      });
+    });
+
+    describe('hasFloatingToc', () => {
+      it('should initially be false', () => {
+        const fixture2 = TestBed.createComponent(AppComponent);
+        const component2 = fixture2.componentInstance;
+
+        expect(component2.hasFloatingToc).toBe(false);
+      });
+
+      it('should be false on narrow screens', () => {
+        component.onResize(hideToCBreakPoint - 1);
+
+        tocService.tocList.next([{}, {}, {}] as TocItem[]);
+        expect(component.hasFloatingToc).toBe(false);
+
+        tocService.tocList.next([]);
+        expect(component.hasFloatingToc).toBe(false);
+
+        tocService.tocList.next([{}, {}, {}] as TocItem[]);
+        expect(component.hasFloatingToc).toBe(false);
+      });
+
+      it('should be true on wide screens unless the toc is empty', () => {
+        component.onResize(hideToCBreakPoint + 1);
+
+        tocService.tocList.next([{}, {}, {}] as TocItem[]);
+        expect(component.hasFloatingToc).toBe(true);
+
+        tocService.tocList.next([]);
+        expect(component.hasFloatingToc).toBe(false);
+
+        tocService.tocList.next([{}, {}, {}] as TocItem[]);
+        expect(component.hasFloatingToc).toBe(true);
+      });
+
+      it('should be false when toc is empty', () => {
+        tocService.tocList.next([]);
+
+        component.onResize(hideToCBreakPoint + 1);
+        expect(component.hasFloatingToc).toBe(false);
+
+        component.onResize(hideToCBreakPoint - 1);
+        expect(component.hasFloatingToc).toBe(false);
+
+        component.onResize(hideToCBreakPoint + 1);
+        expect(component.hasFloatingToc).toBe(false);
+      });
+
+      it('should be true when toc is not empty unless the screen is narrow', () => {
+        tocService.tocList.next([{}, {}, {}] as TocItem[]);
+
+        component.onResize(hideToCBreakPoint + 1);
+        expect(component.hasFloatingToc).toBe(true);
+
+        component.onResize(hideToCBreakPoint - 1);
+        expect(component.hasFloatingToc).toBe(false);
+
+        component.onResize(hideToCBreakPoint + 1);
+        expect(component.hasFloatingToc).toBe(true);
+      });
+    });
+  });
+
+  describe('onDocVersionChange', () => {
+    it('should navigate to the new version url', () => {
+      component.docVersions = [
+        { title: 'next', url: 'https://next.ngrx.io'},
+        { title: 'stable (v6.3)', url: 'https://ngrx.io' }
+      ];
+      spyOn(locationService, 'go');
+      component.onDocVersionChange(1);
+      expect(locationService.go).toHaveBeenCalledTimes(1);
+    });
+  });
 });
 
-class MockDeployment {
-  mode = 'archive';
-}
-
-class MockDocumentService {
-  currentDocument: Observable<DocumentContents> = of({ id: '1', contents: 'contents' });
-}
-
-class MockElementRef {
-  nativeElement: Element;
-}
+// Mock Dependencies
 
 class MockLocationService {
   currentPath = of('path');
@@ -100,10 +277,29 @@ class MockLocationService {
   search = () => {};
 }
 
+
+class MockDeployment {
+  mode = 'stable';
+}
+
+class MockDocumentService {
+  currentDocument: Observable<DocumentContents> = of({ id: '1', contents: 'contents' });
+}
+
+class MockElementRef {
+  nativeElement: Element;
+}
+
 class MockNavigationService {
   currentNodes: Observable<CurrentNodes> = of({ 'view': { url: 'path', view: 'view', nodes: []}})
-  versionInfo = of();
-  navigationViews = of();
+  versionInfo: Observable<VersionInfo> = of(<VersionInfo>{ major: 6, raw: '6.3'});
+  navigationViews: Observable<NavigationViews> = of({ 'docVersions' : [{ title: 'v5'}]});
+}
+
+export class MockTocService {
+  genToc = jasmine.createSpy('TocService#genToc');
+  reset = jasmine.createSpy('TocService#reset');
+  tocList = new ReplaySubject<TocItem[]>(1);
 }
 
 class MockScrollService {
@@ -111,14 +307,7 @@ class MockScrollService {
   scrollToTop = () => {};
 }
 
-class MockSearchService {
-  initWorker = () => of(true);
-  search = () => of();
-}
-
-class MockTocService {
-  tocList = new ReplaySubject<TocItem[]>(1);
-}
+// Mock Child Components
 
 @Component({
   selector: 'aio-notification',
@@ -126,6 +315,7 @@ class MockTocService {
 })
 class MockAioNotificationComponent {
   @Input() dismissOnContentClick;
+  showNotification = 'show'
 }
 
 @Component({
