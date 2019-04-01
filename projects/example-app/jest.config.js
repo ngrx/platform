24711ca16c58c9d2e4391d012585c4f280fb0c43
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2019-04-01 20:27:59 +0200

    chore(example): update jest dependencies and configuration (#1680)
    Closes #1674

--- projects/example-app/jest.config.js
+++ projects/example-app/jest.config.js
@@ -1,19 +1,21 @@
 module.exports = {
   rootDir: '.',
-  setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
   globals: {
     'ts-jest': {
-      tsConfigFile: 'projects/example-app/tsconfig.spec.json',
+      tsConfig: 'projects/example-app/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html?$',
+      astTransformers: [
+        require.resolve('jest-preset-angular/InlineHtmlStripStylesTransformer'),
+      ],
     },
-    __TRANSFORM_HTML__: true,
   },
   transform: {
-    '^.+\\.(ts|js|html)$':
-      '<rootDir>/../../node_modules/jest-preset-angular/preprocessor.js',
+    '^.+\\.(ts|js|html)$': 'ts-jest',
   },
   testMatch: ['<rootDir>/**/*.spec.ts'],
-  moduleFileExtensions: ['ts', 'js', 'html', 'json'],
-  mapCoverage: true,
+  testEnvironment: 'jest-environment-jsdom-thirteen',
+  moduleFileExtensions: ['html', 'js', 'json', 'ts'],
   coveragePathIgnorePatterns: ['/node_modules/', '/modules/*.*/'],
   moduleNameMapper: {
     '^@ngrx/(.*)': '<rootDir>/../../modules/$1',
@@ -21,4 +23,9 @@ module.exports = {
   },
   transformIgnorePatterns: ['node_modules/(?!@ngrx)'],
   modulePathIgnorePatterns: ['dist'],
+  preset: 'jest-preset-angular',
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js',
+  ],
 };
