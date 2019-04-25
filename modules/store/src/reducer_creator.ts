63e49261c21a04f315378ceb3b264020348a9791
Mike Ryan <mike.ryan52@gmail.com>
2019-04-24 21:14:58 -0500

    feat(store): expand createReducer type signature to support up to ten action creators (#1803)

--- modules/store/src/reducer_creator.ts
+++ modules/store/src/reducer_creator.ts
@@ -32,6 +32,139 @@ export function on<
   creator3: C3,
   reducer: OnReducer<S, [C1, C2, C3]>
 ): On<S>;
+export function on<
+  C1 extends ActionCreator,
+  C2 extends ActionCreator,
+  C3 extends ActionCreator,
+  C4 extends ActionCreator,
+  S
+>(
+  creator1: C1,
+  creator2: C2,
+  creator3: C3,
+  creator4: C4,
+  reducer: OnReducer<S, [C1, C2, C3, C4]>
+): On<S>;
+export function on<
+  C1 extends ActionCreator,
+  C2 extends ActionCreator,
+  C3 extends ActionCreator,
+  C4 extends ActionCreator,
+  C5 extends ActionCreator,
+  S
+>(
+  creator1: C1,
+  creator2: C2,
+  creator3: C3,
+  creator4: C4,
+  creator5: C5,
+  reducer: OnReducer<S, [C1, C2, C3, C4, C5]>
+): On<S>;
+export function on<
+  C1 extends ActionCreator,
+  C2 extends ActionCreator,
+  C3 extends ActionCreator,
+  C4 extends ActionCreator,
+  C5 extends ActionCreator,
+  C6 extends ActionCreator,
+  S
+>(
+  creator1: C1,
+  creator2: C2,
+  creator3: C3,
+  creator4: C4,
+  creator5: C5,
+  creator6: C6,
+  reducer: OnReducer<S, [C1, C2, C3, C4, C5, C6]>
+): On<S>;
+export function on<
+  C1 extends ActionCreator,
+  C2 extends ActionCreator,
+  C3 extends ActionCreator,
+  C4 extends ActionCreator,
+  C5 extends ActionCreator,
+  C6 extends ActionCreator,
+  C7 extends ActionCreator,
+  S
+>(
+  creator1: C1,
+  creator2: C2,
+  creator3: C3,
+  creator4: C4,
+  creator5: C5,
+  creator6: C6,
+  creator7: C7,
+  reducer: OnReducer<S, [C1, C2, C3, C4, C5, C6, C7]>
+): On<S>;
+export function on<
+  C1 extends ActionCreator,
+  C2 extends ActionCreator,
+  C3 extends ActionCreator,
+  C4 extends ActionCreator,
+  C5 extends ActionCreator,
+  C6 extends ActionCreator,
+  C7 extends ActionCreator,
+  C8 extends ActionCreator,
+  S
+>(
+  creator1: C1,
+  creator2: C2,
+  creator3: C3,
+  creator4: C4,
+  creator5: C5,
+  creator6: C6,
+  creator7: C7,
+  creator8: C8,
+  reducer: OnReducer<S, [C1, C2, C3, C4, C5, C6, C7, C8]>
+): On<S>;
+export function on<
+  C1 extends ActionCreator,
+  C2 extends ActionCreator,
+  C3 extends ActionCreator,
+  C4 extends ActionCreator,
+  C5 extends ActionCreator,
+  C6 extends ActionCreator,
+  C7 extends ActionCreator,
+  C8 extends ActionCreator,
+  C9 extends ActionCreator,
+  S
+>(
+  creator1: C1,
+  creator2: C2,
+  creator3: C3,
+  creator4: C4,
+  creator5: C5,
+  creator6: C6,
+  creator7: C7,
+  creator8: C8,
+  creator9: C9,
+  reducer: OnReducer<S, [C1, C2, C3, C4, C5, C6, C7, C8, C9]>
+): On<S>;
+export function on<
+  C1 extends ActionCreator,
+  C2 extends ActionCreator,
+  C3 extends ActionCreator,
+  C4 extends ActionCreator,
+  C5 extends ActionCreator,
+  C6 extends ActionCreator,
+  C7 extends ActionCreator,
+  C8 extends ActionCreator,
+  C9 extends ActionCreator,
+  C10 extends ActionCreator,
+  S
+>(
+  creator1: C1,
+  creator2: C2,
+  creator3: C3,
+  creator4: C4,
+  creator5: C5,
+  creator6: C6,
+  creator7: C7,
+  creator8: C8,
+  creator9: C9,
+  creator10: C10,
+  reducer: OnReducer<S, [C1, C2, C3, C4, C5, C6, C7, C8, C9, C10]>
+): On<S>;
 export function on<S>(
   creator: ActionCreator,
   ...rest: (ActionCreator | OnReducer<S, [ActionCreator]>)[]
