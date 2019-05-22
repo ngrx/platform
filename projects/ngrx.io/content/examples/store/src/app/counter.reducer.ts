23c7c752e0ef0960f3e27ed939d093e39a09977b
Santosh Yadav <santosh.yadav198613@gmail.com>
2019-05-23 02:43:06 +0530

    docs: update docs to use createReducer() function (#1856)
    Closes #1853

--- projects/ngrx.io/content/examples/store/src/app/counter.reducer.ts
+++ projects/ngrx.io/content/examples/store/src/app/counter.reducer.ts
@@ -1,21 +1,11 @@
 // #docregion
-import { Action } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { increment, decrement, reset } from './counter.actions';
 
 export const initialState = 0;
 
-export function counterReducer(state = initialState, action: Action) {
-  switch (action.type) {
-    case increment.type:
-      return state + 1;
-
-    case decrement.type:
-      return state - 1;
-
-    case reset.type:
-      return 0;
-
-    default:
-      return state;
-  }
-}
+export const counterReducer = createReducer(initialState,
+  on(increment, state => state + 1),
+  on(decrement, state => state - 1),
+  on(reset, state => 0),
+);
