5a09844758de6a101c0f376cc3fa32df3f6c11a4
Brandon <robertsbt@gmail.com>
2019-06-03 08:33:33 -0500

    ci: add scripts to automate publishing of releases, and deployment of docs (#1903)
    Closes #1555

---
+++ build/publish-latest.ts
@@ -0,0 +1,36 @@
+import * as util from './util';
+import { createBuilder } from './util';
+import { Config, packages } from './config';
+import * as shelljs from 'shelljs';
+
+/**
+ * Publish release to NPM on "latest" tag
+ */
+export async function publishToNpm(config: Config) {
+  for (let pkg of util.getTopLevelPackages(config)) {
+    console.log(`Publishing @ngrx/${pkg}`);
+
+    const cmd = [
+      './node_modules/.bin/bazel run',
+      `//modules/${pkg}:npm_package.publish`,
+      '--',
+      '--access=public',
+      '--tag=latest',
+      '--dry-run',
+    ];
+
+    shelljs.exec(cmd.join(' '));
+  }
+}
+
+const publishLatest = createBuilder([
+  ['Publish packages on latest', publishToNpm],
+]);
+
+publishLatest({
+  scope: '@ngrx',
+  packages,
+}).catch(err => {
+  console.error(err);
+  process.exit(1);
+});
