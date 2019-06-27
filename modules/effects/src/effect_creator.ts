b028ae585393c07cfdb1369400f837ae6f46dda3
ijz953 <john.crowson@capitalone.com>
2019-06-27 15:33:38 -0400

    docs: add docs for createEffect api

--- modules/effects/src/effect_creator.ts
+++ modules/effects/src/effect_creator.ts
@@ -5,10 +5,43 @@ import { EffectMetadata, EffectConfig } from './models';
 const CREATE_EFFECT_METADATA_KEY = '__@ngrx/effects_create__';
 
 type DispatchType<T> = T extends { dispatch: infer U } ? U : unknown;
+type ReturnType<T> = T extends false ? Observable<unknown> : Observable<Action>;
+/**
+ * @description
+ * Creates an effect from an `Observable` and an `EffectConfig`.
+ *
+ * @param source A function which returns an `Observable`.
+ * @param config A partial `EffectConfig` to configure the effect.  By default, `dispatch` is true and `resubscribeOnError` is true.
+ * @returns If `EffectConfig`.`dispatch` is true, returns `Observable<Action>`.  Else, returns `Observable<unknown>`.
+ *
+ * @usageNotes
+ *
+ * ** Mapping to a different action **
+ * ```ts
+ * effectName$ = createEffect(
+ *   () => this.actions$.pipe(
+ *     ofType(FeatureActions.actionOne),
+ *     map(() => FeatureActions.actionTwo())
+ *   )
+ * );
+ * ```
+ *
+ *  ** Non-dispatching effects **
+ * ```ts
+ * effectName$ = createEffect(
+ *   () => this.actions$.pipe(
+ *     ofType(FeatureActions.actionOne),
+ *     tap(() => console.log('Action One Dispatched'))
+ *   ),
+ *   { dispatch: false }
+ *   // FeatureActions.actionOne is not dispatched
+ * );
+ * ```
+ */
 export function createEffect<
   C extends EffectConfig,
   T extends DispatchType<C>,
-  O extends T extends false ? Observable<unknown> : Observable<Action>,
+  O extends ReturnType<T>,
   R extends O | ((...args: any[]) => O)
 >(source: () => R, config?: Partial<C>): R {
   const effect = source();
