4e0299196d41ec29848520d8887c7f398211ed75
ijz953 <john.crowson@capitalone.com>
2019-06-27 15:34:10 -0400

    feat(effects): export EffectConfig and add docs

--- modules/effects/src/models.ts
+++ modules/effects/src/models.ts
@@ -1,5 +1,15 @@
+/**
+ * Configures an effect creator.  See `createEffect`.
+ */
 export interface EffectConfig {
+  /**
+   * Determines if the action emitted by the effect is dispatched to the store.
+   * If false, effect does not need to return type `Observable<Action>`.
+   */
   dispatch?: boolean;
+  /**
+   * Determines if the effect will be resubscribed if an error occurs in the main actions stream.
+   */
   resubscribeOnError?: boolean;
 }
 
