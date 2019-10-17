ngrx/platform

commit 53832a154aae2f5e23e5afee8f79710ffd7b54de

feat(Store): createSelector allow props in selector

modules/store/src/store.ts
@@ -109,11 +109,13 @@ export class Store<T> extends Observable<T> implements Observer<Action> {
 
 export const STORE_PROVIDERS: Provider[] = [Store];
 
-export function select<T, K>(
-  mapFn: (state: T) => K
+export function select<T, Props, K>(
+  mapFn: (state: T, props: Props) => K,
+  props?: Props
 ): (source$: Observable<T>) => Observable<K>;
 export function select<T, a extends keyof T>(
-  key: a
+  key: a,
+  props: null
 ): (source$: Observable<T>) => Observable<T[a]>;
 export function select<T, a extends keyof T, b extends keyof T[a]>(
   key1: a,
@@ -175,20 +177,25 @@ export function select<
  * This overload is used to support spread operator with
  * fixed length tuples type in typescript 2.7
  */
-export function select<T, K = any>(
+export function select<T, Props = any, K = any>(
+  propsOrPath: Props,
   ...paths: string[]
 ): (source$: Observable<T>) => Observable<K>;
-export function select<T, K>(
-  pathOrMapFn: ((state: T) => any) | string,
+export function select<T, Props, K>(
+  pathOrMapFn: ((state: T, props?: Props) => any) | string,
+  propsOrPath: Props | string,
   ...paths: string[]
 ) {
   return function selectOperator(source$: Observable<T>): Observable<K> {
     let mapped$: Observable<any>;
 
     if (typeof pathOrMapFn === 'string') {
-      mapped$ = source$.pipe(pluck(pathOrMapFn, ...paths));
+      const pathSlices = [<string>propsOrPath, ...paths].filter(Boolean);
+      mapped$ = source$.pipe(pluck(pathOrMapFn, ...pathSlices));
     } else if (typeof pathOrMapFn === 'function') {
-      mapped$ = source$.pipe(map(pathOrMapFn));
+      mapped$ = source$.pipe(
+        map(source => pathOrMapFn(source, <Props>propsOrPath))
+      );
     } else {
       throw new TypeError(
         `Unexpected type '${typeof pathOrMapFn}' in select operator,` +

modules/store/spec/integration.spec.ts
@@ -5,6 +5,8 @@ import {
   select,
   Store,
   StoreModule,
+  createFeatureSelector,
+  createSelector,
 } from '@ngrx/store';
 import { combineLatest } from 'rxjs';
 import { first } from 'rxjs/operators';
@@ -172,6 +174,42 @@ describe('ngRx Integration spec', () => {
       });
 
       expect(currentlyVisibleTodos.length).toBe(0);
+    });
+
+    it('should use props to get a todo', () => {
+      const getTodosState = createFeatureSelector<TodoAppSchema, Todo[]>(
+        'todos'
+      );
+      const getTodos = createSelector(getTodosState, todos => todos);
+      const getTodosById = createSelector(
+        getTodos,
+        (state: TodoAppSchema, id: number) => id,
+        (todos, id) => todos.find(todo => todo.id === id)
+      );
+
+      let testCase = 1;
+      const todo$ = store.pipe(select(getTodosById, 2));
+      todo$.subscribe(todo => {
+        if (testCase === 1) {
+          expect(todo).toEqual(undefined);
+        } else if (testCase === 2) {
+          expect(todo).toEqual({
+            id: 2,
+            text: 'second todo',
+            completed: false,
+          });
+        } else if (testCase === 3) {
+          expect(todo).toEqual({ id: 2, text: 'second todo', completed: true });
+        }
+        testCase++;
+      });
+
+      store.dispatch({ type: ADD_TODO, payload: { text: 'first todo' } });
+      store.dispatch({ type: ADD_TODO, payload: { text: 'second todo' } });
+      store.dispatch({
+        type: COMPLETE_TODO,
+        payload: { id: 2 },
+      });
     });
   });
 

modules/store/spec/selector.spec.ts
@@ -125,6 +125,105 @@ describe('Selectors', () => {
     });
   });
 
+  describe('createSelector with props', () => {
+    it('should deliver the value of selectors to the projection function', () => {
+      const projectFn = jasmine.createSpy('projectionFn');
+
+      const selector = createSelector(
+        incrementOne,
+        incrementTwo,
+        (state: any, props: any) => props.value,
+        projectFn
+      );
+
+      selector({}, { value: 47 });
+      expect(projectFn).toHaveBeenCalledWith(countOne, countTwo, 47);
+    });
+
+    it('should be possible to test a projector fn independent from the selectors it is composed of', () => {
+      const projectFn = jasmine.createSpy('projectionFn');
+      const selector = createSelector(
+        incrementOne,
+        incrementTwo,
+        (state: any, props: any) => {
+          fail(`Shouldn't be called`);
+          return props.value;
+        },
+        projectFn
+      );
+      selector.projector('', '', 47);
+
+      expect(incrementOne).not.toHaveBeenCalled();
+      expect(incrementTwo).not.toHaveBeenCalled();
+      expect(projectFn).toHaveBeenCalledWith('', '', 47);
+    });
+
+    it('should call the projector function when the state changes', () => {
+      const projectFn = jasmine.createSpy('projectionFn');
+      const selector = createSelector(
+        incrementOne,
+        (state: any, props: any) => props.value,
+        projectFn
+      );
+
+      const firstSate = { first: 'state' };
+      const props = { foo: 'props' };
+      selector(firstSate, props);
+      selector(firstSate, props);
+      expect(projectFn).toHaveBeenCalledTimes(1);
+
+      const secondState = { second: 'state' };
+      selector(secondState, props);
+      expect(projectFn).toHaveBeenCalledTimes(2);
+    });
+
+    it('should memoize the function', () => {
+      let counter = 0;
+
+      const firstState = { first: 'state' };
+      const secondState = { second: 'state' };
+      const props = { foo: 'props' };
+
+      const projectFn = jasmine.createSpy('projectionFn');
+      const selector = createSelector(
+        incrementOne,
+        incrementTwo,
+        (state: any, props: any) => {
+          counter++;
+          return props;
+        },
+        projectFn
+      );
+
+      selector(firstState, props);
+      selector(firstState, props);
+      selector(firstState, props);
+      selector(secondState, props);
+
+      expect(counter).toBe(2);
+      expect(projectFn).toHaveBeenCalledTimes(2);
+    });
+
+    it('should allow you to release memoized arguments', () => {
+      const state = { first: 'state' };
+      const props = { foo: 'props' };
+      const projectFn = jasmine.createSpy('projectionFn');
+      const selector = createSelector(
+        incrementOne,
+        (state: any, props: any) => props,
+        projectFn
+      );
+
+      selector(state, props);
+      selector(state, props);
+      selector.release();
+      selector(state, props);
+      selector(state, props);
+
+      expect(projectFn).toHaveBeenCalledTimes(2);
+    });
+  });
+
   describe('createSelector with arrays', () => {
     it('should deliver the value of selectors to the projection function', () => {
       const projectFn = jasmine.createSpy('projectionFn');
@@ -208,6 +307,104 @@ describe('Selectors', () => {
 
       expect(grandparent.release).toHaveBeenCalled();
       expect(parent.release).toHaveBeenCalled();
+    });
+  });
+
+  describe('createSelector with arrays and props', () => {
+    it('should deliver the value of selectors to the projection function', () => {
+      const projectFn = jasmine.createSpy('projectionFn');
+      const selector = createSelector(
+        [incrementOne, incrementTwo, (state: any, props: any) => props.value],
+        projectFn
+      )({}, { value: 47 });
+
+      expect(projectFn).toHaveBeenCalledWith(countOne, countTwo, 47);
+    });
+
+    it('should be possible to test a projector fn independent from the selectors it is composed of', () => {
+      const projectFn = jasmine.createSpy('projectionFn');
+      const selector = createSelector(
+        [
+          incrementOne,
+          incrementTwo,
+          (state: any, props: any) => {
+            fail(`Shouldn't be called`);
+            return props.value;
+          },
+        ],
+        projectFn
+      );
+
+      selector.projector('', '', 47);
+
+      expect(incrementOne).not.toHaveBeenCalled();
+      expect(incrementTwo).not.toHaveBeenCalled();
+      expect(projectFn).toHaveBeenCalledWith('', '', 47);
+    });
+
+    it('should call the projector function when the state changes', () => {
+      const projectFn = jasmine.createSpy('projectionFn');
+      const selector = createSelector(
+        [incrementOne, (state: any, props: any) => props.value],
+        projectFn
+      );
+
+      const firstSate = { first: 'state' };
+      const props = { foo: 'props' };
+      selector(firstSate, props);
+      selector(firstSate, props);
+      expect(projectFn).toHaveBeenCalledTimes(1);
+
+      const secondState = { second: 'state' };
+      selector(secondState, props);
+      expect(projectFn).toHaveBeenCalledTimes(2);
+    });
+
+    it('should memoize the function', () => {
+      let counter = 0;
+
+      const firstState = { first: 'state' };
+      const secondState = { second: 'state' };
+      const props = { foo: 'props' };
+
+      const projectFn = jasmine.createSpy('projectionFn');
+      const selector = createSelector(
+        [
+          incrementOne,
+          incrementTwo,
+          (state: any, props: any) => {
+            counter++;
+            return props;
+          },
+        ],
+        projectFn
+      );
+
+      selector(firstState, props);
+      selector(firstState, props);
+      selector(firstState, props);
+      selector(secondState, props);
+
+      expect(counter).toBe(2);
+      expect(projectFn).toHaveBeenCalledTimes(2);
+    });
+
+    it('should allow you to release memoized arguments', () => {
+      const state = { first: 'state' };
+      const props = { foo: 'props' };
+      const projectFn = jasmine.createSpy('projectionFn');
+      const selector = createSelector(
+        [incrementOne, (state: any, props: any) => props],
+        projectFn
+      );
+
+      selector(state, props);
+      selector(state, props);
+      selector.release();
+      selector(state, props);
+      selector(state, props);
+
+      expect(projectFn).toHaveBeenCalledTimes(2);
     });
   });
 

modules/store/src/models.ts
@@ -33,6 +33,9 @@ export interface StoreFeature<T, V extends Action = Action> {
   metaReducers?: MetaReducer<T, V>[];
 }
 
-export interface Selector<T, V> {
-  (state: T): V;
-}
+export type Selector<T, V> = (state: T) => V;
+
+export type SelectorWithProps<State, Props, Result> = (
+  state: State,
+  props: Props
+) => Result;

modules/store/src/selector.ts
@@ -1,4 +1,4 @@
-import { Selector } from './models';
+import { Selector, SelectorWithProps } from './models';
 
 export type AnyFn = (...args: any[]) => any;
 
@@ -10,6 +10,12 @@ export type ComparatorFn = (a: any, b: any) => boolean;
 
 export interface MemoizedSelector<State, Result>
   extends Selector<State, Result> {
+  release(): void;
+  projector: AnyFn;
+}
+
+export interface MemoizedSelectorWithProps<State, Props, Result>
+  extends SelectorWithProps<State, Props, Result> {
   release(): void;
   projector: AnyFn;
 }
@@ -80,31 +86,68 @@ export function defaultMemoize(
 
 export function createSelector<State, S1, Result>(
   s1: Selector<State, S1>,
-  projector: (S1: S1) => Result
+  projector: (s1: S1) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, Result>(
+  s1: SelectorWithProps<State, Props, S1>,
+  projector: (s1: S1) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
 export function createSelector<State, S1, Result>(
   selectors: [Selector<State, S1>],
   projector: (s1: S1) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, Result>(
+  selectors: [SelectorWithProps<State, Props, S1>],
+  projector: (s1: S1) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
+
 export function createSelector<State, S1, S2, Result>(
   s1: Selector<State, S1>,
   s2: Selector<State, S2>,
   projector: (s1: S1, s2: S2) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, S2, Result>(
+  s1: SelectorWithProps<State, Props, S1>,
+  s2: SelectorWithProps<State, Props, S2>,
+  projector: (s1: S1, s2: S2) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
 export function createSelector<State, S1, S2, Result>(
   selectors: [Selector<State, S1>, Selector<State, S2>],
   projector: (s1: S1, s2: S2) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, S2, Result>(
+  selectors: [
+    SelectorWithProps<State, Props, S1>,
+    SelectorWithProps<State, Props, S2>
+  ],
+  projector: (s1: S1, s2: S2) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
+
 export function createSelector<State, S1, S2, S3, Result>(
   s1: Selector<State, S1>,
   s2: Selector<State, S2>,
   s3: Selector<State, S3>,
   projector: (s1: S1, s2: S2, s3: S3) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, S2, S3, Result>(
+  s1: SelectorWithProps<State, Props, S1>,
+  s2: SelectorWithProps<State, Props, S2>,
+  s3: SelectorWithProps<State, Props, S3>,
+  projector: (s1: S1, s2: S2, s3: S3) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
 export function createSelector<State, S1, S2, S3, Result>(
   selectors: [Selector<State, S1>, Selector<State, S2>, Selector<State, S3>],
   projector: (s1: S1, s2: S2, s3: S3) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, S2, S3, Result>(
+  selectors: [
+    SelectorWithProps<State, Props, S1>,
+    SelectorWithProps<State, Props, S2>,
+    SelectorWithProps<State, Props, S3>
+  ],
+  projector: (s1: S1, s2: S2, s3: S3) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
+
 export function createSelector<State, S1, S2, S3, S4, Result>(
   s1: Selector<State, S1>,
   s2: Selector<State, S2>,
@@ -112,6 +155,13 @@ export function createSelector<State, S1, S2, S3, S4, Result>(
   s4: Selector<State, S4>,
   projector: (s1: S1, s2: S2, s3: S3, s4: S4) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, S2, S3, S4, Result>(
+  s1: SelectorWithProps<State, Props, S1>,
+  s2: SelectorWithProps<State, Props, S2>,
+  s3: SelectorWithProps<State, Props, S3>,
+  s4: SelectorWithProps<State, Props, S4>,
+  projector: (s1: S1, s2: S2, s3: S3, s4: S4) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
 export function createSelector<State, S1, S2, S3, S4, Result>(
   selectors: [
     Selector<State, S1>,
@@ -121,6 +171,16 @@ export function createSelector<State, S1, S2, S3, S4, Result>(
   ],
   projector: (s1: S1, s2: S2, s3: S3, s4: S4) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, S2, S3, S4, Result>(
+  selectors: [
+    SelectorWithProps<State, Props, S1>,
+    SelectorWithProps<State, Props, S2>,
+    SelectorWithProps<State, Props, S3>,
+    SelectorWithProps<State, Props, S4>
+  ],
+  projector: (s1: S1, s2: S2, s3: S3, s4: S4) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
+
 export function createSelector<State, S1, S2, S3, S4, S5, Result>(
   s1: Selector<State, S1>,
   s2: Selector<State, S2>,
@@ -129,6 +189,14 @@ export function createSelector<State, S1, S2, S3, S4, S5, Result>(
   s5: Selector<State, S5>,
   projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, S2, S3, S4, S5, Result>(
+  s1: SelectorWithProps<State, Props, S1>,
+  s2: SelectorWithProps<State, Props, S2>,
+  s3: SelectorWithProps<State, Props, S3>,
+  s4: SelectorWithProps<State, Props, S4>,
+  s5: SelectorWithProps<State, Props, S5>,
+  projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
 export function createSelector<State, S1, S2, S3, S4, S5, Result>(
   selectors: [
     Selector<State, S1>,
@@ -139,6 +207,17 @@ export function createSelector<State, S1, S2, S3, S4, S5, Result>(
   ],
   projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, S2, S3, S4, S5, Result>(
+  selectors: [
+    SelectorWithProps<State, Props, S1>,
+    SelectorWithProps<State, Props, S2>,
+    SelectorWithProps<State, Props, S3>,
+    SelectorWithProps<State, Props, S4>,
+    SelectorWithProps<State, Props, S5>
+  ],
+  projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
+
 export function createSelector<State, S1, S2, S3, S4, S5, S6, Result>(
   s1: Selector<State, S1>,
   s2: Selector<State, S2>,
@@ -148,6 +227,15 @@ export function createSelector<State, S1, S2, S3, S4, S5, S6, Result>(
   s6: Selector<State, S6>,
   projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5, s6: S6) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, S2, S3, S4, S5, S6, Result>(
+  s1: SelectorWithProps<State, Props, S1>,
+  s2: SelectorWithProps<State, Props, S2>,
+  s3: SelectorWithProps<State, Props, S3>,
+  s4: SelectorWithProps<State, Props, S4>,
+  s5: SelectorWithProps<State, Props, S5>,
+  s6: SelectorWithProps<State, Props, S6>,
+  projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5, s6: S6) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
 export function createSelector<State, S1, S2, S3, S4, S5, S6, Result>(
   selectors: [
     Selector<State, S1>,
@@ -159,6 +247,18 @@ export function createSelector<State, S1, S2, S3, S4, S5, S6, Result>(
   ],
   projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5, s6: S6) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<State, Props, S1, S2, S3, S4, S5, S6, Result>(
+  selectors: [
+    SelectorWithProps<State, Props, S1>,
+    SelectorWithProps<State, Props, S2>,
+    SelectorWithProps<State, Props, S3>,
+    SelectorWithProps<State, Props, S4>,
+    SelectorWithProps<State, Props, S5>,
+    SelectorWithProps<State, Props, S6>
+  ],
+  projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5, s6: S6) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
+
 export function createSelector<State, S1, S2, S3, S4, S5, S6, S7, Result>(
   s1: Selector<State, S1>,
   s2: Selector<State, S2>,
@@ -169,6 +269,27 @@ export function createSelector<State, S1, S2, S3, S4, S5, S6, S7, Result>(
   s7: Selector<State, S7>,
   projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5, s6: S6, s7: S7) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<
+  State,
+  Props,
+  S1,
+  S2,
+  S3,
+  S4,
+  S5,
+  S6,
+  S7,
+  Result
+>(
+  s1: SelectorWithProps<State, Props, S1>,
+  s2: SelectorWithProps<State, Props, S2>,
+  s3: SelectorWithProps<State, Props, S3>,
+  s4: SelectorWithProps<State, Props, S4>,
+  s5: SelectorWithProps<State, Props, S5>,
+  s6: SelectorWithProps<State, Props, S6>,
+  s7: SelectorWithProps<State, Props, S7>,
+  projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5, s6: S6, s7: S7) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
 export function createSelector<State, S1, S2, S3, S4, S5, S6, S7, Result>(
   selectors: [
     Selector<State, S1>,
@@ -181,6 +302,30 @@ export function createSelector<State, S1, S2, S3, S4, S5, S6, S7, Result>(
   ],
   projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5, s6: S6, s7: S7) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<
+  State,
+  Props,
+  S1,
+  S2,
+  S3,
+  S4,
+  S5,
+  S6,
+  S7,
+  Result
+>(
+  selectors: [
+    SelectorWithProps<State, Props, S1>,
+    SelectorWithProps<State, Props, S2>,
+    SelectorWithProps<State, Props, S3>,
+    SelectorWithProps<State, Props, S4>,
+    SelectorWithProps<State, Props, S5>,
+    SelectorWithProps<State, Props, S6>,
+    SelectorWithProps<State, Props, S7>
+  ],
+  projector: (s1: S1, s2: S2, s3: S3, s4: S4, s5: S5, s6: S6, s7: S7) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
+
 export function createSelector<State, S1, S2, S3, S4, S5, S6, S7, S8, Result>(
   s1: Selector<State, S1>,
   s2: Selector<State, S2>,
@@ -201,6 +346,38 @@ export function createSelector<State, S1, S2, S3, S4, S5, S6, S7, S8, Result>(
     s8: S8
   ) => Result
 ): MemoizedSelector<State, Result>;
+export function createSelector<
+  State,
+  Props,
+  S1,
+  S2,
+  S3,
+  S4,
+  S5,
+  S6,
+  S7,
+  S8,
+  Result
+>(
+  s1: SelectorWithProps<State, Props, S1>,
+  s2: SelectorWithProps<State, Props, S2>,
+  s3: SelectorWithProps<State, Props, S3>,
+  s4: SelectorWithProps<State, Props, S4>,
+  s5: SelectorWithProps<State, Props, S5>,
+  s6: SelectorWithProps<State, Props, S6>,
+  s7: SelectorWithProps<State, Props, S7>,
+  s8: SelectorWithProps<State, Props, S8>,
+  projector: (
+    s1: S1,
+    s2: S2,
+    s3: S3,
+    s4: S4,
+    s5: S5,
+    s6: S6,
+    s7: S7,
+    s8: S8
+  ) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
 export function createSelector<State, S1, S2, S3, S4, S5, S6, S7, S8, Result>(
   selectors: [
     Selector<State, S1>,
@@ -223,23 +400,61 @@ export function createSelector<State, S1, S2, S3, S4, S5, S6, S7, S8, Result>(
     s8: S8
   ) => Result
 ): MemoizedSelector<State, Result>;
-export function createSelector(...input: any[]) {
+export function createSelector<
+  State,
+  Props,
+  S1,
+  S2,
+  S3,
+  S4,
+  S5,
+  S6,
+  S7,
+  S8,
+  Result
+>(
+  selectors: [
+    SelectorWithProps<State, Props, S1>,
+    SelectorWithProps<State, Props, S2>,
+    SelectorWithProps<State, Props, S3>,
+    SelectorWithProps<State, Props, S4>,
+    SelectorWithProps<State, Props, S5>,
+    SelectorWithProps<State, Props, S6>,
+    SelectorWithProps<State, Props, S7>,
+    SelectorWithProps<State, Props, S8>
+  ],
+  projector: (
+    s1: S1,
+    s2: S2,
+    s3: S3,
+    s4: S4,
+    s5: S5,
+    s6: S6,
+    s7: S7,
+    s8: S8
+  ) => Result
+): MemoizedSelectorWithProps<State, Props, Result>;
+
+export function createSelector(
+  ...input: any[]
+): Selector<any, any> | SelectorWithProps<any, any, any> {
   return createSelectorFactory(defaultMemoize)(...input);
 }
 
 export function defaultStateFn(
   state: any,
-  selectors: Selector<any, any>[],
+  selectors: any[],
+  props: any,
   memoizedProjector: MemoizedProjection
 ): any {
-  const args = selectors.map(fn => fn(state));
-
+  const args = selectors.map(fn => fn(state, props));
   return memoizedProjector.memoized.apply(null, args);
 }
 
 export type SelectorFactoryConfig<T = any, V = any> = {
   stateFn: (
     state: T,
+    props: any,
     selectors: Selector<any, any>[],
     memoizedProjector: MemoizedProjection
   ) => V;
@@ -252,13 +467,22 @@ export function createSelectorFactory<T = any, V = any>(
   memoize: MemoizeFn,
   options: SelectorFactoryConfig<T, V>
 ): (...input: any[]) => Selector<T, V>;
+export function createSelectorFactory<T = any, Props = any, V = any>(
+  memoize: MemoizeFn
+): (...input: any[]) => SelectorWithProps<T, Props, V>;
+export function createSelectorFactory<T = any, Props = any, V = any>(
+  memoize: MemoizeFn,
+  options: SelectorFactoryConfig<T, V>
+): (...input: any[]) => SelectorWithProps<T, Props, V>;
 export function createSelectorFactory(
   memoize: MemoizeFn,
   options: SelectorFactoryConfig<any, any> = {
     stateFn: defaultStateFn,
   }
 ) {
-  return function(...input: any[]): Selector<any, any> {
+  return function(
+    ...input: any[]
+  ): Selector<any, any> | SelectorWithProps<any, any, any> {
     let args = input;
     if (Array.isArray(args[0])) {
       const [head, ...tail] = args;
@@ -276,8 +500,13 @@ export function createSelectorFactory(
       return projector.apply(null, selectors);
     });
 
-    const memoizedState = defaultMemoize(function(state: any) {
-      return options.stateFn.apply(null, [state, selectors, memoizedProjector]);
+    const memoizedState = defaultMemoize(function(state: any, props: any) {
+      return options.stateFn.apply(null, [
+        state,
+        selectors,
+        props,
+        memoizedProjector,
+      ]);
     });
 
     function release() {

commit 9a043e55f1237c1a94d942cb61fa41d19199884c

Allow second argument in select to be undefined
This is to fix an issue where, if you do not supply a second argument to "select", you will get a result typed as "any".

This is a regression introduced by https://github.com/ngrx/platform/commit/53832a154aae2f5e23e5afee8f79710ffd7b54de

As an example, https://github.com/ngrx/platform/blob/8110c32e495dc2ee5fa56582b98cde8e5ee3b951/modules/store/spec/edge.spec.ts#L41

The above line would previously have produced a result of the Todo[] type, but now produces an "any" typed object, unless a second null argument is supplied to "select", as in `select('todos', null)`.

modules/store/src/store.ts
@@ -117,7 +117,7 @@ export function select<T, Props, K>(
 ): (source$: Observable<T>) => Observable<K>;
 export function select<T, a extends keyof T>(
   key: a,
-  props: null
+  props?: null
 ): (source$: Observable<T>) => Observable<T[a]>;
 export function select<T, a extends keyof T, b extends keyof T[a]>(
   key1: a,