24be6c6bf50888c0056d232e0e282abbba575254
Eddie Tisma <eddietisma@users.noreply.github.com>
2019-11-15 13:42:58 +0100

    Update mock_reducer_manager.ts

--- modules/store/testing/src/mock_reducer_manager.ts
+++ modules/store/testing/src/mock_reducer_manager.ts
@@ -1,16 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ActionReducer } from '@ngrx/store';
-import {
-  Action,
-  ActionsSubject,
-  INITIAL_STATE,
-  ReducerManager,
-  Store,
-  createSelector,
-  MemoizedSelectorWithProps,
-  MemoizedSelector,
-} from '@ngrx/store';
 
 @Injectable()
 export class MockReducerManager extends BehaviorSubject<
