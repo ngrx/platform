aacd0e9a3913d125be7d0018cfc8416983280601
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-12-09 20:59:12 +0100

    feat(router-store): migration to add the default serializer

---
+++ modules/router-store/migrations/9_0_0/index.ts
@@ -0,0 +1,97 @@
+import * as ts from 'typescript';
+import {
+  Rule,
+  chain,
+  Tree,
+  SchematicContext,
+} from '@angular-devkit/schematics';
+import {
+  visitTSSourceFiles,
+  commitChanges,
+  InsertChange,
+  visitNgModuleImports,
+  insertImport,
+  Change,
+  containsProperty,
+} from '@ngrx/router-store/schematics-core';
+
+function addDefaultSerializer(): Rule {
+  const SERIALIZER_PROPERTY = 'serializer: DefaultRouterStateSerializer';
+  return (tree: Tree, ctx: SchematicContext) => {
+    visitTSSourceFiles(tree, sourceFile => {
+      let changes: Change[] = [];
+
+      visitNgModuleImports(sourceFile, (importsNode, elementsNode) => {
+        elementsNode
+          .filter(
+            element =>
+              ts.isCallExpression(element) &&
+              ts.isPropertyAccessExpression(element.expression) &&
+              ts.isIdentifier(element.expression.expression) &&
+              element.expression.expression.text ===
+                'StoreRouterConnectingModule'
+          )
+          .forEach(element => {
+            const callExpression = element as ts.CallExpression;
+            const callArgument = callExpression.arguments[0];
+
+            // StoreRouterConnectingModule.forRoot() without arguments
+            if (callArgument === undefined) {
+              changes.push(
+                new InsertChange(
+                  sourceFile.fileName,
+                  callExpression.getEnd() - 1,
+                  `{ ${SERIALIZER_PROPERTY} }`
+                )
+              );
+            } else if (ts.isObjectLiteralExpression(callArgument)) {
+              // StoreRouterConnectingModule.forRoot({ key: 'router' }) with arguments
+              const serializerSet = containsProperty(
+                callArgument,
+                'serializer'
+              );
+              const routerStateSet = containsProperty(
+                callArgument,
+                'routerState'
+              );
+
+              if (serializerSet || routerStateSet) {
+                return;
+              }
+
+              changes.push(
+                new InsertChange(
+                  sourceFile.fileName,
+                  callArgument.getStart() + 1,
+                  ` ${SERIALIZER_PROPERTY},`
+                )
+              );
+            }
+          });
+      });
+
+      if (changes.length) {
+        changes.push(
+          insertImport(
+            sourceFile,
+            sourceFile.fileName,
+            'DefaultRouterStateSerializer',
+            '@ngrx/router-store'
+          )
+        );
+      }
+
+      commitChanges(tree, sourceFile.fileName, changes);
+
+      if (changes.length) {
+        ctx.logger.info(
+          `[@ngrx/router-store] Updated StoreRouterConnectingModule's configuration, see the migration guide (https://ngrx.io/guide/migration/v9#ngrxrouter-store) for more info`
+        );
+      }
+    });
+  };
+}
+
+export default function(): Rule {
+  return chain([addDefaultSerializer()]);
+}
---
+++ modules/router-store/schematics-core/utility/queries.ts
@@ -0,0 +1,17 @@
+import * as ts from 'typescript';
+
+export function findNgImports(
+  node: ts.Node,
+  callback: (importNode: ts.PropertyAssignment) => void
+) {
+  ts.forEachChild(node, n => {
+    if (
+      ts.isPropertyAssignment(n) &&
+      ts.isArrayLiteralExpression(n.initializer) &&
+      ts.isIdentifier(n.name) &&
+      n.name.text === 'imports'
+    ) {
+      callback(n);
+    }
+  });
+}
