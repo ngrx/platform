569f77a13c791a39df1cb3456feff37e5cd1fdd9
Michael Hladky <michael@hladky.at>
2020-03-12 18:31:22 +0100

    fix: added comments and typing

--- modules/component/src/core/utils/get-global-this.ts
+++ modules/component/src/core/utils/get-global-this.ts
@@ -1,3 +1,7 @@
-export function getGlobalThis() {
+// Returns a reference to global thin
+// - Browser
+// - SSR
+// - Tests
+export function getGlobalThis(): any {
   return ((globalThis as any) || (self as any) || (window as any)) as any;
 }
