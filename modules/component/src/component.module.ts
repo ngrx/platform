241bfddfb908b0e848fb5541fd49cf0b8de7f036
Michael Hladky <michael@hladky.at>
2020-03-12 16:41:10 +0100

    fix: removed empty imports, removed exports

--- modules/component/src/component.module.ts
+++ modules/component/src/component.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { LetDirective } from './let';
 import { PushPipe } from './push';
 
-export const DECLARATIONS = [LetDirective, PushPipe];
-export const EXPORTS = [DECLARATIONS];
+const DECLARATIONS = [LetDirective, PushPipe];
+const EXPORTS = [DECLARATIONS];
 
 @NgModule({
   declarations: [DECLARATIONS],
-  imports: [],
   exports: [EXPORTS],
 })
 export class ComponentModule {}
