8b5e54b3542ce280b3d86bcf377a459918d7da35
Michael Hladky <michael@hladky.at>
2020-03-14 11:03:30 +0100

    fix: refactored to operator

--- modules/component/src/core/operators/toObservableValue.ts
+++ modules/component/src/core/operators/toObservableValue.ts
@@ -1,24 +1,24 @@
 import { from, Observable, of } from 'rxjs';
-import { isObservableGuard, isPromiseGuard } from '../utils';
+import { isObservableGuard, isPromiseGuard, observableValue } from '../utils';
 
-export function toObservableValue<T>(
-  potentialObservableValue$: Observable<T> | Promise<T> | undefined | null
-): Observable<T | undefined | null> {
-  if (
+export function toObservableValue<T>() {
+  return (
+    potentialObservableValue$: observableValue<T> | undefined | null
+  ): Observable<T | undefined | null> => {
+    if (
+      isPromiseGuard<T>(potentialObservableValue$) ||
+      isObservableGuard<T>(potentialObservableValue$)
+    ) {
+      return from(potentialObservableValue$);
+    }
+
     // Comparing to the literal null value with the == operator covers both null and undefined values.
-    potentialObservableValue$ == null
-  ) {
-    return of(potentialObservableValue$);
-  }
+    if (potentialObservableValue$ == null) {
+      return of(potentialObservableValue$);
+    }
 
-  if (
-    isPromiseGuard<T>(potentialObservableValue$) ||
-    isObservableGuard<T>(potentialObservableValue$)
-  ) {
-    return from(potentialObservableValue$);
-  }
-
-  throw new Error(
-    'Argument not observable. Only null/undefined or Promise/Observable-like values are allowed.'
-  );
+    throw new Error(
+      'Argument not observable. Only null/undefined or Promise/Observable-like values are allowed.'
+    );
+  };
 }
