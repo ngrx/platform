8941d71ffd02862dc472680e7e095b75d9e47932
Michael Hladky <michael@hladky.at>
2020-03-14 09:31:19 +0100

    fix: changed to JsDocs format

--- modules/component/src/core/cd-aware.abstract.ts
+++ modules/component/src/core/cd-aware.abstract.ts
@@ -25,10 +25,15 @@ export interface CoalescingConfig {
   optimized: boolean;
 }
 
-// This abstract class holds all the shared logic for the push pipe and the let directive
-// responsible for change detection
-// If you extend this class you need to implement how the update of the rendered value happens.
-// Also custom behaviour is something you need to implement in the extending class
+/**
+ * class CdAware
+ *
+ * @description
+ * This abstract class holds all the shared logic for the push pipe and the let directive
+ * responsible for change detection
+ * If you extend this class you need to implement how the update of the rendered value happens.
+ * Also custom behaviour is something you need to implement in the extending class
+ */
 export abstract class CdAware implements OnDestroy {
   readonly handleChangeDetection: <T>(
     component?: T
