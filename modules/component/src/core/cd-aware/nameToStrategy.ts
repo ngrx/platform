a24a4ff3ceebea12fb183108ff681b45cd058cfb
Michael Hladky <michael@hladky.at>
2020-04-13 15:06:46 +0200

    fix(component) moved logic for strategy mapping into operator

---
+++ modules/component/src/core/cd-aware/nameToStrategy.ts
@@ -0,0 +1,20 @@
+import { Observable } from 'rxjs';
+import { distinctUntilChanged, map, startWith } from 'rxjs/operators';
+import {
+  CdStrategy,
+  DEFAULT_STRATEGY_NAME,
+  StrategySelection,
+} from './strategy';
+
+export function nameToStrategy<U>(strategies: StrategySelection<U>) {
+  return (o$: Observable<string>): Observable<CdStrategy<U>> => {
+    return o$.pipe(
+      distinctUntilChanged(),
+      startWith(DEFAULT_STRATEGY_NAME),
+      map(
+        (strategy: string): CdStrategy<U> =>
+          strategies[strategy] ? strategies[strategy] : strategies.idle
+      )
+    );
+  };
+}
