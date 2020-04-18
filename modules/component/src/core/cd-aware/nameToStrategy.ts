9d2abd7b68256c9de6bf79123b0004d82bef139f
micha <michael@hladky.at>
2020-04-18 21:33:49 +0200

    fix(component)remove startWith

--- modules/component/src/core/cd-aware/nameToStrategy.ts
+++ modules/component/src/core/cd-aware/nameToStrategy.ts
@@ -10,7 +10,6 @@ export function nameToStrategy<U>(strategies: StrategySelection<U>) {
   return (o$: Observable<string>): Observable<CdStrategy<U>> => {
     return o$.pipe(
       distinctUntilChanged(),
-      startWith(DEFAULT_STRATEGY_NAME),
       map(
         (strategy: string): CdStrategy<U> =>
           strategies[strategy]
