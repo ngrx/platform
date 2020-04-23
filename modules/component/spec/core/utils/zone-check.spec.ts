935ef0db9cc7bd2ec591458c3a096724577f212a
micha <michael@hladky.at>
2020-04-23 19:11:02 +0200

    zone tests

--- modules/component/spec/core/utils/zone-check.spec.ts
+++ modules/component/spec/core/utils/zone-check.spec.ts
@@ -1,14 +1,47 @@
 import { envZonePatched, getGlobalThis } from '../../../src/core/utils';
-import { mockPromise } from '../../fixtures/fixtures';
+import { manualInstanceNgZone, mockPromise } from '../../fixtures/fixtures';
 
-describe('apiZonePatched', () => {
-  it('should return true if `zone.js` did patch the Promise API', () => {
-    getGlobalThis().__zone_symbol__Promise = mockPromise;
-    expect(envZonePatched('Promise')).toBe(true);
+// @TODO use better solution to type that
+type AnyObject = { [prop: string]: any };
+function mutationManagerFactory<T>(obj: AnyObject, props: AnyObject) {
+  const originals: AnyObject = Object.entries(props).reduce(
+    (originals: AnyObject, [prop, defaultValue]: [string, any]): AnyObject => {
+      return {
+        ...originals,
+        [prop]: obj.hasOwnProperty(prop) ? obj[prop] : defaultValue,
+      };
+    },
+    {}
+  );
+
+  return {
+    restore: () => {
+      Object.entries(originals).forEach(([prop, value]) => {
+        obj[prop] = value;
+      });
+    },
+    set: (prop: string, value: any) => {
+      obj[prop] = value;
+    },
+  };
+}
+
+const mutationManager = mutationManagerFactory(getGlobalThis(), {
+  Zone: manualInstanceNgZone,
+});
+
+describe('envZonePatched', () => {
+  beforeEach(() => {
+    mutationManager.restore();
   });
 
-  it('should return false if `zone.js` did not patch the Promise API', () => {
-    getGlobalThis().__zone_symbol__Promise = undefined;
-    expect(envZonePatched('Promise')).toBe(false);
+  it('should return true if `zone.js` did patch the global API', () => {
+    mutationManager.set('Zone', manualInstanceNgZone);
+    expect(envZonePatched()).toBe(true);
+  });
+
+  it('should return false if `zone.js` did not patch the global API', () => {
+    mutationManager.set('Zone', undefined);
+    expect(envZonePatched()).toBe(false);
   });
 });
---
+++ modules/component/spec/fixtures/mock-event-emitter.ts
@@ -0,0 +1,7 @@
+export class MockEventEmitter<T> {
+  next: (value: any) => {};
+  error: (error: any) => {};
+  complete: () => {};
+  subscribe: () => { unsubscribe: () => {} };
+  constructor(async: boolean) {}
+}
