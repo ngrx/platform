60e9bcd243f529dd670cdfccef29ab3dab4c3d6d
Brandon Roberts <brandon@nrwl.io>
2020-05-17 20:39:27 -0500

    chore: add project for example app E2E tests

--- projects/example-app-cypress/plugins/index.js
+++
@@ -1,7 +0,0 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
-
-module.exports = on =>
-  on('file:preprocessor', cypressTypeScriptPreprocessor);
-
-// TODO: uncomment once Applitools work properly
-// require('@applitools/eyes-cypress')(module);
