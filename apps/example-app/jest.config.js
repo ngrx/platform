f092dcdb0dd82b5c4c4d12c97127e9c2fcbd24ca
Brandon Roberts <robertsbt@gmail.com>
2020-05-27 22:03:32 -0500

    chore: fix tests for example app

--- apps/example-app/jest.config.js
+++ apps/example-app/jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   name: 'example-app',
+  displayName: 'Example App',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/modules/example-app',
+  coverageDirectory: '../../coverage/apps/example-app',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
