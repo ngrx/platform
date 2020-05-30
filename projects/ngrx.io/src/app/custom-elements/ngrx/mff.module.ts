728729938fb439bfd24d3470b336b2b1969368e0
Mike Ryan <mike.ryan52@gmail.com>
2020-05-30 11:46:01 -0500

    docs(marketing): Add Minnesota Freedom Fund call to action to the homepage (#2548)

---
+++ projects/ngrx.io/src/app/custom-elements/ngrx/mff.module.ts
@@ -0,0 +1,12 @@
+import { NgModule, Type } from '@angular/core';
+import { WithCustomElementComponent } from '../element-registry';
+import { MffComponent } from './mff.component';
+
+@NgModule({
+  declarations: [MffComponent],
+  exports: [MffComponent],
+  entryComponents: [MffComponent],
+})
+export class MffModule implements WithCustomElementComponent {
+  customElementComponent: Type<any> = MffComponent;
+}
