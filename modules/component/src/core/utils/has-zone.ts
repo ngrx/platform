f07c1cdf5e2280fd1348346804f123c8f2f37780
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-06-15 13:57:54 +0200

    fix(component): remove outdated comment

--- modules/component/src/core/utils/has-zone.ts
+++ modules/component/src/core/utils/has-zone.ts
@@ -5,9 +5,6 @@ import { NgZone } from '@angular/core';
  *
  * Determines if the application uses `NgZone` or `NgNoopZone` as ngZone service instance.
  *
- * It works by detecting if `apply()` is called on a function passed to `runOutsideAngular()`.
- * The `NgZone` does that while the `NgNoopZone` doesn't do it.
- *
  * The function can be just imported and used everywhere.
  *
  * ```ts
