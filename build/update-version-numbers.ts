fe91af0ffe20822468f13cdda5a9552ab3803e7f
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2020-06-18 20:25:07 +0200

    refactor: use builders

--- build/update-version-numbers.ts
+++ build/update-version-numbers.ts
@@ -1,7 +1,9 @@
-import { EOL } from 'os';
 import { readFileSync, writeFileSync } from 'fs';
-import * as glob from 'glob';
+import { EOL } from 'os';
 import * as readline from 'readline';
+import * as glob from 'glob';
+import { createBuilder } from './util';
+import { packages } from './config';
 
 // get the version from the command
 // e.g. ts-node ./build/update-version-numbers.ts 10.0.0
@@ -23,60 +25,74 @@ if (newVersion) {
 }
 
 function updateVersions(version: string) {
-  [updatePackageJson, updateAddSchematic, archivePreviousDocs].forEach((m) =>
-    m(version)
-  );
-}
+  const publishNext = createBuilder([
+    ['Update package.json', createPackageJsonBuilder(version)],
+    ['Update ng-add schematic', createUpdateAddSchematicBuilder(version)],
+    ['Update docs version picker', createArchivePreviousDocsBuilder(version)],
+  ]);
 
-function updatePackageJson(version: string) {
-  glob.sync('**/package.json', { ignore: '**/node_modules/**' }).map((file) => {
-    const content = readFileSync(file, 'utf-8');
-    const pkg = JSON.parse(content);
-    if (pkg?.version && pkg?.name?.startsWith('@ngrx')) {
-      pkg.version = version;
-      writeAsJson(file, pkg);
-    }
+  publishNext({
+    scope: '@ngrx',
+    packages,
+  }).catch((err) => {
+    console.error(err);
+    process.exit(1);
   });
-
-  console.log('✔ Version updated in package.json files.');
 }
 
-function updateAddSchematic(version: string) {
-  glob
-    .sync('**/libs-version.ts', { ignore: '**/node_modules/**' })
-    .map((file) => {
-      writeFileSync(
-        file,
-        `export const platformVersion = '^${version}';${EOL}`
-      );
-    });
-
-  console.log('✔ Version updated for ng-add schematics.');
+function createPackageJsonBuilder(version: string) {
+  return async () => {
+    glob
+      .sync('**/package.json', { ignore: '**/node_modules/**' })
+      .map((file) => {
+        const content = readFileSync(file, 'utf-8');
+        const pkg = JSON.parse(content);
+        if (pkg?.version && pkg?.name?.startsWith('@ngrx')) {
+          pkg.version = version;
+          writeAsJson(file, pkg);
+        }
+      });
+  };
 }
 
-function archivePreviousDocs(version: string) {
-  // only deprecate previous version on MAJOR releases
-  if (!version.endsWith('.0.0')) {
-    return;
-  }
-  const path = './projects/ngrx.io/content/navigation.json';
+function createUpdateAddSchematicBuilder(version: string) {
+  return async () => {
+    glob
+      .sync('**/libs-version.ts', { ignore: '**/node_modules/**' })
+      .map((file) => {
+        writeFileSync(
+          file,
+          `export const platformVersion = '^${version}';${EOL}`
+        );
+      });
+  };
+}
 
-  const [major] = version.split('.');
-  const prevousVersion = Number(major) - 1;
-  const content = readFileSync(path, 'utf-8');
-  const navigation = JSON.parse(content);
-  navigation['docVersions'] = [
-    {
-      title: `v${prevousVersion}`,
-      url: `https://v${prevousVersion}.ngrx.io`,
-    },
-    ...navigation['docVersions'],
-  ];
-  writeAsJson(path, navigation);
+function createArchivePreviousDocsBuilder(version: string) {
+  return async () => {
+    // only deprecate previous version on MAJOR releases
+    if (!version.endsWith('.0.0')) {
+      return;
+    }
+    const path = './projects/ngrx.io/content/navigation.json';
 
-  console.log(
-    '⚠ Previous version added to website doc versions but site needs to be archived manually.'
-  );
+    const [major] = version.split('.');
+    const prevousVersion = Number(major) - 1;
+    const content = readFileSync(path, 'utf-8');
+    const navigation = JSON.parse(content);
+    navigation['docVersions'] = [
+      {
+        title: `v${prevousVersion}`,
+        url: `https://v${prevousVersion}.ngrx.io`,
+      },
+      ...navigation['docVersions'],
+    ];
+    writeAsJson(path, navigation);
+
+    console.log(
+      '\r\n⚠ Previous version added to website doc versions but site needs to be archived manually.'
+    );
+  };
 }
 
 function writeAsJson(path: string, json: object) {
