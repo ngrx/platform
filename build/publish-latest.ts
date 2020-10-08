083e63c67fa1c27db0708dcef3f87673d2894635
Brandon <robertsbt@gmail.com>
2020-10-08 09:20:06 -0500

    chore: update script for publishing to latest

--- build/publish-latest.ts
+++ build/publish-latest.ts
@@ -1,23 +1,24 @@
-import * as util from './util';
 import { createBuilder } from './util';
-import { Config, packages } from './config';
+import { packages } from './config';
 import * as shelljs from 'shelljs';
+import { readFileSync } from 'fs-extra';
 
 /**
  * Publish release to NPM on "latest" tag
  */
-export async function publishToNpm(config: Config) {
-  const publishablePackages = util
-    .getTopLevelPackages(config)
-    .filter((pkg) => !pkg.includes('component'));
+export async function publishLatestToNpm() {
+  const configJSON = JSON.parse(readFileSync('./angular.json').toString());
+  const projects = configJSON.projects;
+  const publishablePackages = Object.keys(projects).filter(
+    (project) => !!projects[project].architect['build-package']
+  );
 
   for (let pkg of publishablePackages) {
     console.log(`Publishing @ngrx/${pkg}`);
 
     const cmd = [
-      './node_modules/.bin/bazel run',
-      `//modules/${pkg}:npm_package.publish`,
-      '--',
+      'npm publish',
+      `./dist/modules/${pkg}`,
       '--access=public',
       '--tag=latest',
     ];
@@ -27,7 +28,7 @@ export async function publishToNpm(config: Config) {
 }
 
 const publishLatest = createBuilder([
-  ['Publish packages on latest', publishToNpm],
+  ['Publish packages on latest\n', publishLatestToNpm],
 ]);
 
 publishLatest({
