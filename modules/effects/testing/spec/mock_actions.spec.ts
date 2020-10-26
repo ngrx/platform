63a4cb185f06d5b10ad1cddb7a21b08f29cc5397
markostanimirovic <markostanimirovic95@gmail.com>
2020-10-26 22:46:08 +0100

    feat(effects): use provideMockActions outside of the TestBed

---
+++ modules/effects/testing/spec/mock_actions.spec.ts
@@ -0,0 +1,59 @@
+import { of } from 'rxjs';
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Actions } from '@ngrx/effects';
+import { Injector } from '@angular/core';
+
+describe('Mock Actions', () => {
+  describe('with TestBed', () => {
+    it('should provide Actions from source', (done) => {
+      TestBed.configureTestingModule({
+        providers: [provideMockActions(of({ type: 'foo' }))],
+      });
+
+      const actions$ = TestBed.inject(Actions);
+      actions$.subscribe((action) => {
+        expect(action.type).toBe('foo');
+        done();
+      });
+    });
+
+    it('should provide Actions from factory', (done) => {
+      TestBed.configureTestingModule({
+        providers: [provideMockActions(() => of({ type: 'bar' }))],
+      });
+
+      const actions$ = TestBed.inject(Actions);
+      actions$.subscribe((action) => {
+        expect(action.type).toBe('bar');
+        done();
+      });
+    });
+  });
+
+  describe('with Injector', () => {
+    it('should provide Actions from source', (done) => {
+      const injector = Injector.create({
+        providers: [provideMockActions(of({ type: 'foo' }))],
+      });
+
+      const actions$ = injector.get(Actions);
+      actions$.subscribe((action) => {
+        expect(action.type).toBe('foo');
+        done();
+      });
+    });
+
+    it('should provide Actions from factory', (done) => {
+      const injector = Injector.create({
+        providers: [provideMockActions(() => of({ type: 'bar' }))],
+      });
+
+      const actions$ = injector.get(Actions);
+      actions$.subscribe((action) => {
+        expect(action.type).toBe('bar');
+        done();
+      });
+    });
+  });
+});
