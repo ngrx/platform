d1873c94eaeaec8a8b72a4d1d978f5982d053156
Alex Okrushko <alex.okrushko@gmail.com>
2020-11-07 08:25:51 -0500

    feat(component-store): add tapResponse operator (#2763)
    * feat(component-store): add handleResponse operator
    
    * Rename to mapResponse
    
    * rename to tapResponse

---
+++ modules/component-store/src/tap-response.ts
@@ -0,0 +1,39 @@
+import { EMPTY, Observable } from 'rxjs';
+
+import { catchError, tap } from 'rxjs/operators';
+
+/**
+ * Handles the response in ComponentStore effects in a safe way, without
+ * additional boilerplate.
+ * It enforces that the error case is handled and that the effect would still be
+ * running should an error occur.
+ *
+ * Takes an optional third argument for a `complete` callback.
+ *
+ * ```typescript
+ * readonly dismissedAlerts = this.effect<Alert>(alert$ => {
+ *  return alert$.pipe(
+ *      concatMap(
+ *          (alert) => this.alertsService.dismissAlert(alert).pipe(
+ *              tapResponse(
+ *                 (dismissedAlert) => this.alertDismissed(dismissedAlert),
+ *                 (error) => this.logError(error),
+ *              ))));
+ *   });
+ * ```
+ */
+export function tapResponse<T>(
+  nextFn: (next: T) => void,
+  errorFn: (error: unknown) => void,
+  completeFn?: () => void
+): (source: Observable<T>) => Observable<T> {
+  return (source) =>
+    source.pipe(
+      tap({
+        next: nextFn,
+        error: errorFn,
+        complete: completeFn,
+      }),
+      catchError(() => EMPTY)
+    );
+}
