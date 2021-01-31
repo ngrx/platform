884379bad2718902b6ce76ea5948e71785ae7eb9
jaybell <jaycooperbell@gmail.com>
2021-01-30 16:32:47 -0800

    doc(data): add example for filter fn in story entity

--- projects/data-example-app/src/state/shared-state-story.ts
+++ projects/data-example-app/src/state/shared-state-story.ts
@@ -5,4 +5,9 @@ export const storyEntityMetadata: EntityMetadata<Story> = {
   entityName: 'Story',
   selectId: (entity: Story): string => entity.storyId,
   sortComparer: (a, b) => a.createdAt.getTime() - b.createdAt.getTime(),
+  filterFn: (entities, pattern) =>
+    entities.filter(
+      (entity) =>
+        entity.title?.includes(pattern) || entity.title?.includes(pattern)
+    ),
 };
