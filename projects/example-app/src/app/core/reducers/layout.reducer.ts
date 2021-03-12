a33705cafed37cf08da17e3f7768434125cb9c66
Matthew Harris <rtpharry@hotmail.com>
2021-03-12 00:40:48 +0000

    refactor(example-app): fix typo "thought" in comment (#2968)

--- projects/example-app/src/app/core/reducers/layout.reducer.ts
+++ projects/example-app/src/app/core/reducers/layout.reducer.ts
@@ -15,7 +15,7 @@ const initialState: State = {
 
 export const reducer = createReducer(
   initialState,
-  // Even thought the `state` is unused, it helps infer the return type
+  // Even though the `state` is unused, it helps infer the return type
   on(LayoutActions.closeSidenav, (state) => ({ showSidenav: false })),
   on(LayoutActions.openSidenav, (state) => ({ showSidenav: true })),
   on(AuthActions.logoutConfirmation, (state) => ({ showSidenav: false }))
