bbcebe1541ed254131bdec669f0927230304ab9d
AlmaniaM <8919797+AlmaniaM@users.noreply.github.com>
2021-04-24 00:28:08 -0700

    docs: remove unused css file from counter component (#3002)
    Closes #2989

--- projects/ngrx.io/content/examples/store/src/app/my-counter/my-counter.component.ts
+++ projects/ngrx.io/content/examples/store/src/app/my-counter/my-counter.component.ts
@@ -6,7 +6,6 @@ import { increment, decrement, reset } from '../counter.actions';
 @Component({
   selector: 'app-my-counter',
   templateUrl: './my-counter.component.html',
-  styleUrls: ['./my-counter.component.css'],
 })
 export class MyCounterComponent {
   count$: Observable<number>;
