e5e8c22e4badec688151b0d9f8f467fbb00a6344
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-08-17 19:06:53 +0200

    docs(effects): add ofType example to API docs

--- modules/effects/src/actions.ts
+++ modules/effects/src/actions.ts
@@ -116,6 +116,18 @@ export function ofType<V extends Action>(
  * Unfortunately, for unknown 'actions: Actions' these types will produce
  * 'Observable<never>'. In such cases one has to manually set the generic type
  * like `actions.ofType<AdditionAction>('add')`.
+ *
+ * ## Example
+ * Filter the Actions stream on the "customers page loaded" action
+ *
+ * ```ts
+ * import { ofType } from '@ngrx/effects';
+ * import * fromCustomers from '../customers';
+ *
+ * this.actions$.pipe(
+ *  ofType(fromCustomers.pageLoaded)
+ * )
+ * ```
  */
 export function ofType(
   ...allowedTypes: Array<string | ActionCreator<string, Creator>>
