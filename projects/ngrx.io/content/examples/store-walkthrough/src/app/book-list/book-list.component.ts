cba08cd1cf89a41f8055eb151fd0438933d29e69
Mauro Mattos <mauromattos00@gmail.com>
2021-09-02 13:23:25 -0300

    docs(example): import new book model

--- projects/ngrx.io/content/examples/store-walkthrough/src/app/book-list/book-list.component.ts
+++ projects/ngrx.io/content/examples/store-walkthrough/src/app/book-list/book-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Book } from './books.model';
+import { Book } from '../models/book.model';
 
 @Component({
   selector: 'app-book-list',
--- projects/ngrx.io/content/examples/store-walkthrough/src/app/state/books.actions.ts
+++ projects/ngrx.io/content/examples/store-walkthrough/src/app/state/books.actions.ts
@@ -1,5 +1,5 @@
 import { createAction, props } from '@ngrx/store';
-import { Book } from '../models';
+import { Book } from '../models/book.model';
 
 export const addBook = createAction(
   '[Book List] Add Book',
