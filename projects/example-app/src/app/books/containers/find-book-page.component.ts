bf593c7bf58e8809db696c4a1edee6f194649422
Constantine <superfedoruk@gmail.com>
2021-11-12 02:37:41 +0200

    Update find-book-page.component.ts
    According to  https://github.com/ngrx/platform/issues/3222

--- projects/example-app/src/app/books/containers/find-book-page.component.ts
+++ projects/example-app/src/app/books/containers/find-book-page.component.ts
@@ -13,9 +13,9 @@ import * as fromBooks from '@example-app/books/reducers';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <bc-book-search
-      [query]="searchQuery$ | async"
-      [searching]="loading$ | async"
-      [error]="error$ | async"
+      [query]="(searchQuery$ | async)!"
+      [searching]="(loading$ | async)!"
+      [error]="(error$ | async)!"
       (search)="search($event)"
     >
     </bc-book-search>
