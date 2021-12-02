114bb03c02a864db9532f1937003872cf41ff7be
Kyle <kthprog@gmail.com>
2021-12-02 15:59:23 -0500

    Update my-counter.component.ts
    Per the default Angular eslint rules, the non-generic Store should be used instead of the generc Store<T>
    https://github.com/timdeschryver/eslint-plugin-ngrx/blob/main/docs/rules/no-typed-global-store.md

--- projects/ngrx.io/content/examples/store/src/app/my-counter/my-counter.component.ts
+++ projects/ngrx.io/content/examples/store/src/app/my-counter/my-counter.component.ts
@@ -10,7 +10,7 @@ import { increment, decrement, reset } from '../counter.actions';
 export class MyCounterComponent {
   count$: Observable<number>;
 
-  constructor(private store: Store<{ count: number }>) {
+  constructor(private store: Store) {
     this.count$ = store.select('count');
   }
 
