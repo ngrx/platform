f36d86509f8d77a0396a39b97ef3c3ecaa881695
Eduardo Speroni <edusperoni@gmail.com>
2022-03-08 11:38:46 -0300

    feat(store-devtools): export private Redux Devtools Extension injection token
    The injection token REDUX_DEVTOOLS_EXTENSION is currently private and unable to be overriden by any means. This change allows for custom implementations of the devtools extension interface on platforms that are not browser

--- modules/store-devtools/src/index.ts
+++ modules/store-devtools/src/index.ts
@@ -1,6 +1,7 @@
 export { StoreDevtoolsModule } from './instrument';
 export { LiftedState, RECOMPUTE } from './reducer';
 export { StoreDevtools } from './devtools';
+export { REDUX_DEVTOOLS_EXTENSION } from './extension';
 export {
   StoreDevtoolsConfig,
   StoreDevtoolsOptions,
