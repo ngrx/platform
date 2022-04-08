bfa3e2852e6e7314b9016a3ef70c87fe4b8e7dc3
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-04-08 14:26:05 +0200

    style: disable lint errors

--- projects/example-app-e2e/src/plugins/index.js
+++ projects/example-app-e2e/src/plugins/index.js
@@ -11,4 +11,5 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (on, config) => {};
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+module.exports = () => {};
--- projects/example-app-e2e/src/support/index.ts
+++ projects/example-app-e2e/src/support/index.ts
@@ -17,6 +17,7 @@
 import './commands';
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
       saveLocalStorage(): Chainable<void>;
