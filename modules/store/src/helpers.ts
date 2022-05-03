f523df25ecf370dbf799359d79e2e23b4e87ad9d
markostanimirovic <markostanimirovic95@gmail.com>
2022-05-03 20:10:45 +0200

    refactor(store): google upstream changes

--- modules/store/src/helpers.ts
+++ modules/store/src/helpers.ts
@@ -1,3 +1,3 @@
 export function capitalize<T extends string>(text: T): Capitalize<T> {
-  return (text.charAt(0).toUpperCase() + text.substr(1)) as Capitalize<T>;
+  return (text.charAt(0).toUpperCase() + text.substring(1)) as Capitalize<T>;
 }
