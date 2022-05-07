bc7586d2cd5f4d6533e211c807a166e5068ce605
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-05-07 21:53:29 +0200

    chore: remove bazel files (#3406)

--- modules/component-store/rollup.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  entry: './dist/component-store/@ngrx/component-store.es5.js',
-  dest: './dist/component-store/bundles/component-store.umd.js',
-  format: 'umd',
-  exports: 'named',
-  moduleName: 'ngrx.component-store',
-  globals: {},
-};
