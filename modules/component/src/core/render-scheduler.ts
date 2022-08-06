38364c1eea1e1c18473f03a827899d41f8ee8921
markostanimirovic <markostanimirovic95@gmail.com>
2022-08-06 14:39:03 +0200

    feat(component): add RenderScheduler to the public API

--- modules/component/src/core/render-scheduler.ts
+++ modules/component/src/core/render-scheduler.ts
@@ -1,6 +1,60 @@
 import { ChangeDetectorRef, inject, Injectable } from '@angular/core';
 import { TickScheduler } from './tick-scheduler';
 
+/**
+ * Provides rendering functionality regardless of whether `zone.js` is present
+ * or not. It must be provided at the component/directive level.
+ *
+ * @usageNotes
+ *
+ * ### Rerender zone-less app on route changes
+ *
+ * ```ts
+ * @Component({
+ *   selector: 'app-root',
+ *   template: '<router-outlet>',
+ *   // 👇 `RenderScheduler` is provided at the component level
+ *   providers: [RenderScheduler],
+ *   changeDetection: ChangeDetectionStrategy.OnPush,
+ * })
+ * export class AppComponent implements OnInit {
+ *   constructor(
+ *     private readonly router: Router,
+ *     private readonly renderScheduler: RenderScheduler
+ *   ) {}
+ *
+ *   ngOnInit(): void {
+ *     this.router.events
+ *       .pipe(filter((e) => e instanceof NavigationEnd))
+ *       .subscribe(() => this.renderScheduler.schedule());
+ *   }
+ * }
+ * ```
+ *
+ * ### Rerender component on interval
+ *
+ * ```ts
+ * @Component({
+ *   selector: 'app-interval',
+ *   template: '{{ elapsedTime }}ms',
+ *   // 👇 `RenderScheduler` is provided at the component level
+ *   providers: [RenderScheduler],
+ *   changeDetection: ChangeDetectionStrategy.OnPush,
+ * })
+ * export class IntervalComponent implements OnInit {
+ *   elapsedTime = 0;
+ *
+ *   constructor(private readonly renderScheduler: RenderScheduler) {}
+ *
+ *   ngOnInit(): void {
+ *     setInterval(() => {
+ *       this.elapsedTime += 1000;
+ *       this.renderScheduler.schedule();
+ *     }, 1000);
+ *   }
+ * }
+ * ```
+ */
 @Injectable()
 export class RenderScheduler {
   constructor(
@@ -8,6 +62,10 @@ export class RenderScheduler {
     private readonly tickScheduler: TickScheduler
   ) {}
 
+  /**
+   * Marks component and its ancestors as dirty.
+   * It also schedules a new change detection cycle in zone-less mode.
+   */
   schedule(): void {
     this.cdRef.markForCheck();
     this.tickScheduler.schedule();
