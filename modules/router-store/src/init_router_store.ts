7044bd5584f83c776dce48703fef704804f914e5
jaybell <jaycooperbell@gmail.com>
2022-08-15 11:53:29 -0700

    fix(router-store): provide connecting service and inject with env initializer

--- modules/router-store/src/init_router_store.ts
+++ modules/router-store/src/init_router_store.ts
@@ -1,26 +1,13 @@
-import {
-  ENVIRONMENT_INITIALIZER,
-  ErrorHandler,
-  inject,
-  Provider,
-} from '@angular/core';
+import { ENVIRONMENT_INITIALIZER, inject, Provider } from '@angular/core';
 import { StoreRouterConnectingService } from './store_router_connecting.service';
-import { ACTIVE_RUNTIME_CHECKS, Store } from '@ngrx/store';
-import { Router } from '@angular/router';
-import { ROUTER_CONFIG, StoreRouterConfig } from './router_store_config';
-import { RouterStateSerializer } from './serializers/base';
 
-export const _initRouterStore: Provider = {
-  provide: ENVIRONMENT_INITIALIZER,
-  multi: true,
-  useValue() {
-    new StoreRouterConnectingService(
-      inject(Store),
-      inject(Router),
-      inject(RouterStateSerializer),
-      inject(ErrorHandler),
-      inject<StoreRouterConfig>(ROUTER_CONFIG),
-      inject(ACTIVE_RUNTIME_CHECKS)
-    );
+export const _initRouterStore: Provider[] = [
+  {
+    provide: ENVIRONMENT_INITIALIZER,
+    multi: true,
+    useValue() {
+      inject(StoreRouterConnectingService);
+    },
   },
-};
+  StoreRouterConnectingService,
+];
