a3773dd679d2e5d4038382b46326deea38a74f80
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-08-29 09:25:00 +0000

    docs: add provide methods to example

--- projects/standalone-app/src/main.ts
+++ projects/standalone-app/src/main.ts
@@ -2,11 +2,14 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { provideStore } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app/app.component';
 
 import { environment } from './environments/environment';
+import { AppEffects } from './app/app.effects';
 
 if (environment.production) {
   enableProdMode();
@@ -25,8 +28,14 @@ bootstrapApplication(AppComponent, {
           },
         ],
         { initialNavigation: 'enabledBlocking' }
-      ),
-      StoreDevtoolsModule.instrument()
+      )
     ),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: environment.production,
+      name: 'NgRx Standalone App',
+    }),
+    provideRouterStore(),
+    provideEffects([AppEffects]),
   ],
 });
