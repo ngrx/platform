49eaf7276d3adeb728bbd1e702cd79bab90df5d4
markostanimirovic <markostanimirovic95@gmail.com>
2022-09-17 00:27:16 +0200

    build: update to TypeScript 4.8

--- modules/effects/src/effect_creator.ts
+++ modules/effects/src/effect_creator.ts
@@ -83,7 +83,7 @@ export function getCreateEffectMetadata<
         instance[propertyName].hasOwnProperty(CREATE_EFFECT_METADATA_KEY)
       ) {
         // If the property type has overridden `hasOwnProperty` we need to ensure
-        // that the metadata is valid (containing a `dispatch`property)
+        // that the metadata is valid (containing a `dispatch` property)
         // https://github.com/ngrx/platform/issues/2975
         const property = instance[propertyName] as any;
         return property[CREATE_EFFECT_METADATA_KEY].hasOwnProperty('dispatch');
