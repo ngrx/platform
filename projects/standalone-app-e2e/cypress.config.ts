0e6dce9365989f77bdb8796e87c5402fd536e12a
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-09-24 16:09:05 +0200

    chore: update cypress

---
+++ projects/standalone-app-e2e/cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+
+const cypressJsonConfig = {
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  video: true,
+  videosFolder: '../../dist/cypress/projects/standalone-app-e2e/videos',
+  screenshotsFolder:
+    '../../dist/cypress/projects/standalone-app-e2e/screenshots',
+  chromeWebSecurity: false,
+  specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
+  supportFile: 'src/support/e2e.ts',
+};
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    ...cypressJsonConfig,
+  },
+});
