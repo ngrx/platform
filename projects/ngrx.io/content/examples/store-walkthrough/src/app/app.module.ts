756c0ee64e6ebba47f877bae3aea1ee38d34f2dd
JR Strayhorn <jrstrayhorn@gmail.com>
2022-10-03 18:58:32 -0400

    making changes from PR review

--- projects/ngrx.io/content/examples/store-walkthrough/src/app/app.module.ts
+++ projects/ngrx.io/content/examples/store-walkthrough/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// #docregion imports
 import { HttpClientModule } from '@angular/common/http';
 import { booksReducer } from './state/books.reducer';
 import { collectionReducer } from './state/collection.reducer';
 import { StoreModule } from '@ngrx/store';
-// #enddocregion imports
 
-// #docregion declareComponents
-import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookCollectionComponent } from './book-collection/book-collection.component';
@@ -17,7 +13,6 @@ import { BookCollectionComponent } from './book-collection/book-collection.compo
 @NgModule({
   imports: [
     BrowserModule,
-    CommonModule,
     StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
     HttpClientModule,
   ],
@@ -25,4 +20,3 @@ import { BookCollectionComponent } from './book-collection/book-collection.compo
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-// #enddocregion declareComponents
