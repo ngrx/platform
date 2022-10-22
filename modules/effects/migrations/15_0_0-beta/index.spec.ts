f7dfeab520a1a52b35032e8c4c265e0f4731dfa8
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-10-23 01:28:03 +0200

    feat(effects): migration for provideEffects argument (#3601)

---
+++ modules/effects/migrations/15_0_0-beta/index.spec.ts
@@ -0,0 +1,134 @@
+import { tags } from '@angular-devkit/core';
+import {
+  SchematicTestRunner,
+  UnitTestTree,
+} from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+import { createWorkspace } from '@ngrx/schematics-core/testing';
+
+describe('Effects Migration 15_0_0-beta', () => {
+  const collectionPath = path.join(__dirname, '../migration.json');
+  const schematicRunner = new SchematicTestRunner('schematics', collectionPath);
+
+  let appTree: UnitTestTree;
+
+  beforeEach(async () => {
+    appTree = await createWorkspace(schematicRunner, appTree);
+  });
+
+  it('replace array in provideEffects', async () => {
+    const input = tags.stripIndent`
+        import { enableProdMode } from '@angular/core';
+        import { bootstrapApplication } from '@angular/platform-browser';
+        import {
+          provideRouter,
+          withEnabledBlockingInitialNavigation,
+        } from '@angular/router';
+        import { provideStore } from '@ngrx/store';
+        import { provideEffects } from '@ngrx/effects';
+        import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+        import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+        import { AppComponent } from './app/app.component';
+
+        import { environment } from './environments/environment';
+        import { AppEffects } from './app/app.effects';
+
+        if (environment.production) {
+          enableProdMode();
+        }
+
+        bootstrapApplication(AppComponent, {
+          providers: [
+            provideStore({ router: routerReducer }),
+            provideRouter(
+              [
+                {
+                  path: 'feature',
+                  loadChildren: () =>
+                    import('./app/lazy/feature.routes').then((m) => m.routes),
+                  providers: [
+                    provideEffects([]),
+                    provideEffects([AppEffects]),
+                    provideEffects([AppEffects1, AppEffect2]),        
+                  ]
+                },
+              ],
+              withEnabledBlockingInitialNavigation()
+            ),
+            provideStoreDevtools({
+              maxAge: 25,
+              logOnly: environment.production,
+              name: 'NgRx Standalone App',
+            }),
+            provideRouterStore(),
+            provideEffects([]),
+            provideEffects([AppEffects]),
+            provideEffects([AppEffects1,AppEffect2]),
+          ],
+        });
+      `;
+
+    const expected = tags.stripIndent`
+        import { enableProdMode } from '@angular/core';
+        import { bootstrapApplication } from '@angular/platform-browser';
+        import {
+          provideRouter,
+          withEnabledBlockingInitialNavigation,
+        } from '@angular/router';
+        import { provideStore } from '@ngrx/store';
+        import { provideEffects } from '@ngrx/effects';
+        import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+        import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+        import { AppComponent } from './app/app.component';
+
+        import { environment } from './environments/environment';
+        import { AppEffects } from './app/app.effects';
+
+        if (environment.production) {
+          enableProdMode();
+        }
+
+        bootstrapApplication(AppComponent, {
+          providers: [
+            provideStore({ router: routerReducer }),
+            provideRouter(
+              [
+                {
+                  path: 'feature',
+                  loadChildren: () =>
+                    import('./app/lazy/feature.routes').then((m) => m.routes),
+                  providers: [
+                    provideEffects(),
+                    provideEffects(AppEffects),
+                    provideEffects(AppEffects1, AppEffect2),        
+                  ]
+                },
+              ],
+              withEnabledBlockingInitialNavigation()
+            ),
+            provideStoreDevtools({
+              maxAge: 25,
+              logOnly: environment.production,
+              name: 'NgRx Standalone App',
+            }),
+            provideRouterStore(),
+            provideEffects(),
+            provideEffects(AppEffects),
+            provideEffects(AppEffects1, AppEffect2),
+          ],
+        });
+      `;
+
+    appTree.create('main.ts', input);
+
+    const tree = await schematicRunner
+      .runSchematicAsync(`ngrx-effects-migration-15-beta`, {}, appTree)
+      .toPromise();
+
+    const actual = tree.readContent('main.ts');
+
+    expect(actual).toBe(expected);
+  });
+});
