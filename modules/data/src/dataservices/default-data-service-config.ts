7194403c3a5c722262286cfc01e7a47e93282ec6
coder925 <coder925@users.noreply.github.com>
2022-12-02 18:31:13 +0100

    docs(data): improve DefaultDataServiceConfig.trailingSlashEndpoints (#3690)
    Closes #3689

--- modules/data/src/dataservices/default-data-service-config.ts
+++ modules/data/src/dataservices/default-data-service-config.ts
@@ -23,6 +23,6 @@ export abstract class DefaultDataServiceConfig {
   saveDelay?: number;
   /** request timeout in MS (default: 0)*/
   timeout?: number; //
-  /** to keep trailing slashes or not; false by default */
+  /** to keep leading & trailing slashes or not; false by default */
   trailingSlashEndpoints?: boolean;
 }
