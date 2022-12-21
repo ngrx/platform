a93e2e773644fe86f43bbdc822ffe6dd766735e5
Daniel Schuba <daniel.schuba@footage.one>
2022-12-21 12:44:54 +0100

    feat(entity): disallow adding ids and entities to additionalState

--- modules/entity/src/entity_state.ts
+++ modules/entity/src/entity_state.ts
@@ -9,7 +9,7 @@ export function getInitialEntityState<V>(): EntityState<V> {
 
 export function createInitialStateFactory<V>() {
   function getInitialState(): EntityState<V>;
-  function getInitialState<S extends object>(
+  function getInitialState<S extends object & { ids: never; entities: never }>(
     additionalState: S
   ): EntityState<V> & S;
   function getInitialState(additionalState: any = {}): any {
