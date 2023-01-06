d9f7f2df16bb1f3b5c49e7e645e74e5e81191738
markostanimirovic <markostanimirovic95@gmail.com>
2023-01-06 15:11:09 +0100

    feat(store): add ability to create derived selectors with createFeature

--- modules/store/spec/types/feature_creator.spec.ts
+++ modules/store/spec/types/feature_creator.spec.ts
@@ -9,8 +9,10 @@ describe('createFeature()', () => {
         createAction,
         createFeature,
         createReducer,
+        createSelector,
         on,
         props,
+        Selector,
         Store,
         StoreModule,
       } from '@ngrx/store';
@@ -82,6 +84,34 @@ describe('createFeature()', () => {
       snippet.toInfer(
         'productsFeatureKeys',
         '"selectProductsState" | "selectQuery" | "selectProducts" | keyof FeatureConfig<"products", State>'
+      );
+    });
+
+    it('should create a feature when reducer is created outside', () => {
+      const snippet = expectSnippet(`
+        const counterReducer = createReducer({ count: 0 });
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: counterReducer,
+        });
+
+        const {
+          name,
+          reducer,
+          selectCounterState,
+          selectCount,
+        } = counterFeature;
+      `);
+
+      snippet.toInfer('name', '"counter"');
+      snippet.toInfer('reducer', 'ActionReducer<{ count: number; }, Action>');
+      snippet.toInfer(
+        'selectCounterState',
+        'MemoizedSelector<Record<string, any>, { count: number; }, DefaultProjectorFn<{ count: number; }>>'
+      );
+      snippet.toInfer(
+        'selectCount',
+        'MemoizedSelector<Record<string, any>, number, DefaultProjectorFn<number>>'
       );
     });
 
@@ -217,6 +247,39 @@ describe('createFeature()', () => {
       );
     });
 
+    it('should create a feature when reducer is created outside', () => {
+      const snippet = expectSnippet(`
+        interface State {
+          bar: string;
+        }
+        const initialState: State = { bar: 'ngrx' };
+
+        const fooReducer = createReducer(initialState);
+        const fooFeature = createFeature<{ foo: State }>({
+          name: 'foo',
+          reducer: fooReducer,
+        });
+
+        const {
+          name,
+          reducer,
+          selectFooState,
+          selectBar,
+        } = fooFeature;
+      `);
+
+      snippet.toInfer('name', '"foo"');
+      snippet.toInfer('reducer', 'ActionReducer<State, Action>');
+      snippet.toInfer(
+        'selectFooState',
+        'MemoizedSelector<{ foo: State; }, State, DefaultProjectorFn<State>>'
+      );
+      snippet.toInfer(
+        'selectBar',
+        'MemoizedSelector<{ foo: State; }, string, DefaultProjectorFn<string>>'
+      );
+    });
+
     it('should fail when name is not key of app state', () => {
       expectSnippet(`
         interface AppState {
@@ -332,6 +395,353 @@ describe('createFeature()', () => {
         'featureKeys',
         '"selectDateState" | keyof FeatureConfig<"date", Date>'
       );
+    });
+  });
+
+  describe('derived selectors', () => {
+    it('should create a feature', () => {
+      const snippet = expectSnippet(`
+        const increment = createAction('increment');
+
+        interface State {
+          count: number;
+        }
+        const initialState: State = {
+          count: 0,
+        };
+
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: createReducer(
+            initialState,
+            on(increment, (state) => ({ count: state.count + 1 }))
+          ),
+          derivedSelectors: ({ selectCounterState, selectCount }) => ({
+            selectCounterState2: createSelector(
+              selectCounterState,
+              (state) => state
+            ),
+            selectCountPlus1: createSelector(
+              selectCount,
+              (count) => count + 1
+            ),
+          }),
+        });
+
+        const {
+          name,
+          reducer,
+          selectCounterState,
+          selectCount,
+          selectCounterState2,
+          selectCountPlus1,
+        } = counterFeature;
+        let counterFeatureKeys: keyof typeof counterFeature;
+      `);
+
+      snippet.toInfer('name', '"counter"');
+      snippet.toInfer('reducer', 'ActionReducer<State, Action>');
+      snippet.toInfer(
+        'selectCounterState',
+        'MemoizedSelector<Record<string, any>, State, DefaultProjectorFn<State>>'
+      );
+      snippet.toInfer(
+        'selectCount',
+        'MemoizedSelector<Record<string, any>, number, DefaultProjectorFn<number>>'
+      );
+      snippet.toInfer(
+        'selectCounterState2',
+        'MemoizedSelector<Record<string, any>, State, (s1: State) => State>'
+      );
+      snippet.toInfer(
+        'selectCountPlus1',
+        'MemoizedSelector<Record<string, any>, number, (s1: number) => number>'
+      );
+      snippet.toInfer(
+        'counterFeatureKeys',
+        '"name" | "reducer" | "selectCounterState" | "selectCount" | "selectCounterState2" | "selectCountPlus1"'
+      );
+    });
+
+    it('should create a feature when reducer is created outside', () => {
+      const snippet = expectSnippet(`
+        const counterReducer = createReducer({ count: 0 });
+
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: counterReducer,
+          derivedSelectors: ({ selectCounterState, selectCount }) => ({
+            selectSquaredCount: createSelector(
+              selectCounterState,
+              selectCount,
+              ({ count }, c) => count * c
+            ),
+          }),
+        });
+
+        const {
+          name,
+          reducer,
+          selectCounterState,
+          selectCount,
+          selectSquaredCount,
+        } = counterFeature;
+        let counterFeatureKeys: keyof typeof counterFeature;
+      `);
+
+      snippet.toInfer('name', '"counter"');
+      snippet.toInfer('reducer', 'ActionReducer<{ count: number; }, Action>');
+      snippet.toInfer(
+        'selectCounterState',
+        'MemoizedSelector<Record<string, any>, { count: number; }, DefaultProjectorFn<{ count: number; }>>'
+      );
+      snippet.toInfer(
+        'selectCount',
+        'MemoizedSelector<Record<string, any>, number, DefaultProjectorFn<number>>'
+      );
+      snippet.toInfer(
+        'selectSquaredCount',
+        'MemoizedSelector<Record<string, any>, number, (s1: { count: number; }, s2: number) => number>'
+      );
+      snippet.toInfer(
+        'counterFeatureKeys',
+        '"name" | "reducer" | "selectCounterState" | "selectCount" | "selectSquaredCount"'
+      );
+    });
+
+    it('should override base selectors if derived selectors have the same names', () => {
+      const snippet = expectSnippet(`
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: createReducer({ count1: 0, count2: 0 }),
+          derivedSelectors: ({ selectCounterState, selectCount1, selectCount2 }) => ({
+            selectCounterState: createSelector(
+              selectCount1,
+              selectCount2,
+              (count3, count4) => ({ count3, count4 })
+            ),
+            selectCount1: createSelector(selectCount1, (count) => count + ''),
+            selectCount10: createSelector(selectCount2, (count) => count + 1),
+          }),
+        });
+
+        const {
+          selectCounterState,
+          selectCount1,
+          selectCount2,
+          selectCount10,
+        } = counterFeature;
+        let counterFeatureKeys: keyof typeof counterFeature;
+      `);
+
+      snippet.toInfer(
+        'selectCounterState',
+        'MemoizedSelector<Record<string, any>, { count3: number; count4: number; }, (s1: number, s2: number) => { count3: number; count4: number; }>'
+      );
+      snippet.toInfer(
+        'selectCount1',
+        'MemoizedSelector<Record<string, any>, string, (s1: number) => string>'
+      );
+      snippet.toInfer(
+        'selectCount2',
+        'MemoizedSelector<Record<string, any>, number, DefaultProjectorFn<number>>'
+      );
+      snippet.toInfer(
+        'selectCount10',
+        'MemoizedSelector<Record<string, any>, number, (s1: number) => number>'
+      );
+      snippet.toInfer(
+        'counterFeatureKeys',
+        '"name" | "reducer" | "selectCounterState" | "selectCount1" | "selectCount2" | "selectCount10"'
+      );
+    });
+
+    it('should not break the feature object when derived selector names are not string literals', () => {
+      const snippet = expectSnippet(`
+        const untypedSelectors: Record<string, Selector<Record<string, any>, unknown>> = {};
+
+        const counterFeature1 = createFeature({
+          name: 'counter1',
+          reducer: createReducer(0),
+          derivedSelectors: ({ selectCounter1State }) => ({
+            ['selectInvalid' as string]: createSelector(
+              selectCounter1State,
+              (count) => count
+            ),
+            selectSquaredCount: createSelector(
+              selectCounter1State,
+              (count) => count * count
+            ),
+            ...untypedSelectors,
+          }),
+        });
+
+        const counterFeature2 = createFeature({
+          name: 'counter2',
+          reducer: createReducer(0),
+          derivedSelectors: () => untypedSelectors,
+        });
+
+        const { selectCounter1State } = counterFeature1;
+        const { selectCounter2State } = counterFeature2;
+
+        let counterFeature1Keys: keyof typeof counterFeature1;
+        let counterFeature2Keys: keyof typeof counterFeature2;
+      `);
+
+      snippet.toInfer(
+        'selectCounter1State',
+        'MemoizedSelector<Record<string, any>, number, DefaultProjectorFn<number>>'
+      );
+      snippet.toInfer(
+        'selectCounter2State',
+        'MemoizedSelector<Record<string, any>, number, DefaultProjectorFn<number>>'
+      );
+      snippet.toInfer(
+        'counterFeature1Keys',
+        '"selectCounter1State" | keyof FeatureConfig<"counter1", number>'
+      );
+      snippet.toInfer(
+        'counterFeature2Keys',
+        '"selectCounter2State" | keyof FeatureConfig<"counter2", number>'
+      );
+    });
+
+    it('should not break the feature object when derived selectors are an empty object', () => {
+      const snippet = expectSnippet(`
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: createReducer(0),
+          derivedSelectors: () => ({}),
+        });
+
+        const { selectCounterState } = counterFeature;
+        let counterFeatureKeys: keyof typeof counterFeature;
+      `);
+
+      snippet.toInfer(
+        'selectCounterState',
+        'MemoizedSelector<Record<string, any>, number, DefaultProjectorFn<number>>'
+      );
+      snippet.toInfer(
+        'counterFeatureKeys',
+        '"name" | "reducer" | "selectCounterState"'
+      );
+    });
+
+    it('should create a feature when derived selectors dictionary is typed as a type', () => {
+      const snippet = expectSnippet(`
+        type DerivedSelectors = {
+          selectCountStr: Selector<Record<string, any>, string>;
+        }
+
+        function getDerivedSelectors(
+          selectCount: Selector<Record<string, any>, number>
+        ): DerivedSelectors {
+          return {
+            selectCountStr: createSelector(
+              selectCount,
+              (count) => count + ''
+            ),
+          };
+        }
+
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: createReducer(0),
+          derivedSelectors: ({ selectCounterState }) =>
+            getDerivedSelectors(selectCounterState),
+        });
+
+        const { selectCountStr } = counterFeature;
+        let counterFeatureKeys: keyof typeof counterFeature;
+      `);
+
+      snippet.toInfer(
+        'selectCountStr',
+        'Selector<Record<string, any>, string>'
+      );
+      snippet.toInfer(
+        'counterFeatureKeys',
+        '"name" | "reducer" | "selectCounterState" | "selectCountStr"'
+      );
+    });
+
+    // This is known behavior.
+    // Record<string, Selector> is not compatible with interface of selectors.
+    it('should fail when derived selectors dictionary is typed as an interface', () => {
+      expectSnippet(`
+        interface DerivedSelectors {
+          selectSquaredCount: Selector<Record<string, any>, number>;
+        }
+
+        function getDerivedSelectors(
+          selectCount: Selector<Record<string, any>, number>
+        ): DerivedSelectors {
+          return {
+            selectSquaredCount: createSelector(
+              selectCount,
+              (count) => count * count
+            ),
+          };
+        }
+
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: createReducer(0),
+          derivedSelectors: ({ selectCounterState }) =>
+            getDerivedSelectors(selectCounterState),
+        });
+      `).toFail(
+        /Index signature for type 'string' is missing in type 'DerivedSelectors'./
+      );
+    });
+
+    it('should fail when derived selectors result is not a dictionary of selectors', () => {
+      expectSnippet(`
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: createReducer(0),
+          derivedSelectors: ({ selectCounterState }) => ({
+            selectSquaredCount: createSelector(
+              selectCounterState,
+              (count) => count * count
+            ),
+            x: 1,
+          }),
+        });
+      `).toFail();
+
+      expectSnippet(`
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: createReducer(0),
+          derivedSelectors: () => 'ngrx',
+        });
+      `).toFail();
+    });
+
+    it('should fail when feature state contains optional properties', () => {
+      expectSnippet(`
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: createReducer({} as { count?: number; }),
+          derivedSelectors: () => ({}),
+        });
+      `).toFail();
+
+      expectSnippet(`
+        interface State {
+          count?: number;
+        }
+        const initialState: State = {};
+
+        const counterFeature = createFeature({
+          name: 'counter',
+          reducer: createReducer(initialState),
+          derivedSelectors: () => ({}),
+        });
+      `).toFail();
     });
   });
 });
--- modules/store/src/feature_creator.ts
+++ modules/store/src/feature_creator.ts
@@ -1,5 +1,5 @@
 import { capitalize } from './helpers';
-import { ActionReducer } from './models';
+import { ActionReducer, Selector } from './models';
 import { isPlainObject } from './meta-reducers/utils';
 import {
   createFeatureSelector,
@@ -8,18 +8,49 @@ import {
 } from './selector';
 import { FeatureSelector, NestedSelectors } from './feature_creator_models';
 
-export type Feature<
-  AppState extends Record<string, any>,
-  FeatureName extends keyof AppState & string,
-  FeatureState extends AppState[FeatureName]
-> = FeatureConfig<FeatureName, FeatureState> &
-  FeatureSelector<AppState, FeatureName, FeatureState> &
-  NestedSelectors<AppState, FeatureState>;
-
 export interface FeatureConfig<FeatureName extends string, FeatureState> {
   name: FeatureName;
   reducer: ActionReducer<FeatureState>;
 }
+
+type Feature<
+  AppState extends Record<string, any>,
+  FeatureName extends keyof AppState & string,
+  FeatureState extends AppState[FeatureName]
+> = FeatureConfig<FeatureName, FeatureState> &
+  BaseSelectors<AppState, FeatureName, FeatureState>;
+
+type FeatureWithDerivedSelectors<
+  FeatureName extends string,
+  FeatureState,
+  DerivedSelectors extends DerivedSelectorsDictionary
+> = string extends keyof DerivedSelectors
+  ? Feature<Record<string, any>, FeatureName, FeatureState>
+  : Omit<
+      Feature<Record<string, any>, FeatureName, FeatureState>,
+      keyof DerivedSelectors
+    > &
+      DerivedSelectors;
+
+type BaseSelectors<
+  AppState extends Record<string, any>,
+  FeatureName extends keyof AppState & string,
+  FeatureState extends AppState[FeatureName]
+> = FeatureSelector<AppState, FeatureName, FeatureState> &
+  NestedSelectors<AppState, FeatureState>;
+
+type DerivedSelectorsDictionary = Record<
+  string,
+  Selector<Record<string, any>, unknown>
+>;
+
+type DerivedSelectorsFactory<
+  FeatureName extends string,
+  FeatureState,
+  DerivedSelectors extends DerivedSelectorsDictionary
+> = (
+  baseSelectors: BaseSelectors<Record<string, any>, FeatureName, FeatureState>
+) => DerivedSelectors;
 
 type NotAllowedFeatureStateCheck<FeatureState> =
   FeatureState extends Required<FeatureState>
@@ -27,13 +58,55 @@ type NotAllowedFeatureStateCheck<FeatureState> =
     : 'optional properties are not allowed in the feature state';
 
 /**
+ * Creates a feature object with derived selectors.
+ *
+ * @param featureConfig An object that contains a feature name, a feature
+ * reducer, and derived selectors factory.
+ * @returns An object that contains a feature name, a feature reducer,
+ * a feature selector, a selector for each feature state property, and
+ * derived selectors.
+ */
+export function createFeature<
+  FeatureName extends string,
+  FeatureState,
+  DerivedSelectors extends DerivedSelectorsDictionary
+>(
+  featureConfig: FeatureConfig<FeatureName, FeatureState> & {
+    derivedSelectors: DerivedSelectorsFactory<
+      FeatureName,
+      FeatureState,
+      DerivedSelectors
+    >;
+  } & NotAllowedFeatureStateCheck<FeatureState>
+): FeatureWithDerivedSelectors<FeatureName, FeatureState, DerivedSelectors>;
+/**
+ * Creates a feature object.
+ *
+ * @param featureConfig An object that contains a feature name and a feature
+ * reducer.
+ * @returns An object that contains a feature name, a feature reducer,
+ * a feature selector, and a selector for each feature state property.
+ */
+export function createFeature<
+  AppState extends Record<string, any>,
+  FeatureName extends keyof AppState & string = keyof AppState & string,
+  FeatureState extends AppState[FeatureName] = AppState[FeatureName]
+>(
+  featureConfig: FeatureConfig<FeatureName, FeatureState> &
+    NotAllowedFeatureStateCheck<FeatureState>
+): Feature<AppState, FeatureName, FeatureState>;
+/**
  * @description
  * A function that accepts a feature name and a feature reducer, and creates
  * a feature selector and a selector for each feature state property.
+ * This function also provides the ability to add derived selectors to
+ * the feature object.
  *
- * @param featureConfig An object that contains a feature name and a feature reducer.
+ * @param featureConfig An object that contains a feature name and a feature
+ * reducer as required, and derived selectors factory as an optional argument.
  * @returns An object that contains a feature name, a feature reducer,
- * a feature selector, and a selector for each feature state property.
+ * a feature selector, a selector for each feature state property, and derived
+ * selectors.
  *
  * @usageNotes
  *
@@ -87,25 +160,88 @@ type NotAllowedFeatureStateCheck<FeatureState> =
  *   selectSelectedId, // type: MemoizedSelector<Record<string, any, string | null>
  * } = productsFeature;
  * ```
+ *
+ * **Creating Feature with Derived Selectors**
+ *
+ * ```ts
+ * type CallState = 'init' | 'loading' | 'loaded' | { error: string };
+ *
+ * interface State extends EntityState<Product> {
+ *   callState: CallState;
+ * }
+ *
+ * const adapter = createEntityAdapter<Product>();
+ * const initialState: State = adapter.getInitialState({
+ *   callState: 'init',
+ * });
+ *
+ * export const productsFeature = createFeature({
+ *   name: 'products',
+ *   reducer: createReducer(initialState),
+ *   derivedSelectors: ({ selectProductsState, selectCallState }) => ({
+ *     ...adapter.getSelectors(selectBooksState),
+ *     ...getCallStateSelectors(selectCallState)
+ *   }),
+ * });
+ *
+ * const {
+ *   name,
+ *   reducer,
+ *   // feature selector
+ *   selectProductsState,
+ *   // feature state properties selectors
+ *   selectIds,
+ *   selectEntities,
+ *   selectCallState,
+ *   // derived selectors returned by `adapter.getSelectors`
+ *   selectAll,
+ *   selectTotal,
+ *   // derived selectors returned by `getCallStateSelectors`
+ *   selectIsLoading,
+ *   selectIsLoaded,
+ *   selectError,
+ * } = productsFeature;
+ * ```
  */
 export function createFeature<
   AppState extends Record<string, any>,
-  FeatureName extends keyof AppState & string = keyof AppState & string,
-  FeatureState extends AppState[FeatureName] = AppState[FeatureName]
+  FeatureName extends keyof AppState & string,
+  FeatureState extends AppState[FeatureName],
+  DerivedSelectors extends Record<
+    string,
+    Selector<Record<string, any>, unknown>
+  >
 >(
-  featureConfig: FeatureConfig<FeatureName, FeatureState> &
-    NotAllowedFeatureStateCheck<FeatureState>
-): Feature<AppState, FeatureName, FeatureState> {
-  const { name, reducer } = featureConfig;
+  featureConfig: FeatureConfig<FeatureName, FeatureState> & {
+    derivedSelectors?: DerivedSelectorsFactory<
+      FeatureName,
+      FeatureState,
+      DerivedSelectors
+    >;
+  }
+): Feature<AppState, FeatureName, FeatureState> & DerivedSelectors {
+  const {
+    name,
+    reducer,
+    derivedSelectors: derivedSelectorsFactory,
+  } = featureConfig;
+
   const featureSelector = createFeatureSelector<FeatureState>(name);
   const nestedSelectors = createNestedSelectors(featureSelector, reducer);
+  const baseSelectors = {
+    [`select${capitalize(name)}State`]: featureSelector,
+    ...nestedSelectors,
+  } as BaseSelectors<Record<string, any>, FeatureName, FeatureState>;
+  const derivedSelectors = derivedSelectorsFactory
+    ? derivedSelectorsFactory(baseSelectors)
+    : {};
 
   return {
     name,
     reducer,
-    [`select${capitalize(name)}State`]: featureSelector,
-    ...nestedSelectors,
-  } as unknown as Feature<AppState, FeatureName, FeatureState>;
+    ...baseSelectors,
+    ...derivedSelectors,
+  } as Feature<AppState, FeatureName, FeatureState> & DerivedSelectors;
 }
 
 function createNestedSelectors<
