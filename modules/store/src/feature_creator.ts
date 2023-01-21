e4f873b5c336629b9c534e1f2d75e16dad2a73e2
Marko Stanimirović <markostanimirovic95@gmail.com>
2023-01-21 02:45:37 +0100

    feat(store): add ability to create extra selectors with createFeature (#3744)
    Closes #3719

--- modules/store/src/feature_creator.ts
+++ modules/store/src/feature_creator.ts
@@ -1,5 +1,5 @@
 import { capitalize } from './helpers';
-import { ActionReducer } from './models';
+import { ActionReducer, Selector } from './models';
 import { isPlainObject } from './meta-reducers/utils';
 import {
   createFeatureSelector,
@@ -8,18 +8,49 @@ import {
 } from './selector';
 import { FeatureSelector, NestedSelectors } from './feature_creator_models';
 
-export type Feature<
-  AppState extends Record<string, any>,
-  FeatureName extends keyof AppState & string,
-  FeatureState extends AppState[FeatureName]
-> = FeatureConfig<FeatureName, FeatureState> &
-  FeatureSelector<AppState, FeatureName, FeatureState> &
-  NestedSelectors<AppState, FeatureState>;
-
 export interface FeatureConfig<FeatureName extends string, FeatureState> {
   name: FeatureName;
   reducer: ActionReducer<FeatureState>;
 }
+
+type Feature<
+  AppState extends Record<string, any>,
+  FeatureName extends keyof AppState & string,
+  FeatureState extends AppState[FeatureName]
+> = FeatureConfig<FeatureName, FeatureState> &
+  BaseSelectors<AppState, FeatureName, FeatureState>;
+
+type FeatureWithExtraSelectors<
+  FeatureName extends string,
+  FeatureState,
+  ExtraSelectors extends SelectorsDictionary
+> = string extends keyof ExtraSelectors
+  ? Feature<Record<string, any>, FeatureName, FeatureState>
+  : Omit<
+      Feature<Record<string, any>, FeatureName, FeatureState>,
+      keyof ExtraSelectors
+    > &
+      ExtraSelectors;
+
+type BaseSelectors<
+  AppState extends Record<string, any>,
+  FeatureName extends keyof AppState & string,
+  FeatureState extends AppState[FeatureName]
+> = FeatureSelector<AppState, FeatureName, FeatureState> &
+  NestedSelectors<AppState, FeatureState>;
+
+type SelectorsDictionary = Record<
+  string,
+  Selector<Record<string, any>, unknown>
+>;
+
+type ExtraSelectorsFactory<
+  FeatureName extends string,
+  FeatureState,
+  ExtraSelectors extends SelectorsDictionary
+> = (
+  baseSelectors: BaseSelectors<Record<string, any>, FeatureName, FeatureState>
+) => ExtraSelectors;
 
 type NotAllowedFeatureStateCheck<FeatureState> =
   FeatureState extends Required<FeatureState>
@@ -27,13 +58,55 @@ type NotAllowedFeatureStateCheck<FeatureState> =
     : 'optional properties are not allowed in the feature state';
 
 /**
+ * Creates a feature object with extra selectors.
+ *
+ * @param featureConfig An object that contains a feature name, a feature
+ * reducer, and extra selectors factory.
+ * @returns An object that contains a feature name, a feature reducer,
+ * a feature selector, a selector for each feature state property, and
+ * extra selectors.
+ */
+export function createFeature<
+  FeatureName extends string,
+  FeatureState,
+  ExtraSelectors extends SelectorsDictionary
+>(
+  featureConfig: FeatureConfig<FeatureName, FeatureState> & {
+    extraSelectors: ExtraSelectorsFactory<
+      FeatureName,
+      FeatureState,
+      ExtraSelectors
+    >;
+  } & NotAllowedFeatureStateCheck<FeatureState>
+): FeatureWithExtraSelectors<FeatureName, FeatureState, ExtraSelectors>;
+/**
+ * Creates a feature object.
+ *
+ * @param featureConfig An object that contains a feature name and a feature
+ * reducer.
+ * @returns An object that contains a feature name, a feature reducer,
+ * a feature selector, and a selector for each feature state property.
+ */
+export function createFeature<
+  AppState extends Record<string, any>,
+  FeatureName extends keyof AppState & string = keyof AppState & string,
+  FeatureState extends AppState[FeatureName] = AppState[FeatureName]
+>(
+  featureConfig: FeatureConfig<FeatureName, FeatureState> &
+    NotAllowedFeatureStateCheck<FeatureState>
+): Feature<AppState, FeatureName, FeatureState>;
+/**
  * @description
  * A function that accepts a feature name and a feature reducer, and creates
  * a feature selector and a selector for each feature state property.
+ * This function also provides the ability to add extra selectors to
+ * the feature object.
  *
- * @param featureConfig An object that contains a feature name and a feature reducer.
+ * @param featureConfig An object that contains a feature name and a feature
+ * reducer as required, and extra selectors factory as an optional argument.
  * @returns An object that contains a feature name, a feature reducer,
- * a feature selector, and a selector for each feature state property.
+ * a feature selector, a selector for each feature state property, and extra
+ * selectors.
  *
  * @usageNotes
  *
@@ -87,25 +160,85 @@ type NotAllowedFeatureStateCheck<FeatureState> =
  *   selectSelectedId, // type: MemoizedSelector<Record<string, any, string | null>
  * } = productsFeature;
  * ```
+ *
+ * **Creating Feature with Extra Selectors**
+ *
+ * ```ts
+ * type CallState = 'init' | 'loading' | 'loaded' | { error: string };
+ *
+ * interface State extends EntityState<Product> {
+ *   callState: CallState;
+ * }
+ *
+ * const adapter = createEntityAdapter<Product>();
+ * const initialState: State = adapter.getInitialState({
+ *   callState: 'init',
+ * });
+ *
+ * export const productsFeature = createFeature({
+ *   name: 'products',
+ *   reducer: createReducer(initialState),
+ *   extraSelectors: ({ selectProductsState, selectCallState }) => ({
+ *     ...adapter.getSelectors(selectBooksState),
+ *     ...getCallStateSelectors(selectCallState)
+ *   }),
+ * });
+ *
+ * const {
+ *   name,
+ *   reducer,
+ *   // feature selector
+ *   selectProductsState,
+ *   // feature state properties selectors
+ *   selectIds,
+ *   selectEntities,
+ *   selectCallState,
+ *   // selectors returned by `adapter.getSelectors`
+ *   selectAll,
+ *   selectTotal,
+ *   // selectors returned by `getCallStateSelectors`
+ *   selectIsLoading,
+ *   selectIsLoaded,
+ *   selectError,
+ * } = productsFeature;
+ * ```
  */
 export function createFeature<
   AppState extends Record<string, any>,
-  FeatureName extends keyof AppState & string = keyof AppState & string,
-  FeatureState extends AppState[FeatureName] = AppState[FeatureName]
+  FeatureName extends keyof AppState & string,
+  FeatureState extends AppState[FeatureName],
+  ExtraSelectors extends SelectorsDictionary
 >(
-  featureConfig: FeatureConfig<FeatureName, FeatureState> &
-    NotAllowedFeatureStateCheck<FeatureState>
-): Feature<AppState, FeatureName, FeatureState> {
-  const { name, reducer } = featureConfig;
+  featureConfig: FeatureConfig<FeatureName, FeatureState> & {
+    extraSelectors?: ExtraSelectorsFactory<
+      FeatureName,
+      FeatureState,
+      ExtraSelectors
+    >;
+  }
+): Feature<AppState, FeatureName, FeatureState> & ExtraSelectors {
+  const {
+    name,
+    reducer,
+    extraSelectors: extraSelectorsFactory,
+  } = featureConfig;
+
   const featureSelector = createFeatureSelector<FeatureState>(name);
   const nestedSelectors = createNestedSelectors(featureSelector, reducer);
+  const baseSelectors = {
+    [`select${capitalize(name)}State`]: featureSelector,
+    ...nestedSelectors,
+  } as BaseSelectors<Record<string, any>, FeatureName, FeatureState>;
+  const extraSelectors = extraSelectorsFactory
+    ? extraSelectorsFactory(baseSelectors)
+    : {};
 
   return {
     name,
     reducer,
-    [`select${capitalize(name)}State`]: featureSelector,
-    ...nestedSelectors,
-  } as unknown as Feature<AppState, FeatureName, FeatureState>;
+    ...baseSelectors,
+    ...extraSelectors,
+  } as Feature<AppState, FeatureName, FeatureState> & ExtraSelectors;
 }
 
 function createNestedSelectors<
