b53e7304f2182286f537586c779e5f96f4227b2d
Ben Lesh <ben@benlesh.com>
2023-04-11 12:47:18 -0500

    feat(concatLatestFrom): Now accepts anything that can be converted to observable
    + removes identity use of `pipe(fn)`.
    + removes use of `of(x).pipe(withLatestFrom(...args)))` in favor of just `forkJoin(args).pipe(map(r => [x, ...r]))` because it's more efficient and does basically the same thing.
    + No longer checks to see if there's an array
    + Changes the typings to use `ObservableInput` rather than `Observable`. The code always supported Promise, Iterable, AsyncIterable, etc, but the TypeScript typings prevented it.

--- modules/effects/src/concat_latest_from.ts
+++ modules/effects/src/concat_latest_from.ts
@@ -1,5 +1,4 @@
-import { Observable, ObservedValueOf, of, OperatorFunction, pipe } from 'rxjs';
-import { concatMap, withLatestFrom } from 'rxjs/operators';
+import { Observable, ObservableInput, ObservedValueOf, OperatorFunction, concatMap, forkJoin } from 'rxjs';
 
 // The array overload is needed first because we want to maintain the proper order in the resulting tuple
 export function concatLatestFrom<T extends Observable<unknown>[], V>(
@@ -38,24 +37,19 @@ export function concatLatestFrom<T extends Observable<unknown>, V>(
  * ```
  */
 export function concatLatestFrom<
-  T extends Observable<unknown>[] | Observable<unknown>,
+  T extends ObservableInput<unknown>[] | ObservableInput<unknown>,
   V,
   R = [
     V,
-    ...(T extends Observable<unknown>[]
+    ...(T extends ObservableInput<unknown>[]
       ? { [i in keyof T]: ObservedValueOf<T[i]> }
       : [ObservedValueOf<T>])
   ]
 >(observablesFactory: (value: V) => T): OperatorFunction<V, R> {
-  return pipe(
-    concatMap((value) => {
-      const observables = observablesFactory(value);
-      const observablesAsArray = Array.isArray(observables)
-        ? observables
-        : [observables];
-      return of(value).pipe(
-        withLatestFrom(...observablesAsArray)
-      ) as unknown as Observable<R>;
-    })
+  return concatMap(
+    (value) =>
+      forkJoin(observablesFactory(value)).pipe(
+        map((others) => [value, ...others])
+      ) as Observable<R>
   );
 }
