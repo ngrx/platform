ded42402f2b4c9fa301468708fdbb771a9e9a1de
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2023-04-12 19:02:26 +0200

    feat(store): add createMockStore migration (#3810)

---
+++ modules/store/migrations/16_0_0-beta/index.spec.ts
@@ -0,0 +1,50 @@
+import { Tree } from '@angular-devkit/schematics';
+import {
+  SchematicTestRunner,
+  UnitTestTree,
+} from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+
+describe('Store Migration 16_0_0-beta', () => {
+  const collectionPath = path.join(__dirname, '../migration.json');
+
+  it(`should replace getMockStore with createMockStore`, async () => {
+    const input = `
+    import { getMockStore } from '@ngrx/store';
+    import { SomethingElse } from '@ngrx/store';
+    import {getMockStore} from '@ngrx/store';
+    import {foo, getMockStore, bar} from '@ngrx/store';
+    
+    const mockStore = getMockStore();
+
+    it('just a test', () => {
+      const s =getMockStore();
+    })
+`;
+
+    const expected = `
+    import {createMockStore } from '@ngrx/store';
+    import { SomethingElse } from '@ngrx/store';
+    import {createMockStore} from '@ngrx/store';
+    import {foo,createMockStore, bar} from '@ngrx/store';
+    
+    const mockStore =createMockStore();
+
+    it('just a test', () => {
+      const s =createMockStore();
+    })
+`;
+    const appTree = new UnitTestTree(Tree.empty());
+    appTree.create('./fixture.ts', input);
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+
+    const newTree = await runner.runSchematic(
+      `ngrx-store-migration-16-0-0-beta`,
+      {},
+      appTree
+    );
+    const file = newTree.readContent('fixture.ts');
+
+    expect(file).toBe(expected);
+  });
+});
