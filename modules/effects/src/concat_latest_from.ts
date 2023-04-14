245349548c7b6842ff5d9dcbfb5c75b5de0967dd
Ben Lesh <ben@benlesh.com>
2023-04-13 20:41:20 -0500

    refactor: Revert to original without the additional `pipe`.
    I'm not super happy with this solution, as it seems like this could all be more efficient. But it's terse and it works.

--- modules/effects/src/concat_latest_from.ts
+++ modules/effects/src/concat_latest_from.ts
@@ -1,5 +1,5 @@
-import { Observable, ObservableInput, ObservedValueOf, OperatorFunction, combineLatest } from 'rxjs';
-import { concatMap, map, take } from 'rxjs/operators';
+import { Observable, ObservableInput, ObservedValueOf, OperatorFunction } from 'rxjs';
+import { concatMap, withLatestFrom } from 'rxjs/operators';
 
 // The array overload is needed first because we want to maintain the proper order in the resulting tuple
 export function concatLatestFrom<T extends Observable<unknown>[], V>(
@@ -47,11 +47,13 @@ export function concatLatestFrom<
       : [ObservedValueOf<T>])
   ]
 >(observablesFactory: (value: V) => T): OperatorFunction<V, R> {
-  return concatMap(
-    (value) =>
-      combineLatest(observablesFactory(value)).pipe(
-        take(1),
-        map((others: unknown[]) => [value, ...others])
-      ) as Observable<R>
-  );
+  return concatMap((value) => {
+    const observables = observablesFactory(value);
+    const observablesAsArray = Array.isArray(observables)
+      ? observables
+      : [observables];
+    return of(value).pipe(
+      withLatestFrom(...observablesAsArray)
+    ) as unknown as Observable<R>;
+  })
 }
