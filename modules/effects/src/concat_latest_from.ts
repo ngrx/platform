dacb9727d95ce3ee0a605d266b3e8104bc2abd9c
Ben Lesh <ben@benlesh.com>
2023-04-14 11:43:47 -0500

    chore: Never again with this GitHub Web IDE.

--- modules/effects/src/concat_latest_from.ts
+++ modules/effects/src/concat_latest_from.ts
@@ -1,4 +1,4 @@
-import { Observable, ObservableInput, ObservedValueOf, OperatorFunction } from 'rxjs';
+import { Observable, ObservableInput, of, ObservedValueOf, OperatorFunction } from 'rxjs';
 import { concatMap, withLatestFrom } from 'rxjs/operators';
 
 // The array overload is needed first because we want to maintain the proper order in the resulting tuple
