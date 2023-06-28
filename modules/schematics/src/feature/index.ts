0460889c28b0a1f95d7a5121cd6f441100b02874
wgd3 <wallace.daniel3@gmail.com>
2023-06-28 14:53:12 -0400

    updated schematic order for feature to match snapshot

--- modules/schematics/src/feature/index.ts
+++ modules/schematics/src/feature/index.ts
@@ -1,16 +1,65 @@
 import {
+  chain,
   Rule,
+  schematic,
   SchematicContext,
   Tree,
-  chain,
-  schematic,
 } from '@angular-devkit/schematics';
+
 import { Schema as FeatureOptions } from './schema';
 
 export default function (options: FeatureOptions): Rule {
   return (host: Tree, context: SchematicContext) => {
     return chain(
-      [
+      (options.entity
+        ? [
+            schematic('entity', {
+              name: options.name,
+              path: options.path,
+              project: options.project,
+              flat: options.flat,
+              skipTests: options.skipTests,
+              module: options.module,
+              reducers: options.reducers,
+              group: options.group,
+              feature: true,
+            }),
+          ]
+        : [
+            schematic('action', {
+              flat: options.flat,
+              group: options.group,
+              name: options.name,
+              path: options.path,
+              project: options.project,
+              skipTests: options.skipTests,
+              api: options.api,
+              prefix: options.prefix,
+            }),
+            schematic('reducer', {
+              flat: options.flat,
+              group: options.group,
+              module: options.module,
+              name: options.name,
+              path: options.path,
+              project: options.project,
+              skipTests: options.skipTests,
+              reducers: options.reducers,
+              feature: true,
+              api: options.api,
+              prefix: options.prefix,
+            }),
+            schematic('selector', {
+              flat: options.flat,
+              group: options.group,
+              name: options.name,
+              path: options.path,
+              project: options.project,
+              skipTests: options.skipTests,
+              feature: true,
+            }),
+          ]
+      ).concat([
         schematic('effect', {
           flat: options.flat,
           group: options.group,
@@ -23,56 +72,7 @@ export default function (options: FeatureOptions): Rule {
           api: options.api,
           prefix: options.prefix,
         }),
-      ].concat(
-        options.entity
-          ? [
-              schematic('entity', {
-                name: options.name,
-                path: options.path,
-                project: options.project,
-                flat: options.flat,
-                skipTests: options.skipTests,
-                module: options.module,
-                reducers: options.reducers,
-                group: options.group,
-                feature: true,
-              }),
-            ]
-          : [
-              schematic('action', {
-                flat: options.flat,
-                group: options.group,
-                name: options.name,
-                path: options.path,
-                project: options.project,
-                skipTests: options.skipTests,
-                api: options.api,
-                prefix: options.prefix,
-              }),
-              schematic('reducer', {
-                flat: options.flat,
-                group: options.group,
-                module: options.module,
-                name: options.name,
-                path: options.path,
-                project: options.project,
-                skipTests: options.skipTests,
-                reducers: options.reducers,
-                feature: true,
-                api: options.api,
-                prefix: options.prefix,
-              }),
-              schematic('selector', {
-                flat: options.flat,
-                group: options.group,
-                name: options.name,
-                path: options.path,
-                project: options.project,
-                skipTests: options.skipTests,
-                feature: true,
-              }),
-            ]
-      )
+      ])
     )(host, context);
   };
 }
