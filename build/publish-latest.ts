d2502fbaf027898820a53a8b99f9ebb22b8231ee
Brandon Roberts <robertsbt@gmail.com>
2023-08-09 12:15:49 -0500

    build: update packages for npm publishing (#3999)

--- build/publish-latest.ts
+++ build/publish-latest.ts
@@ -1,17 +1,23 @@
 import { createBuilder } from './util';
 import { packages } from './config';
 import * as shelljs from 'shelljs';
-import { readFileSync } from 'fs-extra';
 
 /**
  * Publish release to NPM on "latest" tag
  */
 export async function publishLatestToNpm() {
-  const configJSON = JSON.parse(readFileSync('./angular.json').toString());
-  const projects = configJSON.projects;
-  const publishablePackages = Object.keys(projects).filter(
-    (project) => !!projects[project].architect['build-package']
-  );
+  const publishablePackages = [
+    'store',
+    'effects',
+    'router-store',
+    'store-devtools',
+    'entity',
+    'component-store',
+    'component',
+    'schematics',
+    'eslint-plugin',
+    'data',
+  ];
 
   for (let pkg of publishablePackages) {
     console.log(`Publishing @ngrx/${pkg}`);
