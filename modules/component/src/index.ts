7316d1a11a8734a575c74864b4f2dadf85746697
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2023-10-21 19:38:11 +0200

    feat(component): remove PushModule
    BREAKING CHANGE:
    
    The `PushModule` is deprecated in favor of the standalone `PushPipe`.
    
    BEFORE:
    
    ```ts
    import { PushModule } from '@ngrx/component';
    
    @NgModule({
      imports: [
        // ... other imports
        PushModule,
      ],
    })
    export class MyFeatureModule {}
    ```
    
    AFTER:
    
    ```ts
    import { Component } from '@angular/core';
    import { PushPipe } from '@ngrx/component';
    
    @Component({
      // ... other metadata
      standalone: true,
      imports: [
        // ... other imports
        PushPipe,
      ],
    })
    export class MyStandaloneComponent {}
    ```

--- modules/component/src/index.ts
+++ modules/component/src/index.ts
@@ -2,4 +2,3 @@ export { RenderScheduler } from './core/render-scheduler';
 export { LetDirective } from './let/let.directive';
 export { LetModule } from './let/let.module';
 export { PushPipe } from './push/push.pipe';
-export { PushModule } from './push/push.module';
