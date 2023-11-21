beb4dc9965c68caa51b73fa42d8bddcc0c5cf525
Jits <dev@jits.co.uk>
2023-11-21 12:01:14 +0000

    fix(signals): `getState` now returns computed signals too

--- modules/signals/spec/get-state.spec.ts
+++ modules/signals/spec/get-state.spec.ts
@@ -1,6 +1,12 @@
-import { effect } from '@angular/core';
+import { computed, effect } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { getState, patchState, signalStore, withState } from '../src';
+import {
+  getState,
+  patchState,
+  signalStore,
+  withComputed,
+  withState,
+} from '../src';
 
 describe('getState', () => {
   const initialState = {
@@ -45,6 +51,36 @@ describe('getState', () => {
 
       TestBed.flushEffects();
       expect(executionCount).toBe(2);
+    });
+
+    it('outputs the values of computed signals', () => {
+      const Store = signalStore(
+        withState(initialState),
+        withComputed((store) => ({
+          fullName: computed(
+            () => `${store.user.firstName()} ${store.user.lastName()}`
+          ),
+        }))
+      );
+      const store = new Store();
+
+      expect(getState(store)).toEqual({
+        ...initialState,
+        fullName: 'John Smith',
+      });
+
+      patchState(store, { user: { firstName: 'Jane', lastName: 'Doe' } });
+
+      console.log(getState(store));
+
+      expect(getState(store)).toEqual({
+        ...initialState,
+        user: {
+          firstName: 'Jane',
+          lastName: 'Doe',
+        },
+        fullName: 'Jane Doe',
+      });
     });
   });
 });
--- modules/signals/src/get-state.ts
+++ modules/signals/src/get-state.ts
@@ -1,7 +1,24 @@
+import { SIGNAL } from '@angular/core/primitives/signals';
+import { SignalsDictionary } from './signal-store-models';
 import { STATE_SIGNAL, SignalStateMeta } from './signal-state';
+import { Prettify } from './ts-helpers';
 
-export function getState<State extends object>(
-  signalState: SignalStateMeta<State>
-): State {
-  return signalState[STATE_SIGNAL]();
+export function getState<State extends Record<string, unknown>>(
+  store: Prettify<SignalStateMeta<State> & SignalsDictionary>
+): Record<string, unknown> {
+  const state: Record<string, unknown> = store[STATE_SIGNAL]();
+
+  return Object.keys(store).reduce((acc, key) => {
+    // Ignore values already existing from the initial state
+    if (key in state) {
+      return acc;
+    }
+
+    const value = store[key];
+    if (SIGNAL in value) {
+      acc[key] = value();
+    }
+
+    return acc;
+  }, state);
 }
