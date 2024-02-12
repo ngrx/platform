b90da9d338569db3de75e46515fa19ee8989c8a0
Tomasz Ducin <tomasz.ducin@gmail.com>
2024-02-12 16:10:45 +0100

    perf(signals): avoid creating unnecessary objects in excludeKeys (#4240)
    Co-authored-by: Tomasz Ducin <tomasz@ducin.it>

--- modules/signals/src/helpers.ts
+++ modules/signals/src/helpers.ts
@@ -2,8 +2,10 @@ export function excludeKeys<
   Obj extends Record<string, unknown>,
   Keys extends string[]
 >(obj: Obj, keys: Keys): Omit<Obj, Keys[number]> {
-  return Object.keys(obj).reduce(
-    (acc, key) => (keys.includes(key) ? acc : { ...acc, [key]: obj[key] }),
-    {}
-  ) as Omit<Obj, Keys[number]>;
+  return Object.keys(obj).reduce<Record<string, unknown>>((acc, key) => {
+    if (!keys.includes(key)) {
+      acc[key] = obj[key];
+    }
+    return acc;
+  }, {}) as Omit<Obj, Keys[number]>;
 }
