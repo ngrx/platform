3d45e5aff70092dbe325ceab8d4ffc6692e30435
Tomasz Ducin <tomasz@ducin.dev>
2024-02-21 18:13:01 +0100

    fix(signals): add `StateSignal` to the public API (#4247)

--- modules/signals/src/index.ts
+++ modules/signals/src/index.ts
@@ -4,6 +4,7 @@ export { signalState } from './signal-state';
 export { signalStore } from './signal-store';
 export { signalStoreFeature, type } from './signal-store-feature';
 export { SignalStoreFeature } from './signal-store-models';
+export { StateSignal } from './state-signal';
 
 export { withComputed } from './with-computed';
 export { withHooks } from './with-hooks';
