015b8640f9b0ec0065a6459ebe0ac28a4976e9ff
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-03-15 20:42:12 +0100

    test: fix tests after upgrade

--- modules/effects/test-setup.ts
+++ modules/effects/test-setup.ts
@@ -1,1 +1,4 @@
 import 'jest-preset-angular/setup-jest';
+import { TextEncoder, TextDecoder } from 'util';
+
+Object.assign(global, { TextDecoder, TextEncoder });
--- modules/signals/test-setup.ts
+++ modules/signals/test-setup.ts
@@ -1,2 +1,5 @@
 import 'zone.js/plugins/zone-legacy';
 import 'jest-preset-angular/setup-jest';
+import { TextEncoder, TextDecoder } from 'util';
+
+Object.assign(global, { TextDecoder, TextEncoder });
