9c3f8456eee21d952f38afab1c6b0fb3adea2868
Lars Hanisch <flensrocker@users.noreply.github.com>
2024-04-10 21:06:10 +0200

    refactor(signals): simpler entity named types (#4271)

--- modules/signals/entities/src/models.ts
+++ modules/signals/entities/src/models.ts
@@ -10,15 +10,15 @@ export type EntityState<Entity> = {
 };
 
 export type NamedEntityState<Entity, Collection extends string> = {
-  [K in Collection as `${K}EntityMap`]: EntityMap<Entity>;
-} & { [K in Collection as `${K}Ids`]: EntityId[] };
+  [K in keyof EntityState<Entity> as `${Collection}${Capitalize<K>}`]: EntityState<Entity>[K];
+};
 
 export type EntitySignals<Entity> = {
   entities: Signal<Entity[]>;
 };
 
 export type NamedEntitySignals<Entity, Collection extends string> = {
-  [K in Collection as `${K}Entities`]: Signal<Entity[]>;
+  [K in keyof EntitySignals<Entity> as `${Collection}${Capitalize<K>}`]: EntitySignals<Entity>[K];
 };
 
 export type EntityIdProps<Entity> = {
