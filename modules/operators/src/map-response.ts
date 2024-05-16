afc14de328dc321cf3fcfeaeadd33f8b7481d6fe
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-05-16 20:51:45 +0200

    docs(operators): add JSDoc for `mapResponse` (#4337)
    Co-authored-by: Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>

--- modules/operators/src/map-response.ts
+++ modules/operators/src/map-response.ts
@@ -6,6 +6,32 @@ type MapResponseObserver<T, E, R1, R2> = {
   error: (error: E) => R2;
 };
 
+/**
+ * `mapResponse` is a map operator with included error handling.
+ * It is similar to `tapResponse`, but allows to map the response as well.
+ *
+ * The main use case is for NgRx Effects which requires an action to be dispatched.
+ *
+ * @usageNotes
+ * ```ts
+ * export const loadAllUsers = createEffect((
+ *   actions$ = inject(Actions),
+ *   usersService = inject(UsersService)
+ * ) => {
+ *   return actions$.pipe(
+ *     ofType(UsersPageActions.opened),
+ *     exhaustMap(() => {
+ *       return usersService.getAll().pipe(
+ *         mapResponse({
+ *           next: (users) => UsersApiActions.usersLoadedSuccess({ users }),
+ *           error: (error) => UsersApiActions.usersLoadedFailure({ error }),
+ *         })
+ *       );
+ *     })
+ *   );
+ * });
+ * ```
+ */
 export function mapResponse<T, E, R1, R2>(
   observer: MapResponseObserver<T, E, R1, R2>
 ): (source$: Observable<T>) => Observable<R1 | R2> {
