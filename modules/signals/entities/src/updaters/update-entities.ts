9990abd78e0859473c17bae23dfe3e5d8725d53e
markostanimirovic <markostanimirovic95@gmail.com>
2024-06-29 14:08:41 +0200

    fix(signals): allow modifying entity id on update
    Closes #4235

--- modules/signals/entities/src/updaters/update-entities.ts
+++ modules/signals/entities/src/updaters/update-entities.ts
@@ -5,22 +5,16 @@ import {
   EntityPredicate,
   EntityState,
   NamedEntityState,
+  SelectEntityId,
 } from '../models';
 import {
   cloneEntityState,
+  getEntityIdSelector,
   getEntityStateKeys,
   getEntityUpdaterResult,
   updateEntitiesMutably,
 } from '../helpers';
 
-export function updateEntities<Entity>(update: {
-  ids: EntityId[];
-  changes: EntityChanges<Entity & {}>;
-}): PartialStateUpdater<EntityState<Entity>>;
-export function updateEntities<Entity>(update: {
-  predicate: EntityPredicate<Entity>;
-  changes: EntityChanges<Entity & {}>;
-}): PartialStateUpdater<EntityState<Entity>>;
 export function updateEntities<
   Collection extends string,
   State extends NamedEntityState<any, Collection>,
@@ -28,9 +22,12 @@ export function updateEntities<
 >(
   update: {
     ids: EntityId[];
-    changes: EntityChanges<Entity & {}>;
+    changes: EntityChanges<NoInfer<Entity>>;
   },
-  config: { collection: Collection }
+  config: {
+    collection: Collection;
+    selectId: SelectEntityId<NoInfer<Entity>>;
+  }
 ): PartialStateUpdater<State>;
 export function updateEntities<
   Collection extends string,
@@ -39,16 +36,74 @@ export function updateEntities<
 >(
   update: {
     predicate: EntityPredicate<Entity>;
-    changes: EntityChanges<Entity & {}>;
+    changes: EntityChanges<NoInfer<Entity>>;
+  },
+  config: {
+    collection: Collection;
+    selectId: SelectEntityId<NoInfer<Entity>>;
+  }
+): PartialStateUpdater<State>;
+export function updateEntities<
+  Collection extends string,
+  State extends NamedEntityState<any, Collection>,
+  Entity = State extends NamedEntityState<
+    infer E extends { id: EntityId },
+    Collection
+  >
+    ? E
+    : never
+>(
+  update: {
+    ids: EntityId[];
+    changes: EntityChanges<NoInfer<Entity>>;
   },
   config: { collection: Collection }
 ): PartialStateUpdater<State>;
+export function updateEntities<
+  Collection extends string,
+  State extends NamedEntityState<any, Collection>,
+  Entity = State extends NamedEntityState<
+    infer E extends { id: EntityId },
+    Collection
+  >
+    ? E
+    : never
+>(
+  update: {
+    predicate: EntityPredicate<Entity>;
+    changes: EntityChanges<NoInfer<Entity>>;
+  },
+  config: { collection: Collection }
+): PartialStateUpdater<State>;
+export function updateEntities<Entity>(
+  update: {
+    ids: EntityId[];
+    changes: EntityChanges<NoInfer<Entity>>;
+  },
+  config: { selectId: SelectEntityId<NoInfer<Entity>> }
+): PartialStateUpdater<EntityState<Entity>>;
+export function updateEntities<Entity>(
+  update: {
+    predicate: EntityPredicate<Entity>;
+    changes: EntityChanges<NoInfer<Entity>>;
+  },
+  config: { selectId: SelectEntityId<NoInfer<Entity>> }
+): PartialStateUpdater<EntityState<Entity>>;
+export function updateEntities<Entity extends { id: EntityId }>(update: {
+  ids: EntityId[];
+  changes: EntityChanges<NoInfer<Entity>>;
+}): PartialStateUpdater<EntityState<Entity>>;
+export function updateEntities<Entity extends { id: EntityId }>(update: {
+  predicate: EntityPredicate<Entity>;
+  changes: EntityChanges<NoInfer<Entity>>;
+}): PartialStateUpdater<EntityState<Entity>>;
 export function updateEntities(
   update: ({ ids: EntityId[] } | { predicate: EntityPredicate<any> }) & {
     changes: EntityChanges<any>;
   },
-  config?: { collection?: string }
+  config?: { collection?: string; selectId?: SelectEntityId<any> }
 ): PartialStateUpdater<EntityState<any> | NamedEntityState<any, string>> {
+  const selectId = getEntityIdSelector(config);
   const stateKeys = getEntityStateKeys(config);
   const idsOrPredicate = 'ids' in update ? update.ids : update.predicate;
 
@@ -57,7 +112,8 @@ export function updateEntities(
     const didMutate = updateEntitiesMutably(
       clonedState,
       idsOrPredicate,
-      update.changes
+      update.changes,
+      selectId
     );
 
     return getEntityUpdaterResult(clonedState, stateKeys, didMutate);
