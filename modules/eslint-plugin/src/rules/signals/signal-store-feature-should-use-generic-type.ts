e2ab916bead64041387ca835494c72652a05c3ad
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-07-23 18:49:20 +0200

    feat(eslint-plugin): add signalStoreFeatureShouldUseGenericType rule (#4454)
    Closes #4438

---
+++ modules/eslint-plugin/src/rules/signals/signal-store-feature-should-use-generic-type.ts
@@ -0,0 +1,101 @@
+import { ESLintUtils, type TSESTree } from '@typescript-eslint/utils';
+import * as path from 'path';
+import { createRule } from '../../rule-creator';
+import {
+  isArrowFunctionExpression,
+  isCallExpression,
+  isFunctionDeclaration,
+  isIdentifier,
+} from '../../utils';
+
+export const messageId = 'signalStoreFeatureShouldUseGenericType';
+
+type MessageIds = typeof messageId;
+type Options = readonly [];
+
+export default createRule<Options, MessageIds>({
+  name: path.parse(__filename).name,
+  meta: {
+    type: 'problem',
+    ngrxModule: 'signals',
+    docs: {
+      description: `A custom Signal Store feature that accepts an input should define a generic type.`,
+    },
+    fixable: 'code',
+    schema: [],
+    messages: {
+      [messageId]: `Add an unused generic type to the function creating the signal store feature.`,
+    },
+  },
+  defaultOptions: [],
+  create: (context) => {
+    function report(
+      signalStoreFeature: TSESTree.CallExpression,
+      func?: TSESTree.Node
+    ) {
+      if (
+        !func ||
+        (!isFunctionDeclaration(func) && !isArrowFunctionExpression(func))
+      ) {
+        return;
+      }
+      const parentHasGenerics =
+        func.typeParameters && func.typeParameters.params.length > 0;
+      if (!parentHasGenerics) {
+        context.report({
+          node: signalStoreFeature.callee,
+          messageId,
+          fix(fixer) {
+            if (isFunctionDeclaration(func)) {
+              if (func.id) {
+                return fixer.insertTextAfter(func.id, '<_>');
+              }
+            }
+
+            return fixer.insertTextBefore(func, '<_>');
+          },
+        });
+      }
+    }
+
+    function hasInputAsArgument(node: TSESTree.CallExpression) {
+      const [inputArg] = node.arguments;
+      return (
+        !isCallExpression(inputArg) ||
+        (isIdentifier(inputArg.callee) && inputArg.callee.name === 'type')
+      );
+    }
+
+    return {
+      [`ArrowFunctionExpression > CallExpression[callee.name=signalStoreFeature]`](
+        node: TSESTree.CallExpression
+      ) {
+        if (hasInputAsArgument(node)) {
+          report(node, node.parent);
+        }
+      },
+      [`ArrowFunctionExpression > BlockStatement CallExpression[callee.name=signalStoreFeature]`](
+        node: TSESTree.CallExpression
+      ) {
+        if (hasInputAsArgument(node)) {
+          let parent: TSESTree.Node | undefined = node.parent;
+          while (parent && !isArrowFunctionExpression(parent)) {
+            parent = parent.parent;
+          }
+          report(node, parent);
+        }
+      },
+      [`FunctionDeclaration > BlockStatement CallExpression[callee.name=signalStoreFeature]`](
+        node: TSESTree.CallExpression
+      ) {
+        if (hasInputAsArgument(node)) {
+          let parent: TSESTree.Node | undefined = node.parent;
+          while (parent && !isFunctionDeclaration(parent)) {
+            parent = parent.parent;
+          }
+          report(node, parent);
+        }
+      },
+    };
+  },
+});
