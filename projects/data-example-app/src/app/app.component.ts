4eef57e1d08b18453afb180be9c1c2e4df804f16
Paweł Twardziak <pawel.twardziak.dev@gmail.com>
2024-11-14 23:15:01 +0100

    chore(example-app): remove data-example-app
    Closes #4586

--- projects/data-example-app/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngrx-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  standalone: false,
-})
-export class AppComponent {}
