7b513ba76c0c8963a96ae3a8c34faa72b6b03fdb
adxdits <adamalmounayar@hotmail.com>
2025-02-12 11:08:32 +0100

    test(eslint-plugin): add tests for ESLint plugin schematics

--- modules/eslint-plugin/spec/schematics/ng-add.spec.ts
+++ modules/eslint-plugin/spec/schematics/ng-add.spec.ts
@@ -1,4 +1,4 @@
-import { Tree } from '@angular-devkit/schematics';
+import { Tree, HostTree } from '@angular-devkit/schematics';
 import {
   SchematicTestRunner,
   UnitTestTree,
@@ -49,7 +49,6 @@ test('registers the plugin with a different config', async () => {
 test('registers the plugin in overrides when it supports TS', async () => {
   const appTree = new UnitTestTree(Tree.empty());
 
-  // this is a trimmed down version of the default angular-eslint schematic
   const initialConfig = {
     overrides: [
       {
@@ -148,4 +147,74 @@ test('does not add the plugin if it is already added manually as an override', a
   const eslintContent = appTree.readContent(`.eslintrc.json`);
   const eslintJson = JSON.parse(eslintContent);
   expect(eslintJson).toEqual(initialConfig);
+});
+
+describe('addNgRxESLintPlugin (flat config)', () => {
+  const flatSchematicRunner = new SchematicTestRunner(
+    '@ngrx/eslint-plugin',
+    path.join(__dirname, '../../schematics/collection.json')
+  );
+  let host: UnitTestTree;
+
+  beforeEach(() => {
+    host = new UnitTestTree(new HostTree());
+  });
+
+  it('should add NgRx plugin to flat ESLint config', async () => {
+    host.create('eslint.config.js', `
+      import somePlugin from 'some-plugin';
+      export default [];
+    `);
+
+    await flatSchematicRunner.runSchematic('add-ngrx-eslint-plugin', { config: 'recommended' }, host);
+
+    const content = host.readText('eslint.config.js');
+    expect(content).toContain("import ngrxEslintPlugin from '@ngrx/eslint-plugin';");
+    expect(content).toContain("extends: [`plugin:@ngrx/recommended`]");
+  });
+
+  it('should not add duplicate NgRx plugin entries in flat config', async () => {
+    host.create('eslint.config.js', `
+      import ngrxEslintPlugin from '@ngrx/eslint-plugin';
+      export default [{
+        files: ['*.ts'],
+        extends: ['plugin:@ngrx/recommended']
+      }];
+    `);
+
+    await flatSchematicRunner.runSchematic('add-ngrx-eslint-plugin', { config: 'recommended' }, host);
+
+    const content = host.readText('eslint.config.js');
+    const ngrxConfigCount = (content.match(/plugin:@ngrx\/recommended/g) || []).length;
+    expect(ngrxConfigCount).toBe(1);
+  });
+
+  it('should prioritize flat config over JSON config', async () => {
+    // Create both config files
+    host.create('eslint.config.js', 'export default [];');
+    host.create('.eslintrc.json', JSON.stringify({}));
+
+    await flatSchematicRunner.runSchematic('add-ngrx-eslint-plugin', { config: 'recommended' }, host);
+
+    // Verify flat config was modified
+    const flatConfig = host.readText('eslint.config.js');
+    expect(flatConfig).toContain('@ngrx/eslint-plugin');
+
+    // Verify JSON config was NOT modified
+    const jsonConfig = host.readText('.eslintrc.json');
+    expect(jsonConfig).not.toContain('@ngrx/eslint-plugin');
+  });
+
+  it('should modify JSON config when flat config is missing', async () => {
+    host.create('.eslintrc.json', JSON.stringify({
+      overrides: []
+    }));
+
+    await flatSchematicRunner.runSchematic('add-ngrx-eslint-plugin', { config: 'recommended' }, host);
+
+    const content = JSON.parse(host.readText('.eslintrc.json'));
+    expect(content.overrides).toContain(jasmine.objectContaining({
+      extends: ['plugin:@ngrx/recommended']
+    }));
+  });
 });
