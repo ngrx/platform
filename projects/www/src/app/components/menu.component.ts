4397bfb4783920779c9f816aa620ef34d3d35fa9
DuskoPeric <dule@teretane.net>
2025-06-05 15:43:53 +0200

    feat(www): add sidebar for mobile view and make home page responsive (#4813)
    Closes #4807

--- projects/www/src/app/components/menu.component.ts
+++ projects/www/src/app/components/menu.component.ts
@@ -1,48 +1,136 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  effect,
+  ElementRef,
+  HostListener,
+  inject,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { GuideSectionComponent } from './guide-section.component';
 import { GuideMenuService } from '../services/guide-menu.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'ngrx-menu',
   standalone: true,
   imports: [MatIconModule, RouterLink, RouterLinkActive, GuideSectionComponent],
   template: `
-    <a routerLink="" class="logoLink">
-      <img src="/ngrx-logo-pink.svg" alt="ngrx logo" />
-      NgRx
-    </a>
-    <hr />
-    <a routerLink="/workshops" routerLinkActive="active" class="menu-link">
-      <mat-icon>co_present</mat-icon>
-      Workshops
-    </a>
-    <a routerLink="/api" routerLinkActive="active" class="menu-link">
-      <mat-icon>description</mat-icon>
-      API Reference
-    </a>
-    <a routerLink="/support" routerLinkActive="active" class="menu-link">
-      <mat-icon>help</mat-icon>
-      Support
-    </a>
-    <a
-      href="https://github.com/ngrx/platform"
-      target="__blank"
-      class="menu-link"
-    >
-      <mat-icon>code</mat-icon>
-      GitHub
-    </a>
-    <hr />
-    <span class="guideHeader">Guide</span>
-    <ngrx-guide-section
-      [section]="guideMenu.getMenu()"
-      [collapsible]="false"
-    ></ngrx-guide-section>
+    <div class="mobile-nav-bar">
+      <button class="menu-toggle" #toggleBtnRef (click)="toggleMenu()">
+        <img src="/ngrx-logo-pink.svg" alt="ngrx logo" />
+        <mat-icon>menu</mat-icon>
+      </button>
+    </div>
+    <nav class="sidebar" #sidebarRef [class.open]="isMenuOpen()">
+      <button class="close-menu" (click)="closeMenu()">
+        <mat-icon class="close-menu-icon">close</mat-icon>
+      </button>
+      <a routerLink="" class="logoLink" (click)="closeMenu()">
+        <img src="/ngrx-logo-pink.svg" alt="ngrx logo" />
+        NgRx
+      </a>
+      <hr />
+      <a
+        routerLink="/workshops"
+        routerLinkActive="active"
+        class="menu-link"
+        (click)="closeMenu()"
+      >
+        <mat-icon>co_present</mat-icon>
+        Workshops
+      </a>
+      <a
+        routerLink="/api"
+        routerLinkActive="active"
+        class="menu-link"
+        (click)="closeMenu()"
+      >
+        <mat-icon>description</mat-icon>
+        API Reference
+      </a>
+      <a routerLink="/support" routerLinkActive="active" class="menu-link">
+        <mat-icon>help</mat-icon>
+        Support
+      </a>
+      <a
+        href="https://github.com/ngrx/platform"
+        target="__blank"
+        class="menu-link"
+      >
+        <mat-icon>code</mat-icon>
+        GitHub
+      </a>
+      <hr />
+      <span class="guideHeader">Guide</span>
+      <ngrx-guide-section
+        [section]="guideMenu.getMenu()"
+        [collapsible]="false"
+      ></ngrx-guide-section>
+    </nav>
   `,
   styles: [
     `
+      .mobile-nav-bar {
+        position: fixed;
+        top: 0;
+        display: none;
+        background-color: #17111a;
+        width: 100%;
+        padding: 15px 20px;
+        .menu-toggle {
+          display: flex;
+          align-items: center;
+          background-color: transparent;
+          border: none;
+          cursor: pointer;
+          img {
+            width: 30px;
+            margin-right: 8px;
+          }
+        }
+        @media only screen and (max-width: 1280px) {
+          display: block;
+        }
+      }
+      .sidebar {
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+        padding: 32px 24px;
+        border-right: 1px solid rgba(255, 255, 255, 0.12);
+        @media only screen and (max-width: 1280px) {
+          background-color: #17111a;
+          position: fixed;
+          top: 0;
+          left: -270px;
+          transition: left 0.3s ease-in-out;
+          width: 270px;
+          height: 100lvh;
+          overflow-y: scroll;
+        }
+        &.open {
+          @media only screen and (max-width: 1280px) {
+            display: flex;
+            left: 0;
+          }
+        }
+        .close-menu {
+          display: none;
+          background-color: transparent;
+          border: none;
+          width: 25px;
+          padding: 0;
+          @media only screen and (max-width: 1280px) {
+            display: block;
+          }
+          .close-menu-icon {
+            cursor: pointer;
+          }
+        }
+      }
       .logoLink {
         font-family: 'Oxanium', sans-serif;
         font-weight: 600;
@@ -59,15 +147,17 @@ import { GuideMenuService } from '../services/guide-menu.service';
       }
 
       :host {
-        display: flex;
-        flex-direction: column;
+        z-index: 2;
         width: 270px;
         height: 100lvh;
-        gap: 16px;
-        padding: 32px 24px;
         background-color: #17111a;
-        border-right: 1px solid rgba(255, 255, 255, 0.12);
         overflow-y: scroll;
+        border-right: 1px solid rgba(255, 255, 255, 0.12);
+        @media only screen and (max-width: 1280px) {
+          border-right: none;
+          width: 0px;
+          padding: 0px;
+        }
       }
 
       .menu-link {
@@ -117,4 +207,40 @@ import { GuideMenuService } from '../services/guide-menu.service';
 })
 export class MenuComponent {
   guideMenu = inject(GuideMenuService);
+  isMenuOpen = signal(false);
+
+  sidebarRef = viewChild.required<ElementRef>('sidebarRef');
+  toggleBtnRef = viewChild.required<ElementRef>('toggleBtnRef');
+
+  document = inject(DOCUMENT);
+
+  constructor() {
+    effect(() => {
+      if (this.isMenuOpen()) {
+        this.document.body.classList.add('no-scroll');
+      } else {
+        this.document.body.classList.remove('no-scroll');
+      }
+    });
+  }
+
+  toggleMenu() {
+    this.isMenuOpen.set(!this.isMenuOpen());
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeSidebarWhenClickedOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const isSidebarClicked = this.sidebarRef().nativeElement.contains(target);
+    const isToggleBtnClicked =
+      this.toggleBtnRef().nativeElement.contains(target);
+
+    if (!isSidebarClicked && !isToggleBtnClicked && this.isMenuOpen()) {
+      this.closeMenu();
+    }
+  }
 }
