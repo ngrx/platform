5ad03ae124e78c6eb020e06797504c1e152de401
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-08-16 20:05:58 +0200

    fix: define proper example paths

--- projects/www/src/tools/prepare-examples.ts
+++ projects/www/src/tools/prepare-examples.ts
@@ -2,7 +2,7 @@ import * as globby from 'globby';
 import * as fs from 'node:fs';
 
 function copyFiles() {
-  const files = globby.sync(['projects/www/src/app/examples/**/*.ts']);
+  const files = globby.sync(['./src/app/examples/**/*.ts']);
 
   files.forEach((file) => {
     const txtFile = file.replace(/\.ts$/, '.txt');
