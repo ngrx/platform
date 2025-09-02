714beaac063406747032430cd2bb2d3a868e975c
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-09-02 12:27:01 +0200

    chore: consolidate dev dependencies

--- build/util.ts
+++ build/util.ts
@@ -1,69 +1,18 @@
-import * as fs from 'fs';
+import * as path from 'node:path';
 import * as cp from 'child_process';
-import * as glob from 'glob';
-import * as fsExtra from 'fs-extra';
-import * as path from 'path';
-import * as rimraf from 'rimraf';
+import ora from 'ora';
 import { Config } from './config';
 
 export type RunnerFn = (config: Config) => Promise<any>;
 export type TaskDef = [string, RunnerFn];
 export type BaseFn = (command: string) => string;
 
-export function copy(target: string, destination: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    fsExtra.copy(target, path.resolve(destination), (err) => {
-      if (err) return reject(err);
-      resolve();
-    });
-  });
-}
-
-export function remove(target: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    fsExtra.remove(target, (err) => {
-      if (err) return reject(err);
-      resolve();
-    });
-  });
-}
-
-export function writeFile(target: string, contents: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(target, contents, (err) => {
-      if (err) return reject(err);
-      resolve();
-    });
-  });
-}
-
-export function getListOfFiles(
-  globPath: string,
-  exclude?: string | string[]
-): Promise<string[]> {
-  return new Promise((resolve, reject) => {
-    const options = exclude ? { ignore: exclude } : {};
-
-    glob(globPath, options, (error, matches) => {
-      if (error) {
-        return reject(error);
-      }
-
-      resolve(matches);
-    });
-  });
-}
-
-export function removeRecursively(glob: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    rimraf(glob, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
+export function createBuilder(tasks: TaskDef[]) {
+  return async function (config: Config) {
+    for (const [name, runner] of tasks) {
+      await runTask(name, () => runner(config));
+    }
+  };
 }
 
 export function exec(
@@ -82,6 +31,10 @@ export function exec(
   });
 }
 
+export function getTopLevelPackages(config: Config) {
+  return config.packages.map((packageDescription) => packageDescription.name);
+}
+
 export function cmd(command: string, args: string[]): Promise<string> {
   return exec(command, args, (command: string) => command);
 }
@@ -90,19 +43,6 @@ export function git(args: string[]): Promise<string> {
   return cmd('git', args);
 }
 
-export function ignoreErrors<T>(promise: Promise<T>): Promise<T | null> {
-  return promise.catch(() => null);
-}
-
-export function fromNpm(command: string) {
-  return baseDir(`./node_modules/.bin/${command}`);
-}
-
-export function getPackageFilePath(pkg: string, filename: string) {
-  return baseDir(`./modules/${pkg}/${filename}`);
-}
-
-const ora = require('ora');
 async function runTask(name: string, taskFn: () => Promise<any>) {
   const spinner = ora(name);
 
@@ -119,40 +59,10 @@ async function runTask(name: string, taskFn: () => Promise<any>) {
   }
 }
 
-export function createBuilder(tasks: TaskDef[]) {
-  return async function (config: Config) {
-    for (let [name, runner] of tasks) {
-      await runTask(name, () => runner(config));
-    }
-  };
-}
-
-export function flatMap<K, J>(list: K[], mapFn: (item: K) => J[]): J[] {
-  return list.reduce(function (newList, nextItem) {
-    return [...newList, ...mapFn(nextItem)];
-  }, [] as J[]);
-}
-
-export function getTopLevelPackages(config: Config) {
-  return config.packages.map((packageDescription) => packageDescription.name);
-}
-
-export function baseDir(...dirs: string[]): string {
+function baseDir(...dirs: string[]): string {
   return `"${path.resolve(__dirname, '../', ...dirs)}"`;
 }
 
-export async function sleep(ms: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-export function getPrNumber(prNumber: string, circlePR: string): string {
-  const PR_NUMBER = prNumber;
-
-  if (!PR_NUMBER && circlePR) {
-    return circlePR;
-  }
-
-  return PR_NUMBER;
+function fromNpm(command: string) {
+  return baseDir(`./node_modules/.bin/${command}`);
 }
