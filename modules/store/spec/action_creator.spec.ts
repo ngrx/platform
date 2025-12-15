8eea5cf663cf5a06259e8fde19f0b6f400c3f5cf
Exequiel Ceasar Navarrete <dev@exequielnavarrete.me>
2025-12-15 09:21:52 +0800

    test(store): remove testTimeout variable

--- modules/store/spec/action_creator.spec.ts
+++ modules/store/spec/action_creator.spec.ts
@@ -1,132 +1,98 @@
 import { createAction, props, union } from '..';
 
 describe('Action Creators', () => {
-  const testTimeout = 2000;
-
   describe('createAction', () => {
-    it(
-      'should create an action',
-      () => {
-        const foo = createAction('FOO', (foo: number) => ({ foo }));
-        const fooAction = foo(42);
+    it('should create an action', () => {
+      const foo = createAction('FOO', (foo: number) => ({ foo }));
+      const fooAction = foo(42);
 
-        expect(fooAction).toEqual({ type: 'FOO', foo: 42 });
-      },
-      testTimeout
-    );
+      expect(fooAction).toEqual({ type: 'FOO', foo: 42 });
+    });
 
-    it(
-      'should narrow the action',
-      () => {
-        const foo = createAction('FOO', (foo: number) => ({ foo }));
-        const bar = createAction('BAR', (bar: number) => ({ bar }));
-        const both = union({ foo, bar });
-        const narrow = (action: typeof both) => {
-          if (action.type === foo.type) {
-            expect(action.foo).toEqual(42);
-          } else {
-            throw new Error('Should not get here.');
-          }
-        };
+    it('should narrow the action', () => {
+      const foo = createAction('FOO', (foo: number) => ({ foo }));
+      const bar = createAction('BAR', (bar: number) => ({ bar }));
+      const both = union({ foo, bar });
+      const narrow = (action: typeof both) => {
+        if (action.type === foo.type) {
+          expect(action.foo).toEqual(42);
+        } else {
+          throw new Error('Should not get here.');
+        }
+      };
 
-        narrow(foo(42));
-      },
-      testTimeout
-    );
+      narrow(foo(42));
+    });
 
-    it(
-      'should be serializable',
-      () => {
-        const foo = createAction('FOO', (foo: number) => ({ foo }));
-        const fooAction = foo(42);
-        const text = JSON.stringify(fooAction);
+    it('should be serializable', () => {
+      const foo = createAction('FOO', (foo: number) => ({ foo }));
+      const fooAction = foo(42);
+      const text = JSON.stringify(fooAction);
 
-        expect(JSON.parse(text)).toEqual({ type: 'FOO', foo: 42 });
-      },
-      testTimeout
-    );
+      expect(JSON.parse(text)).toEqual({ type: 'FOO', foo: 42 });
+    });
   });
 
   describe('empty', () => {
-    it(
-      'should allow empty action',
-      () => {
-        const foo = createAction('FOO');
-        const fooAction = foo();
+    it('should allow empty action', () => {
+      const foo = createAction('FOO');
+      const fooAction = foo();
 
-        expect(fooAction).toEqual({ type: 'FOO' });
-      },
-      testTimeout
-    );
+      expect(fooAction).toEqual({ type: 'FOO' });
+    });
   });
 
   describe('props', () => {
-    it(
-      'should create an action',
-      () => {
-        const foo = createAction('FOO', props<{ foo: number }>());
-        const fooAction = foo({ foo: 42 });
+    it('should create an action', () => {
+      const foo = createAction('FOO', props<{ foo: number }>());
+      const fooAction = foo({ foo: 42 });
 
-        expect(fooAction).toEqual({ type: 'FOO', foo: 42 });
-      },
-      testTimeout
-    );
+      expect(fooAction).toEqual({ type: 'FOO', foo: 42 });
+    });
 
-    it(
-      'should narrow the action',
-      () => {
-        const foo = createAction('FOO', props<{ foo: number }>());
-        const bar = createAction('BAR', props<{ bar: number }>());
-        const both = union({ foo, bar });
-        const narrow = (action: typeof both) => {
-          if (action.type === foo.type) {
-            expect(action.foo).toEqual(42);
-          } else {
-            throw new Error('Should not get here.');
-          }
-        };
-
-        narrow(foo({ foo: 42 }));
-      },
-      testTimeout
-    );
-
-    it(
-      'should allow the union of types in props',
-      () => {
-        interface A {
-          sameProp: 'A';
+    it('should narrow the action', () => {
+      const foo = createAction('FOO', props<{ foo: number }>());
+      const bar = createAction('BAR', props<{ bar: number }>());
+      const both = union({ foo, bar });
+      const narrow = (action: typeof both) => {
+        if (action.type === foo.type) {
+          expect(action.foo).toEqual(42);
+        } else {
+          throw new Error('Should not get here.');
         }
-        interface B {
-          sameProp: 'B';
-          extraProp: string;
-        }
-        type U = A | B;
-        const foo = createAction('FOO', props<U>());
+      };
 
-        const fooA = foo({ sameProp: 'A' });
-        const fooB = foo({ sameProp: 'B', extraProp: 'allowed' });
+      narrow(foo({ foo: 42 }));
+    });
 
-        expect(fooA).toEqual({ type: 'FOO', sameProp: 'A' });
-        expect(fooB).toEqual({
-          type: 'FOO',
-          sameProp: 'B',
-          extraProp: 'allowed',
-        });
-      },
-      testTimeout
-    );
+    it('should allow the union of types in props', () => {
+      interface A {
+        sameProp: 'A';
+      }
+      interface B {
+        sameProp: 'B';
+        extraProp: string;
+      }
+      type U = A | B;
+      const foo = createAction('FOO', props<U>());
 
-    it(
-      'should be serializable',
-      () => {
-        const foo = createAction('FOO', props<{ foo: number }>());
-        const fooAction = foo({ foo: 42 });
-        const text = JSON.stringify(fooAction);
+      const fooA = foo({ sameProp: 'A' });
+      const fooB = foo({ sameProp: 'B', extraProp: 'allowed' });
 
-        expect(JSON.parse(text)).toEqual({ foo: 42, type: 'FOO' });
-      },
-      testTimeout
-    );
+      expect(fooA).toEqual({ type: 'FOO', sameProp: 'A' });
+      expect(fooB).toEqual({
+        type: 'FOO',
+        sameProp: 'B',
+        extraProp: 'allowed',
+      });
+    });
+
+    it('should be serializable', () => {
+      const foo = createAction('FOO', props<{ foo: number }>());
+      const fooAction = foo({ foo: 42 });
+      const text = JSON.stringify(fooAction);
+
+      expect(JSON.parse(text)).toEqual({ foo: 42, type: 'FOO' });
+    });
   });
 });
