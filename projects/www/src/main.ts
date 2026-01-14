58c48918a4225eb38eccb67da3b0ca0b177dd181
Marko Stanimirović <markostanimirovic95@gmail.com>
2026-01-14 22:32:53 +0100

    docs(www): unregister legacy service worker

--- projects/www/src/main.ts
+++ projects/www/src/main.ts
@@ -2,4 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.browser';
 
+// Deactivate service worker from old ngrx.io app
+// TODO: Remove after 6 months
+if (typeof window !== 'undefined' && window.navigator.serviceWorker) {
+  window.navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 bootstrapApplication(AppComponent, config).catch((err) => console.error(err));
