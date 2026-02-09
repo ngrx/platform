71406e2055f91c6e324fc803322f34e4b7925f42
jase <804836+jase88@users.noreply.github.com>
2026-02-09 23:28:49 +0100

    refactor(example-app): migrate unit tests to Vitest (#5087)

--- projects/example-app/src/test-setup.ts
+++ projects/example-app/src/test-setup.ts
@@ -1,18 +1,5 @@
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+import '@angular/compiler';
+import '@analogjs/vitest-angular/setup-snapshots';
+import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
 
-setupZoneTestEnv();
-
-(global as any)['CSS'] = null;
-
-/**
- * ISSUE: https://github.com/angular/material2/issues/7101
- * Workaround for JSDOM missing transform property
- */
-Object.defineProperty(document.body.style, 'transform', {
-  value: () => {
-    return {
-      enumerable: true,
-      configurable: true,
-    };
-  },
-});
+setupTestBed();
