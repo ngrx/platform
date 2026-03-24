3a8bb62f7ab5e0c355de47bf8e4171ad34c0bdc6
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2026-03-24 15:08:29 +0100

    docs(store): show correct testing snippets (#5124)
    Co-authored-by: Marko Stanimirović <markostanimirovic95@gmail.com>

--- projects/www/src/app/examples/testing-store/src/app/app.component.spec.ts
+++ projects/www/src/app/examples/testing-store/src/app/app.component.spec.ts
@@ -1,14 +1,13 @@
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { AppState } from './state/app.state';
 import { AppComponent } from './app.component';
-import { addBook, removeBook } from './state/books.actions';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookCollectionComponent } from './book-collection/book-collection.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { By } from '@angular/platform-browser';
 import { selectBooks, selectBookCollection } from './state/books.selectors';
 
 describe('AppComponent', () => {
@@ -18,88 +17,32 @@ describe('AppComponent', () => {
   let mockBookCollectionSelector;
   let mockBooksSelector;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [provideMockStore()],
-      imports: [HttpClientTestingModule],
-      declarations: [BookListComponent, BookCollectionComponent, AppComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    });
-
-    store = TestBed.inject(MockStore);
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
-
-    mockBooksSelector = store.overrideSelector(selectBooks, [
-      {
-        id: 'firstId',
-        volumeInfo: {
-          title: 'First Title',
-          authors: ['First Author'],
-        },
-      },
-      {
-        id: 'secondId',
-        volumeInfo: {
-          title: 'Second Title',
-          authors: ['Second Author'],
-        },
-      },
-      {
-        id: 'thirdId',
-        volumeInfo: {
-          title: 'Third Title',
-          authors: ['Third Author'],
-        },
-      },
-      {
-        id: 'fourthId',
-        volumeInfo: {
-          title: 'Fourth Title',
-          authors: ['Fourth Author'],
-        },
-      },
-    ]);
-
-    mockBookCollectionSelector = store.overrideSelector(selectBookCollection, [
-      {
-        id: 'thirdId',
-        volumeInfo: {
-          title: 'Third Title',
-          authors: ['Third Author'],
-        },
-      },
-    ]);
-
-    fixture.detectChanges();
-    spyOn(store, 'dispatch').and.callFake(() => {});
+  TestBed.configureTestingModule({
+    providers: [provideMockStore()],
+    imports: [HttpClientTestingModule],
+    declarations: [BookListComponent, BookCollectionComponent, AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
   });
 
-  afterEach(() => {
-    TestBed.inject(MockStore)?.resetSelectors();
-  });
+  store = TestBed.inject(MockStore);
+  fixture = TestBed.createComponent(AppComponent);
+  component = fixture.componentInstance;
 
-  it('add method should dispatch add action', () => {
-    component.onAdd('firstId');
-    expect(store.dispatch).toHaveBeenCalledWith(addBook({ bookId: 'firstId' }));
-  });
+  //#docregion mockSelector
+  mockBooksSelector = store.overrideSelector(selectBooks, [
+    {
+      id: 'firstId',
+      volumeInfo: {
+        title: 'First Title',
+        authors: ['First Author'],
+      },
+    },
+  ]);
 
-  it('remove method should dispatch remove action', () => {
-    component.onRemove('thirdId');
-    expect(store.dispatch).toHaveBeenCalledWith(
-      removeBook({ bookId: 'thirdId' })
-    );
-  });
+  mockBookCollectionSelector = store.overrideSelector(selectBookCollection, []);
 
-  it('should render a book list and a book collection', () => {
-    expect(
-      fixture.debugElement.queryAll(By.css('.book-list .book-item')).length
-    ).toBe(4);
-    expect(
-      fixture.debugElement.queryAll(By.css('.book-collection .book-item'))
-        .length
-    ).toBe(1);
-  });
+  fixture.detectChanges();
+  spyOn(store, 'dispatch').and.callFake(() => {});
 
   it('should update the UI when the store changes', () => {
     mockBooksSelector.setResult([
@@ -117,13 +60,6 @@ describe('AppComponent', () => {
           authors: ['Second Author'],
         },
       },
-      {
-        id: 'thirdId',
-        volumeInfo: {
-          title: 'Third Title',
-          authors: ['Third Author'],
-        },
-      },
     ]);
 
     mockBookCollectionSelector.setResult([
@@ -134,13 +70,6 @@ describe('AppComponent', () => {
           authors: ['First Author'],
         },
       },
-      {
-        id: 'secondId',
-        volumeInfo: {
-          title: 'Second Title',
-          authors: ['Second Author'],
-        },
-      },
     ]);
 
     store.refreshState();
@@ -148,11 +77,60 @@ describe('AppComponent', () => {
 
     expect(
       fixture.debugElement.queryAll(By.css('.book-list .book-item')).length
-    ).toBe(3);
+    ).toBe(2);
 
     expect(
       fixture.debugElement.queryAll(By.css('.book-collection .book-item'))
         .length
-    ).toBe(2);
+    ).toBe(1);
+  });
+  //#enddocregion mockSelector
+});
+
+//#docregion resetMockSelector
+describe('AppComponent reset selectors', () => {
+  let store: MockStore;
+
+  afterEach(() => {
+    store?.resetSelectors();
+  });
+
+  it('should return the mocked value', (done: any) => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({
+          selectors: [
+            {
+              selector: selectBooks,
+              value: [
+                {
+                  id: 'mockedId',
+                  volumeInfo: {
+                    title: 'Mocked Title',
+                    authors: ['Mocked Author'],
+                  },
+                },
+              ],
+            },
+          ],
+        }),
+      ],
+    });
+
+    store = TestBed.inject(MockStore);
+
+    store.select(selectBooks).subscribe((mockBooks) => {
+      expect(mockBooks).toEqual([
+        {
+          id: 'mockedId',
+          volumeInfo: {
+            title: 'Mocked Title',
+            authors: ['Mocked Author'],
+          },
+        },
+      ]);
+      done();
+    });
   });
 });
+//#enddocregion resetMockSelector
