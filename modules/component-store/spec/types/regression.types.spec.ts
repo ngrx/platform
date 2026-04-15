0ff8b88e05174018888caf079be92c0639ea893a
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2026-04-15 22:21:50 +0200

    test(component-store): refactor to Vitest type tests

--- modules/component-store/spec/types/regression.types.spec.ts
+++ modules/component-store/spec/types/regression.types.spec.ts
@@ -1,44 +1,33 @@
-import { expecter } from 'ts-snippet';
-import { compilerOptions } from './utils';
+import { ComponentStore } from '../../';
+import { Observable } from 'rxjs';
+import { describe, it } from 'vitest';
 
 describe('regression component-store', () => {
-  const expectSnippet = expecter(
-    (code) => `
-        import { ComponentStore } from '@ngrx/component-store';
-        import { of, EMPTY, Observable } from 'rxjs';
-        import { concatMap } from 'rxjs/operators';
-
-        ${code}
-      `,
-    compilerOptions()
-  );
-
   it('https://github.com/ngrx/platform/issues/3482', () => {
-    const effectTest = `
-        interface SomeType {
-          name: string;
-          prop: string;
-        }
+    interface SomeType {
+      name: string;
+      prop: string;
+    }
 
-        export abstract class MyStore<
-          QueryVariables extends SomeType
-        > extends ComponentStore<any> {
-          protected abstract readonly query$: Observable<Omit<QueryVariables, 'name'>>;
+    abstract class MyStore<
+      QueryVariables extends SomeType,
+    > extends ComponentStore<any> {
+      protected abstract readonly query$: Observable<
+        Omit<QueryVariables, 'name'>
+      >;
 
-          readonly load = this.effect(
-            (origin$: Observable<Omit<QueryVariables, 'name'> | null>) => origin$
-          );
+      readonly load = this.effect(
+        (origin$: Observable<Omit<QueryVariables, 'name'> | null>) => origin$
+      );
 
-          protected constructor() {
-            super();
-          }
+      protected constructor() {
+        super();
+      }
 
-          protected initializeLoad() {
-            // 👇 this should work
-            this.load(this.query$);
-          }
-        }
-      `;
-    expectSnippet(effectTest).toSucceed();
+      protected initializeLoad() {
+        // 👇 this should work
+        this.load(this.query$);
+      }
+    }
   });
 });
