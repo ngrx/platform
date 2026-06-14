84a0ab5f6aade0d5d0d8da7b65009608b4ff1eca
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2026-06-14 19:10:48 +0200

    chore: update lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,20 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import js from '@eslint/js';
 import nxEslintPlugin from '@nx/eslint-plugin';
+import angularEslint from 'angular-eslint';
+import tseslint from 'typescript-eslint';
 
-const compat = new FlatCompat({
-  baseDirectory: dirname(fileURLToPath(import.meta.url)),
-  recommendedConfig: js.configs.recommended,
-});
+export const angularTsConfig = tseslint.config(
+  nxEslintPlugin.configs['flat/angular'],
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@angular-eslint/prefer-on-push-component-change-detection': 'off',
+    },
+  }
+);
 
-export default [
+export const angularTemplateConfig = angularEslint.configs.templateRecommended;
+
+export default tseslint.config(
   {
     ignores: ['**/dist'],
   },
@@ -33,78 +38,62 @@ export default [
       '@typescript-eslint/no-inferrable-types': 'warn',
     },
   },
-  ...compat
-    .config({
-      extends: ['plugin:@nx/typescript'],
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
-      rules: {
-        ...config.rules,
-        'no-extra-semi': 'off',
-      },
-    })),
-  ...compat
-    .config({
-      extends: ['plugin:@nx/javascript'],
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
-      rules: {
-        ...config.rules,
-        'no-extra-semi': 'off',
-      },
-    })),
-  ...compat
-    .config({
-      plugins: ['eslint-plugin-import', '@typescript-eslint'],
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.ts'],
-      rules: {
-        ...config.rules,
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'warn',
-          {
-            argsIgnorePattern: '^_',
-            varsIgnorePattern: '^_',
-            caughtErrorsIgnorePattern: '^_',
-          },
-        ],
-        '@typescript-eslint/naming-convention': 'off',
-        '@typescript-eslint/prefer-namespace-keyword': 'error',
-        '@typescript-eslint/no-empty-function': 'warn',
-        '@typescript-eslint/no-empty-object-type': 'warn',
-        eqeqeq: ['off', 'smart'],
-        'id-blacklist': [
-          'error',
-          'any',
-          'Number',
-          'number',
-          'String',
-          'string',
-          'Boolean',
-          'boolean',
-          'Undefined',
-          'undefined',
-        ],
-        'id-match': 'error',
-        'no-eval': 'off',
-        'no-redeclare': 'off',
-        'no-underscore-dangle': 'off',
-        'no-var': 'error',
-        'no-prototype-builtins': 'off',
-        '@typescript-eslint/no-wrapper-object-types': 'off',
-        '@typescript-eslint/no-unsafe-function-type': 'off',
-      },
-    })),
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
+    extends: [nxEslintPlugin.configs['flat/typescript']],
+    rules: {
+      'no-extra-semi': 'off',
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
+    extends: [nxEslintPlugin.configs['flat/javascript']],
+    rules: {
+      'no-extra-semi': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/prefer-namespace-keyword': 'error',
+      '@typescript-eslint/no-empty-function': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      eqeqeq: ['off', 'smart'],
+      'id-blacklist': [
+        'error',
+        'any',
+        'Number',
+        'number',
+        'String',
+        'string',
+        'Boolean',
+        'boolean',
+        'Undefined',
+        'undefined',
+      ],
+      'id-match': 'error',
+      'no-eval': 'off',
+      'no-redeclare': 'off',
+      'no-underscore-dangle': 'off',
+      'no-var': 'error',
+      'no-prototype-builtins': 'off',
+      '@typescript-eslint/no-wrapper-object-types': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+    },
+  },
   {
     files: ['**/*.html'],
     // Override or add rules here
     rules: {},
-  },
-];
+  }
+);
--- modules/component-store/eslint.config.mjs
+++ modules/component-store/eslint.config.mjs
@@ -1,58 +1,34 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import js from '@eslint/js';
-import baseConfig from '../../eslint.config.mjs';
+import tseslint from 'typescript-eslint';
+import baseConfig, {
+  angularTemplateConfig,
+  angularTsConfig,
+} from '../../eslint.config.mjs';
 
-const compat = new FlatCompat({
-  baseDirectory: dirname(fileURLToPath(import.meta.url)),
-  recommendedConfig: js.configs.recommended,
-});
-
-export default [
+export default tseslint.config(
   {
-    ignores: [
-      '**/dist',
-      '**/schematics-core/**/*.ts',
-    ],
+    ignores: ['**/dist', '**/schematics-core/**/*.ts'],
   },
-  ...baseConfig,
-  ...compat
-    .config({
-      extends: [
-        'plugin:@nx/angular',
-        'plugin:@angular-eslint/template/process-inline-templates',
-      ],
-      plugins: ['@typescript-eslint'],
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.ts'],
-      rules: {
-        ...config.rules,
-        '@angular-eslint/directive-selector': 'off',
-        '@angular-eslint/component-selector': 'off',
-        '@angular-eslint/prefer-standalone': 'off',
-        '@angular-eslint/prefer-inject': 'off',
+  baseConfig,
+  {
+    files: ['**/*.ts'],
+    extends: [angularTsConfig],
+    rules: {
+      '@angular-eslint/directive-selector': 'off',
+      '@angular-eslint/component-selector': 'off',
+      '@angular-eslint/prefer-standalone': 'off',
+      '@angular-eslint/prefer-inject': 'off',
+    },
+    languageOptions: {
+      parserOptions: {
+        project: ['modules/component-store/tsconfig.*.json'],
       },
-      languageOptions: {
-        parserOptions: {
-          project: ['modules/component-store/tsconfig.*.json'],
-        },
-      },
-    })),
-  ...compat
-    .config({
-      extends: ['plugin:@nx/angular-template'],
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.html'],
-      rules: {
-        ...config.rules,
-      },
-    })),
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angularTemplateConfig],
+  },
   {
     ignores: ['schematics-core'],
-  },
-];
+  }
+);
