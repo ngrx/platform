40e658f4ccecb51c86026fa44c30aedfe6d0d469
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2026-06-20 18:36:01 +0200

    refactor: use tree.visit to traverse files

--- modules/eslint-plugin/migrations/22_0_0-beta_0-rename-eslint-plugin-v9-imports/index.ts
+++ modules/eslint-plugin/migrations/22_0_0-beta_0-rename-eslint-plugin-v9-imports/index.ts
@@ -1,4 +1,3 @@
-import type { DirEntry } from '@angular-devkit/schematics';
 import type { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 const OLD_IMPORT = '@ngrx/eslint-plugin/v9';
@@ -16,43 +15,30 @@ const SUPPORTED_EXTENSIONS = [
 
 export default function renameEslintPluginV9Imports(): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    for (const filePath of visitFiles(tree.root)) {
-      if (!shouldMigrate(filePath)) {
-        continue;
+    tree.visit((filePath) => {
+      if (isInNodeModules(filePath) || !shouldMigrate(filePath)) {
+        return;
       }
 
       const content = tree.read(filePath)?.toString('utf-8');
       if (!content?.includes(OLD_IMPORT)) {
-        continue;
+        return;
       }
 
       tree.overwrite(filePath, content.replaceAll(OLD_IMPORT, NEW_IMPORT));
       context.logger.info(
         `[@ngrx/eslint-plugin] Renamed '${OLD_IMPORT}' to '${NEW_IMPORT}' in ${formatPath(filePath)}`
       );
-    }
+    });
   };
-}
-
-function* visitFiles(directory: DirEntry): Iterable<string> {
-  for (const fileName of directory.subfiles) {
-    const file = directory.file(fileName);
-    if (file) {
-      yield file.path;
-    }
-  }
-
-  for (const directoryName of directory.subdirs) {
-    if (directoryName === 'node_modules') {
-      continue;
-    }
-
-    yield* visitFiles(directory.dir(directoryName));
-  }
 }
 
 function shouldMigrate(filePath: string): boolean {
   return SUPPORTED_EXTENSIONS.some((extension) => filePath.endsWith(extension));
+}
+
+function isInNodeModules(filePath: string): boolean {
+  return filePath.split('/').includes('node_modules');
 }
 
 function formatPath(filePath: string): string {
