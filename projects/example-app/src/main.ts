7d70f02f33cca3388f55d30c04968ed612bd4d5f
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2026-07-29 20:49:27 +0200

    build: remove deprecated @angular/platform-browser-dynamic

--- projects/example-app/src/main.ts
+++ projects/example-app/src/main.ts
@@ -1,6 +1,6 @@
 import './polyfills';
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowser().bootstrapModule(AppModule);
